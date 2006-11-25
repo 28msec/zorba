1d142a798336f1e85152417c0a658fdb698caf15
paulped@D.ETHZ.CH <>
2006-11-25 00:42:31 +0000

    Initial commit of Fowler-Noll-Vo hash. This hash function has been floating around the net for a while.

---
+++ util/hashfun_test.cpp
@@ -0,0 +1,32 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: hashfun_test.cpp,v 1.1 2006/10/09 07:07:54 Paul Pedersen Exp $
+ *
+ *  Copyright 2006-2007 FLWOR Foundation.  All Rights Reserved.
+ *
+ */
+
+#include "hashfun.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace xqp;
+
+int main(int argc, char* argv[])
+{
+	string line;
+	unsigned key = 0;
+	while (!cin.eof()) {
+		getline(cin, line);
+		key = hashfun::h32(line);
+		cout << "h(" << line << ") = " << key << endl;
+		uint64_t key1 = hashfun::h64(line);
+		cout << "h64(" << line << ") = " << key1 << endl;
+		uint64_t key2 = hashfun::h64(line);
+		cout << "h64(" << line << ") = " << key2 << endl;
+		uint32_t key3 = (uint32_t)key2;
+		cout << "(unsigned)h64(" << line << ") = " << key3 << endl;
+	}
+}
