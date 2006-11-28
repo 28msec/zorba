61ab17d5687d1cc33dd50c96898d849f2a75be92
paulped@D.ETHZ.CH <>
2006-11-28 04:27:01 +0000

    Initial commit of 'file' class - encapsulates system file properties.  This class is vaguely modeled on the Java File class, but it uses some of the Unix file concepts.

---
+++ util/file.cpp
@@ -0,0 +1,291 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: file.cpp,v 1.3 2006/10/13 06:53:30 Paul Pedersen Exp $
+ *
+ *  Copyright 2006-2007 FLWOR Foundation.
+ *
+ */
+
+#include "file.h"
+
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/vfs.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "xqpexception.h"
+
+
+using namespace std;
+namespace xqp {
+
+static const uint64_t FS_TYPES[] = {
+    0x0000EF53, 0x0000adf5, 0x0000adff, 0x73757245, 0x00001373, 0x00414A53,
+    0x0000EF53, 0x0000EF53, 0xf995e849, 0x00009660, 0x000072b6, 0x0000137F,
+    0x0000138F, 0x00002468, 0x00002478, 0x00004d44, 0x00006969, 0x00009fa1,
+    0x00009fa0, 0x0000002f, 0x52654973, 0x012FF7B3, 0x012FF7B4, 0x012FF7B5,
+    0x012FF7B6, 0x00009fa2
+  };
+
+static const char *FS_NAMES[] = {
+    "ext2", "adfs", "affs", "coda", "devfs", "efs", "ext2", "ext3", "hpfs", 
+		"isofs", "jffs2", "minix", "minix_30charnames", "minix2", "minix2_30charnames",
+		"msdos", "nfs", "openprom", "proc", "qnx4", "reiserfs", "xenix", "sysv4",
+		"sysv2", "coh", "usbdevice"
+  };
+
+
+file::file(const std::string& _path)
+: path(_path),
+  type(type_non_existent)
+{
+	struct stat st;
+  if (::stat(path.c_str(), &st)) {
+    if (errno!=ENOENT) error(__FUNCTION__,"stat failed on "+path);
+  } 
+	else {
+  	size	= st.st_size;
+  	atime	= st.st_atime;
+  	mtime	= st.st_mtime;
+		type 	=	(st.st_mode & S_IFDIR)  ? type_directory :
+  					(st.st_mode & S_IFREG ) ? type_file :
+  					(st.st_mode & S_IFLNK)  ? type_link : type_invalid;
+	}
+}
+
+
+file::file(
+	std::string const& base,
+	std::string const& name)
+:
+	path(base+"/"+name),
+  type(type_non_existent)
+{
+	struct stat st;
+  if (::stat(path.c_str(), &st)) {
+    if (errno!=ENOENT) error(__FUNCTION__,"stat failed on "+path);
+  } 
+	else {
+  	size	= st.st_size;
+  	atime	= st.st_atime;
+  	mtime	= st.st_mtime;
+		type 	=	(st.st_mode & S_IFDIR)  ? type_directory :
+  					(st.st_mode & S_IFREG ) ? type_file :
+  					(st.st_mode & S_IFLNK)  ? type_link : type_invalid;
+	}
+}
+
+
+file::~file()
+{
+}
+
+
+enum file::filetype file::get_filetype( )
+{
+  if (type!=type_non_existent) return type;
+
+	// call native file system status
+	struct stat st;
+  if (::stat(path.c_str(), &st)) {
+    if (errno==ENOENT) {
+			errno = 0;
+    	return (type = type_non_existent);
+		}
+    error(__FUNCTION__,"stat failed on "+path);
+  } 
+  size	= st.st_size;
+  atime	= st.st_atime;
+  mtime	= st.st_mtime;
+	return (type 	=	(st.st_mode & S_IFDIR)  ? type_directory :
+  								(st.st_mode & S_IFREG ) ? type_file :
+  								(st.st_mode & S_IFLNK)  ? type_link : type_invalid );
+}
+
+
+volatile void file::error(
+	string const& location,
+	string const& msg)
+{
+  std::string err = strerror(errno);
+  errno = 0;
+  throw xqpexception(location, msg + " ["+err+']');
+}
+
+
+void file::create()
+{
+  int fd = ::creat(path.c_str(),0666);
+  if (fd < 0) error(__FUNCTION__, "failed to create file "+path);
+  ::close(fd);
+  set_filetype(type_file); 
+}
+
+
+void file::mkdir()
+{
+	if (::mkdir(path.c_str(),0777)) {
+		ostringstream oss;
+		oss<<"mkdir failed ["<<strerror(errno) << "]"<<"] for: "<<path;
+		cout << oss.str() << endl;	//XXX DEBUG
+    error(__FUNCTION__,oss.str());
+	}
+  set_filetype(file::type_directory);
+}
+
+
+void file::remove(bool ignore)
+{
+  if (::remove(path.c_str()) && !ignore) {
+    error(__FUNCTION__, "failed to remove "+path);
+	}
+  set_filetype(type_non_existent);
+}
+
+
+void file::rmdir(bool ignore)
+{
+	if (::rmdir(path.c_str()) && !ignore) {
+    error(__FUNCTION__, "rmdir failed on "+path);
+	}
+  set_filetype(file::type_non_existent);
+}
+
+
+void file::chdir()
+{
+  if (!is_directory()) return;
+  if (::chdir(path.c_str())) {
+    error(__FUNCTION__, "chdir failed on "+path);
+	}
+}
+
+
+void file::rename(
+	std::string const& newpath)
+{
+  if (::rename(path.c_str(), newpath.c_str())) {
+    ostringstream oss;
+    oss << path << " to " << newpath;
+    error(__FUNCTION__, "failed to rename: "+oss.str());
+  }
+  set_path(newpath);
+}
+
+
+void file::touch()
+{
+  int fd = open(path.c_str(),O_CREAT|O_WRONLY,0666);
+  if (fd<0) error(__FUNCTION__, "failed to open "+path);
+  try {
+    if (fsync(fd)) error(__FUNCTION__, "failed to fsync "+path);
+  } catch (xqpexception &e) {
+    close(fd);
+    throw;
+  }
+  if (close(fd)) error(__FUNCTION__, "failed to close "+path);
+}
+
+
+void file::do_statfs(
+	std::string const& path)
+{
+  struct statfs buf;
+  if (statfs(path.c_str(),&buf))
+		error(__FUNCTION__,"statfs failed on "+path);
+
+  blksize  = buf.f_bsize;
+  blktotal = buf.f_blocks;
+  blkfree  = buf.f_bfree;
+  blkavail = buf.f_bavail;
+  filtotal = buf.f_files;
+  filfree  = buf.f_ffree;
+  filavail = buf.f_ffree;
+  fsid     = buf.f_fsid;
+  fstype   = buf.f_type;
+
+  for (uint32_t i = 0; i < sizeof(FS_TYPES); ++i) {
+    if (FS_TYPES[i]==fstype) {
+      fstypename = FS_NAMES[i];
+      break;
+    }
+  }
+
+  access = rw;
+  setuid = true;
+  truncnames = false;
+}
+
+
+// dir_iterator
+
+#define END true
+
+file::dir_iterator file::begin()
+{
+  return file::dir_iterator(path);
+}
+
+file::dir_iterator file::end()
+{
+  return file::dir_iterator(path, END);
+}
+
+file::dir_iterator::dir_iterator(
+	string const& path,
+	bool end_iterator)
+:
+	dirpath(path),
+	dir(opendir(path.c_str())),
+  dirent(0)
+{
+  if (dir==0) {
+    error(__FUNCTION__, "opendir failed on "+dirpath);
+	}
+  if (!end_iterator) operator++();
+}
+
+
+file::dir_iterator::~dir_iterator()
+{
+  if (dir!=0) closedir(dir);
+}
+
+
+void file::dir_iterator::operator++()
+{
+  if (dir!=0) {
+    while (true) {
+      dirent = readdir(dir);
+      if (dirent==0) { 
+        closedir(dir); 
+        dir = 0; 
+        break; 
+      }
+      if (strcmp(dirent->d_name,".") &&
+          strcmp(dirent->d_name,"..") &&
+          strcmp(dirent->d_name,"lost+found")) {
+        break;
+			}
+    }
+  }
+}
+
+
+bool operator!=(
+	file::dir_iterator const& x,
+	file::dir_iterator const& y)
+{
+	if (x.dirpath==y.dirpath) return false;
+	if (x.dirent==y.dirent) return false;
+	return true;
+}
+
+
+} /* namespace xqp */
+
---
+++ util/file.h
@@ -0,0 +1,150 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: file.h,v 1.4 2006/10/22 01:32:21 Paul Pedersen Exp $
+ *
+ *  Copyright 2006-2007 FLWOR Foundation.
+ *
+ */
+
+#ifndef XQP_FILE_H
+#define XQP_FILE_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <dirent.h>
+#include <string>
+
+#include "rchandle.h"
+
+namespace xqp {
+
+class file : public rcobject
+{
+public:
+  enum filetype {
+    type_invalid,
+    type_non_existent,
+    type_directory,
+    type_link,
+    type_file,
+    type_volume
+  };
+
+public:
+  enum rwaccess {
+    ro = 1, // Read-Only
+    wo = 2, // WriteOnly
+    rw = 3  // Read and Write
+  };
+
+protected:
+  std::string path;
+  enum filetype type; 
+
+private:	// file attributes
+  int64_t  size;      		// size in bytes
+  time_t   atime;     		// most recent access time
+  time_t   mtime;     		// most recent mod time
+  uint32_t owner;     		// file owner uid
+  uint32_t group;     		// file group gid
+  uint32_t perms;     		// file permissions
+
+private:	// volume attributes
+  uint64_t filtotal;    	// total number of file inodes in file system
+  uint64_t filfree;     	// number of free file inodes in file system
+  uint64_t filavail;    	// number of free file inodes available to non super-users
+  uint64_t fsid;          // file system id
+  uint64_t fstype;        // numeric file system type identifier (fs-specific)
+  uint64_t blksize;     	// recommended file system block size in bytes
+  uint64_t blktotal;    	// total number of blocks in file system
+  uint64_t blkfree;     	// number of free blocks in file system
+  uint64_t blkavail;    	// number of free blocks available to non super-users
+  std::string fstypename;	// string name of file system type (null-terminated)
+  enum rwaccess access;   // level of read/write access
+  bool setuid;            // allows setuid/setguid
+  bool truncnames;        // truncates long filenames
+
+public:
+  file(std::string const& pathname);
+  file(std::string const& rootpath, std::string const& name);
+  ~file();
+
+public:	// common methods
+  void set_path(std::string const& _path ) { path = _path; }
+  void set_filetype(enum filetype _type ) { type = _type ; }
+  std::string const& get_path() const { return path; }
+  enum filetype get_filetype();
+
+  bool is_directory() const { return (type==type_directory); }  
+  bool is_file() const { return (type==type_file); }  
+  bool is_link() const { return (type==type_link); }  
+  bool is_volume() const { return (type==type_volume); }  
+
+  bool is_invalid() const { return (type==type_invalid); }  
+  bool exists() const { return (type!=type_non_existent && type!=type_invalid); }  
+  static volatile void error(std::string const& location, std::string const& msg);
+  static void sync() { ::sync(); }
+
+
+public:	// file methods
+  void create();
+  void remove(bool ignore);
+  void rename(std::string const& newpath);
+  void touch();
+
+  int64_t get_size() const				{ return size; }
+  time_t  get_acctime() const			{ return atime; }
+  time_t  get_modtime() const			{ return mtime; }
+  uint32_t get_ownerid() const		{ return owner; }
+  uint32_t get_groupid() const		{ return group; }
+  uint32_t get_permissions() const	{ return perms; }
+
+
+public:	// directory methods
+	class dir_iterator : public rcobject
+	{
+	public:
+  	dir_iterator(const std::string& path, bool end_iterator = false);
+  	~dir_iterator();
+	public:
+		void operator++();
+		const char* operator*() { return dirent->d_name; }
+	public:
+		const char* get_name() const { return dirent?dirent->d_name:0; } 
+	public:
+  	std::string dirpath;
+  	std::string path;
+  	DIR *dir;
+  	struct dirent *dirent;
+	};
+
+	void mkdir();
+	void rmdir(bool ignore);
+  void chdir();
+
+  dir_iterator begin();
+  dir_iterator end();
+
+	friend bool operator!=(dir_iterator const& x, dir_iterator const& y);
+
+public:	// volume methods
+  uint64_t get_block_size() const   { return blksize; }
+  uint64_t get_total_blocks() const { return blktotal; }
+  uint64_t get_avail_blocks() const { return blkavail; }
+  uint64_t get_total_inodes() const { return filtotal; }
+  uint64_t get_avail_inodes() const { return filavail; } 
+
+  std::string get_volume_type() const { return fstypename; }
+  enum rwaccess get_access() const { return access; }
+  bool has_set_uid() const { return setuid; }
+  bool has_truncated_names() const { return truncnames; }
+  bool is_empty() const { return (size == (int64_t)0); }
+
+  void do_statfs(std::string const& path);
+};
+
+
+} /* namespace xqp */
+#endif /* XQP_FILE_H */
