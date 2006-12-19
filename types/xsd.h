26f0d0444bb1ba10c8e12963013417be78f3b17b
paulped@D.ETHZ.CH <>
2006-12-19 07:54:33 +0000

    Initial commit of cleaned-up Axis xsd constraint facets and xsd -> C++ mapping declarations.

--- types/xsd.h
+++ types/xsd.h
@@ -23,59 +23,158 @@
  *
  */
 
-#ifndef XQP_AXIS_XSD_H
-#define XQP_AXIS_XSD_H
+#ifndef XQP_XSD_H
+#define XQP_XSD_H
 
 #include <ctime>
 #include <string>
 
-#include "Decimal.hpp"
-#include "ENTITY.hpp"
-#include "IAnySimpleType.hpp"
-#include "IDREF.hpp"
-#include "Int.hpp"
-#include "Integer.hpp"
-#include "Long.hpp"
-#include "Name.hpp"
-#include "NCName.hpp"
-#include "NMTOKEN.hpp"
-#include "NonNegativeInteger.hpp"
-#include "NonPositiveInteger.hpp"
-#include "NormalizedString.hpp"
-#include "Short.hpp"
-#include "Token.hpp"
-#include "UnsignedInt.hpp"
-#include "UnsignedLong.hpp"
-#include "UnsignedShort.hpp"
+#include "xsd_mapping.h"
+#include "xsd_constraints.h"
+#include "../util/xqpexception.h"
 
-
-#include "constraints/Enumeration.hpp"
-#include "constraints/FractionDigits.hpp"
-#include "constraints/Length.hpp"
-#include "constraints/MaxExclusive.hpp"
-#include "constraints/MaxInclusive.hpp"
-#include "constraints/MaxLength.hpp"
-#include "constraints/MinExclusive.hpp"
-#include "constraints/MinInclusive.hpp"
-#include "constraints/MinLength.hpp"
-#include "constraints/Pattern.hpp"
-#include "constraints/TotalDigits.hpp"
-#include "constraints/WhiteSpace.hpp"
 
 
 #include <axis/AxisUserAPI.hpp>
-#include <axis/GDefine.hpp>
-#include <axis/TypeMapping.hpp>
-
-
 #include "../apr_base64.h"
-#include "../AxisSoapException.h"
 #include "../HexCoder.h"
 #include "../../platforms/PlatformAutoSense.hpp"
 
 
 
 namespace xqp {
+
+class Decimal;
+class ENTITY;
+class IDREF;
+class Int;
+class Integer;
+class Long;
+class Name;
+class NCName;
+class NMTOKEN;
+class NonNegativeInteger;
+class NonPositiveInteger;
+class NormalizedString;
+class Short;
+class Token;
+class UnsignedInt;
+class UnsignedLong;
+class UnsignedShort;
+
+
+/**
+ *   @class IAnySimpleType
+ *   @brief Interface for all XSD built-in simple type.
+ *
+ *   @author Adrian Dick (adrian.dick@uk.ibm.com)
+ *
+ */
+class IAnySimpleType
+{
+public:
+	/**
+	 * Constructor
+	 */
+	IAnySimpleType();
+	
+	/**
+	 * Destructor
+	 */
+	virtual ~IAnySimpleType();
+
+	/**
+	 * This method provides ability to determine if value is nill.
+	 * @return true is value is nil, otherwise false
+	 */
+	bool isNil();
+
+	/**
+	 * Get the deserialized value
+	 * @return the deserialized value.
+	 *		Note: it is the responsibility of the calling code to delete this value!
+	 */
+	virtual void* getValue() = 0;
+
+	/**
+	 * Get the xsd type of this simple type.
+	 * @return the xsd type of this simple type
+	 */
+	virtual XSDTYPE getType() = 0;
+
+	/**
+	 * Provides the contained value in it's on-the-wire string form.
+	 * @return Serialized form of value
+	 */
+	AxisChar* serialize();
+
+	/**
+	 * Takes the value in it's on-the-wire string form
+	 * @param Serialized form of value
+	 */
+	void deserialize(const AxisChar* value);
+
+protected:
+	/**
+	 * Take partially serialized value, process WhiteSpace and validate Pattern.
+	 * @param value partially serialized value
+	 * @return Serialized form of value
+	 */
+	AxisChar* serialize(const AxisChar* value) throw (AxisSoapException);
+
+	/**
+	 * Set whether or not value is nil
+	 * @param nil
+	 */
+	void setNil(bool nil);
+
+	/**
+	 * Replace all XML reserved characters in string
+	 * @param value Original string
+	 * @return String with all reserved characters replaced
+	 */
+	const AxisString& replaceReservedCharacters(AxisString& value);
+
+	/**
+	 * Creates a WhiteSpace object which preserve original string form
+	 * @return WhiteSpace object
+	 */
+	virtual WhiteSpace* getWhiteSpace();
+
+	/**
+	 * Creates an Enumeration object. Enumeration is not defined for IAnySimpleType
+	 * so an unset Enumeration object is created.
+	 * @return Enumeration object
+	 */
+	virtual Enumeration* getEnumeration();
+   
+	/**
+	 * Create a Pattern object with no rules
+	 * @return Pattern object
+	 */
+	Pattern* getPattern();
+
+	/**
+	 * Reserved Characters
+	 */
+	enum
+	{
+		GREATER_THAN_CHAR		=    '>',		/* Greater than character */
+		LESSER_THAN_CHAR		=    '<',		/* Less than character */
+		SINGLE_QUOTE_CHAR		=    '\'',	/* Single quotation character */
+		DOUBLE_QUOTE_CHAR		=    '\"',	/* Double quotation character */
+		AMPERSAND_CHAR			=    '&'		/* Ampersand character */
+	};
+	AxisChar* m_Buf;
+   
+private:
+	bool m_isNil;
+	AxisString m_strReturnVal;
+
+};
+
+
+
 
 class AnyURI : public IAnySimpleType 
 {
@@ -154,7 +253,6 @@ protected:
 	Length* getLength();
 
 };
-
 
 
 
@@ -1597,117 +1695,6 @@ const AxisChar ENCODED_DBL_QUOTE_STR[]        = "&quot;";
 
 /* Encoded string for double quote character */
 const AxisChar ENCODED_SGL_QUOTE_STR[]        = "&apos;";    
-
-
-/**
- *   @class IAnySimpleType
- *   @brief Interface for all XSD built-in simple type.
- *
- *   @author Adrian Dick (adrian.dick@uk.ibm.com)
- *
- */
-class IAnySimpleType
-{
-public:
-	/**
-	 * Constructor
-	 */
-	IAnySimpleType();
-	
-	/**
-	 * Destructor
-	 */
-	virtual ~IAnySimpleType();
-
-	/**
-	 * This method provides ability to determine if value is nill.
-	 * @return true is value is nil, otherwise false
-	 */
-	bool isNil();
-
-	/**
-	 * Get the deserialized value
-	 * @return the deserialized value.
-	 *		Note: it is the responsibility of the calling code to delete this value!
-	 */
-	virtual void* getValue() = 0;
-
-	/**
-	 * Get the xsd type of this simple type.
-	 * @return the xsd type of this simple type
-	 */
-	virtual XSDTYPE getType() = 0;
-
-	/**
-	 * Provides the contained value in it's on-the-wire string form.
-	 * @return Serialized form of value
-	 */
-	AxisChar* serialize();
-
-	/**
-	 * Takes the value in it's on-the-wire string form
-	 * @param Serialized form of value
-	 */
-	void deserialize(const AxisChar* value);
-
-protected:
-	/**
-	 * Take partially serialized value, process WhiteSpace and validate Pattern.
-	 * @param value partially serialized value
-	 * @return Serialized form of value
-	 */
-	AxisChar* serialize(const AxisChar* value) throw (AxisSoapException);
-
-	/**
-	 * Set whether or not value is nil
-	 * @param nil
-	 */
-	void setNil(bool nil);
-
-	/**
-	 * Replace all XML reserved characters in string
-	 * @param value Original string
-	 * @return String with all reserved characters replaced
-	 */
-	const AxisString& replaceReservedCharacters(AxisString& value);
-
-	/**
-	 * Creates a WhiteSpace object which preserve original string form
-	 * @return WhiteSpace object
-	 */
-	virtual WhiteSpace* getWhiteSpace();
-
-	/**
-	 * Creates an Enumeration object. Enumeration is not defined for IAnySimpleType
-	 * so an unset Enumeration object is created.
-	 * @return Enumeration object
-	 */
-	virtual Enumeration* getEnumeration();
-   
-	/**
-	 * Create a Pattern object with no rules
-	 * @return Pattern object
-	 */
-	Pattern* getPattern();
-
-	/**
-	 * Reserved Characters
-	 */
-	enum
-	{
-		GREATER_THAN_CHAR		=    '>',		/* Greater than character */
-		LESSER_THAN_CHAR		=    '<',		/* Less than character */
-		SINGLE_QUOTE_CHAR		=    '\'',	/* Single quotation character */
-		DOUBLE_QUOTE_CHAR		=    '\"',	/* Double quotation character */
-		AMPERSAND_CHAR			=    '&'		/* Ampersand character */
-	};
-	AxisChar* m_Buf;
-   
-private:
-	bool m_isNil;
-	AxisString m_strReturnVal;
-
-};
 
 
 
@@ -3396,6 +3383,6 @@ protected:
 
 };
 
-}
-#endif
 
+}	/* namespace xqp */
+#endif	/* XQP_XSD_H */
