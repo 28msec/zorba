fb5ec2e72cf806941cbbd14c9166fa628a00b597
paulped@D.ETHZ.CH <>
2006-12-30 05:53:35 +0000

    Initial commit: context interface

---
+++ runtime/content_handler.h
@@ -0,0 +1,11 @@
+
+#ifndef XQP_CONTENT_HANDLER_H
+#define XQP_CONTENT_HANDLER_H
+
+   
+void handle_request(
+  request_rec* r,
+  char* buf,
+  int max);
+
+#endif  /* XQP_CONTENT_HANDLER_H */
---
+++ runtime/namespace.h
@@ -0,0 +1,35 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: namespace.h,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ *
+ *  Author:  Paul Pedersen
+ *
+ */
+
+#ifndef XQP_NAMESPACE_H
+#define XQP_NAMESPACE_H
+
+#include <string>
+
+namespace xqp {
+
+class name_space
+{
+protected:
+	std::string uri;
+
+public:
+	name_space(std::string const& _uri) : uri(_uri) {}
+	name_space() {}
+	~name_space() {}
+
+public:
+	std::string get_uri() const { return uri; }
+
+};
+
+
+};	/* namespace xqp */
+#endif	/* XQP_NAMESPACE_H */
