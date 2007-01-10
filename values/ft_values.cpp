f40e13c80035767e19ac29d15c5e1eb73d0f79d8
paulped@D.ETHZ.CH <>
2007-01-10 23:32:21 +0000

    Initial commit. values representing ft search specification objects these are the values of ft_selection expressions

---
+++ values/ft_values.cpp
@@ -0,0 +1,59 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: ft_values.cpp,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ *
+ *  Author: Paul Pedersen
+ *
+ */
+
+#include "ft_values.h"
+#include <iostream>
+
+using namespace std;
+namespace xqp {
+
+ostream& ft_option_value::operator<<(ostream& os)
+{
+	return os << "ft_option_value[]";
+}
+
+ostream& ft_or_value::operator<<(ostream& os)
+{
+	return os << "ft_or_value[]";
+}
+
+ostream& ft_and_value::operator<<(ostream& os)
+{
+	return os << "ft_and_value[]";
+}
+
+ostream& ft_mildnot_value::operator<<(ostream& os)
+{
+	return os << "ft_mildnot_value[]";
+}
+
+ostream& ft_unarynot_value::operator<<(ostream& os)
+{
+	return os << "ft_unarynot_value[]";
+}
+
+ostream& ft_qphrase_value::operator<<(ostream& os)
+{
+	return os << "ft_qphrase_value[]";
+}
+
+
+ft_mildnot_value::ft_mildnot_value(
+	rchandle<ft_value> _posval_h,
+	rchandle<ft_value> _negval_h)
+:
+	posval_h(_posval_h)
+{
+	negval_hv.push_back(_negval_h);
+}
+
+
+} /* namespace xqp */
+
---
+++ values/ft_values_test.cpp
@@ -0,0 +1,67 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: ft_values_test.cpp,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ *
+ *  Author: Paul Pedersen
+ *
+ */
+
+#include "ft_values.h"
+#include "../util/rchandle.h"
+#include <iostream>
+
+using namespace std;
+using namespace xqp;
+
+int main(int argc, char* argv[])
+{
+	rchandle<ft_phrase_value> p1_h = new ft_phrase_value();
+	pv.add("to");
+	pv.add("be");
+	pv.set_opt(ft_qphrase_value::ft_all);
+
+	rchandle<ft_phrase_value> p2_h = new ft_phrase_value();
+	pv.add("or");
+	pv.set_opt(ft_qphrase_value::ft_all);
+
+	rchandle<ft_phrase_value> p3_h = new ft_phrase_value();
+	pv.add("not");
+	pv.add("to");
+	pv.add("be");
+	pv.set_opt(ft_qphrase_value::ft_all);
+
+	rchandle<ft_unarynot_value> unot_h =
+		new ft_unarynot_value(p3_h);
+
+	rchandle<ft_mildnot_value> mnot_h =
+		new ft_mildnot_value(p1_h, p2_h);
+
+	rchandle<ft_and_value> and_h =
+		new ft_and_value(mnot_h, unot_h);
+
+	rchandle<ft_or_value> or_h =
+		new ft_or_value(mnot_h, unot_h);
+
+	ft_option_value opt;
+
+	uint32_t optval = ft_lowercase
+									| ft_case_sensitive
+									| ft_with_diacritics
+									| ft_without_stemming;
+
+	theasurus_id tid = { "mythesaurus URI", ft_thesaurus_id::use, 2 };
+
+	opt.set_matchopt(optval)
+	opt.add_thesaurus_id(tid);
+	opt.set_stopword_uri("my stopword URI");
+	opt.set_language("en");
+	opt.set_weight(0.5);
+	opt.set_stopword_exclusion_uri("my stopword exclusion URI");
+
+	cout << "ft_or_value = " << *or_h << endl;
+	cout << "ft_and_value = " << *and_h << endl;
+	cout << "ft_option_value = " << opt << endl;
+}
+
