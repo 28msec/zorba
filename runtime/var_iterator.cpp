b40359dcf292bfe32dcd939cec8fad417ddb1b17
paulped@D.ETHZ.CH <>
2007-01-13 02:56:14 +0000

    initial commit

---
+++ runtime/var_iterator.cpp
@@ -0,0 +1,52 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: var_iterator.cpp,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ *
+ *  Author: Paul Pedersen
+ *
+ */
+
+#include "var_iterator.h"
+
+using namespace std;
+namespace xqp {
+
+void var_iterator::open()
+{
+}
+
+void var_iterator::close()
+{
+}
+
+rchandle<item> var_iterator::next()
+{
+	return NULL;
+}
+
+bool var_iterator::done()
+{
+	return true;
+}
+
+void var_iterator::rewind()
+{
+}
+
+var_iterator::var_iterator(
+	context *const ctx_p,
+	rchandle<QName> _varname_h)
+:
+	item_iterator(ctx_p),
+	varname_h(_varname_h)
+{
+}
+
+var_iterator::~var_iterator()
+{
+}
+
+
+}	/* namespace xqp */
---
+++ runtime/var_iterator.h
@@ -0,0 +1,44 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: var_iterator.h,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ *
+ *  Author: Paul Pedersen
+ *
+ */
+
+#ifndef XQP_VAR_ITERATOR_H
+#define XQP_VAR_ITERATOR_H
+
+#include "item_iterator.h"
+
+namespace xqp {
+
+class var_iterator : public item_iterator
+{
+protected:
+	rchandle<QName> varname_h;
+
+public:	// iterator interface
+	void open();
+	void close();
+	rchandle<item> next();
+	bool done();
+	void rewind();
+
+public:	// ctor,dtor
+	var_iterator(
+		context *const ctx_p,
+		rchandle<QName>);
+
+	~var_iterator();
+
+public:	// manipulators
+	rchandle<QName> get_varname() const { return varname_h; };
+
+};
+
+
+}	/* namespace xqp */
+#endif	/* XQP_VAR_ITERATOR_H */
