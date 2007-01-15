c412d14e0ecc48677b53e5cba47157ad9aaff817
paulped@D.ETHZ.CH <>
2007-01-15 07:50:55 +0000

    formating for query parser output

---
+++ parser/indent.h
@@ -0,0 +1,53 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: indent.h,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ *
+ *  Author: Paul Pedersen
+ *
+ */
+
+#ifndef XQP_INDENT_H
+#define XQP_INDENT_H
+
+namespace xqp {
+
+// printing
+static char* indent[] = {
+	"",
+	"  ",
+	"    ",
+	"      ",
+	"        ",
+	"          ",
+	"            ",
+	"              ",
+	"                ",
+	"                  ",
+	"                    ",
+	"                      ",
+	"                        ",
+	"                          ",
+	"                            ",
+	"                              ",
+	"                                ",
+	"                                  ",
+	"                                    ",
+	"                                      ",
+	"                                        ",
+	"                                          ",
+	"                                            ",
+	"                                              ",
+	"                                                ",
+	"                                                  ",
+	"                                                    ",
+	"                                                      ",
+	"                                                        ",
+	"                                                          ",
+	"                                                            "
+									};
+
+} /* namespace xqp */
+#endif /* XQP_INDENT_H */
+
