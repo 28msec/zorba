4c5c5823ad71836a19317c9694a92cf4dae20f3d
paulped@D.ETHZ.CH <>
2007-01-15 01:03:39 +0000

    initial commit   flex headers   symbol_table test module

---
+++ parser/symbol_table_test.cpp
@@ -0,0 +1,39 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: symbol_table.cpp,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ */
+
+using namespace std;
+namespace xqp {
+
+/*
+	charheap heap;
+	hashmap<off_t> symtab;
+*/
+
+
+symbol_table::symbol_table()
+{
+}
+
+
+symbol_table::~symbol_table()
+{
+}
+
+
+uint32_t symbol_table::put(
+	std::string const& token)
+{
+}
+
+
+std::string symbol_table::get(
+	uint32_t id)
+{
+}
+
+
+}	/* namespace xqp */
