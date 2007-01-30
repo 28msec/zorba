cb8abb711ce0948da6d6681fcb0cd99e0da59389
paulped@D.ETHZ.CH <>
2007-01-30 05:17:43 +0000

    sync

--- build.sh
+++ build.sh
@@ -5,55 +5,55 @@ cd util
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../context
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../exprtree
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../functions
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../listeners
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../native_impl
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../parser
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../runtime
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../store
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../types
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 cd ../values
 if (!(test -d Build)); then
 	mkdir Build;
 fi;
-make all -f Makefile.local
+make all -f makefile.l
 
