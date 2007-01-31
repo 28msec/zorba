a2508779776795f2fe5a08da9598350c0ad56036
paulped@D.ETHZ.CH <>
2007-01-31 18:46:05 +0000

    initial commit

---
+++ functions/signature.cpp
@@ -0,0 +1,22 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: signature.cpp,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ *
+ *  Author: Paul Pedersen
+ *
+ */
+ 
+#include <vector>
+#include <string>
+
+#include "signature.h"
+ 
+using namespace std;
+namespace xqp {
+
+hashmap<signature const*> signature::sigmap;
+
+} /* namespace xqp */
+
