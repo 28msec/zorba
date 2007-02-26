4c8f5ad951ec2cdce34ee64dda864603699ea321
paulped@D.ETHZ.CH <>
2007-02-26 06:13:52 +0000

    initial commit: C++ <-> primitive type mapping
    This needs more work: duration is mapped to long,
    and date/time types are still mapped to 'struct tm'.

---
+++ types/representations.h
@@ -0,0 +1,66 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*-
+ *
+ *  $Id: representations.h,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ *  Author: John Cowan, Paul Pedersen
+ *
+ */
+
+#ifndef XQP_REPRESENTATIONS_H
+#define XQP_REPRESENTATIONS_H
+
+namespace xqp {
+
+/*______________________________________________________________________
+|  
+|	The mapping of basic schema types to C++ language types.
+|_______________________________________________________________________*/
+
+typedef std::string				xqp_anyURI;
+typedef std::string				xqp_string;
+typedef std::string				xqp_normalizedString;
+typedef std::string				xqp_token;
+typedef std::string				xqp_language;
+typedef std::string				xqp_Name;
+typedef std::string				xqp_NCName;
+typedef std::string				xqp_ID;
+typedef std::string				xqp_IDREF;
+typedef std::string				xqp_IDREFS;
+typedef std::string				xqp_ENTITY;
+typedef std::string				xqp_ENTITIES;
+typedef std::string				xqp_NMTOKEN;
+typedef std::string				xqp_NMTOKENS;
+typedef double   					xqp_integer;
+typedef double						xqp_int;
+typedef double						xqp_long;
+typedef double						xqp_short;
+typedef double						xqp_decimal;
+typedef double						xqp_float;
+typedef double						xqp_double;
+typedef double						xqp_byte;
+typedef bool							xqp_boolean;
+typedef std::string				xqp_QName;
+typedef std::string				xqp_NOTATION;
+typedef struct tm					xqp_dateTime;
+typedef struct tm					xqp_date;
+typedef struct tm					xqp_time;
+typedef struct tm					xqp_gYearMonth;
+typedef struct tm					xqp_gYear;
+typedef struct tm					xqp_gMonthDay;
+typedef struct tm					xqp_gDay;
+typedef struct tm					xqp_gMonth;
+typedef long							xqp_duration;
+typedef double						xqp_nonNegativeInteger;
+typedef double						xqp_unsignedByte;
+typedef double						xqp_unsignedInt;
+typedef double						xqp_unsignedLong;
+typedef double						xqp_unsignedShort;
+typedef double						xqp_nonPositiveInteger;
+typedef double						xqp_positiveInteger;
+typedef double						xqp_negativeInteger;
+
+
+} /* namespace xqp */
+#endif /* XQP_REPRESENTATIONS_H */
+
