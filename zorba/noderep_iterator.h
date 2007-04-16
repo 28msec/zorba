78988e3cc125083b0b2d306f4b67d022c6501b1b
paulped@D.ETHZ.CH <>
2007-04-16 06:04:33 +0000

    Initial commit. We are going to have a three-level architecture:
        noderep maps directly to storage
        zorba_node contains nodereps, and inherits from
        abstract_node
    
    The point is to have zorba_nodes that use rtti, and nodereps that use placement new.   There is no way, in practice, to mix these two models.
    
    This means that we have iterators that allocate and deallocate zorba_nodes containing noderep pointers in response to each call to next().  The system then needs to run a completely independent zorba_node cache to avoid excess allocation and deallocation of heap-allocated zorba_nodes.
    
    Additional implementations can then implement abstract_node with references to other storage systems.  Again, these implementations are responsible for their own allocation optimization.
    
    This  architecture is unfortunate, but it is the only way to reconcile the need for complete abstraction with the objective of having at least one native storage implementation.

---
+++ zorba/noderep_iterator.h
@@ -0,0 +1,49 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*- *
+ *
+ *  $Id: noderep_iterator.h,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
+ *
+ *	Copyright 2006-2007 FLWOR Foundation.
+ *  Author: John Cowan,Paul Pedersen
+ *
+ */
+
+#ifndef XQP_NODEREP_ITERATOR_H
+#define XQP_NODEREP_ITERATOR_H
+
+#include "../util/rchandle.h"
+
+namespace xqp {
+
+class noderep;
+
+class noderep_iterator : public rcobject
+{
+public:
+	// open: aquire resources
+	virtual void open() = 0;						
+
+	// close: release resources
+	virtual void close() = 0;
+
+	// next: seek + delta (or NULL)
+	virtual noderep* next(uint32_t d = 1) = 0;
+
+	// current item (or NULL)
+	virtual noderep* peek() const = 0;
+
+	// done => true <-> no more items
+	virtual bool done() const = 0;
+
+	// rewind: equivalent to:  close(); open() 
+	virtual void rewind() = 0; 
+
+public:	// C++ interface
+	virtual noderep* operator*() const = 0;
+	virtual noderep_iterator& operator++() = 0;
+
+};
+
+
+}	/* namespace xqp */
+#endif	/* XQP_NODEREP_ITERATOR_H */
+
