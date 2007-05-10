b1c8f3fbf553d7c8870f1dc36668dc50d9867095
paulped@D.ETHZ.CH <>
2007-05-10 03:24:09 +0000

    corrected the value_factory interface along with the zorba_value_factory implementation.

--- values/value_factory.h
+++ values/value_factory.h
@@ -28,10 +28,10 @@ public:
 		const std::string& localname) = 0;
 
 	// value construtors
-	virtual rchandle<atomic_value> make_xs_anyURI() = 0;
-	virtual rchandle<atomic_value> make_xs_base64Binary() = 0;
+	virtual rchandle<atomic_value> make_xs_anyURI(const std::string&) = 0;
+	virtual rchandle<atomic_value> make_xs_base64Binary(const void*,uint32_t) = 0;
 	virtual rchandle<atomic_value> make_xs_boolean(bool) = 0;
-	virtual rchandle<atomic_value> make_xs_decimal() = 0;
+	virtual rchandle<atomic_value> make_xs_decimal(long double) = 0;
 	virtual rchandle<atomic_value> make_xs_integer(int) = 0;
 	virtual rchandle<atomic_value> make_xs_long(long) = 0;
 	virtual rchandle<atomic_value> make_xs_int(int) = 0;
@@ -41,18 +41,18 @@ public:
 	virtual rchandle<atomic_value> make_xs_dateTime() = 0;
 	virtual rchandle<atomic_value> make_xs_double(double) = 0;
 	virtual rchandle<atomic_value> make_xs_duration() = 0;
-	virtual rchandle<atomic_value> make_xs_ENTITIES() = 0;
-	virtual rchandle<atomic_value> make_xs_ENTITY() = 0;
+	virtual rchandle<atomic_value> make_xs_ENTITIES(const std::string&) = 0;
+	virtual rchandle<atomic_value> make_xs_ENTITY(const std::string&) = 0;
 	virtual rchandle<atomic_value> make_xs_float(float) = 0;
 	virtual rchandle<atomic_value> make_xs_gDay() = 0;
 	virtual rchandle<atomic_value> make_xs_gMonth() = 0;
 	virtual rchandle<atomic_value> make_xs_gMonthDay() = 0;
 	virtual rchandle<atomic_value> make_xs_gYear() = 0;
 	virtual rchandle<atomic_value> make_xs_gYearMonth() = 0;
-	virtual rchandle<atomic_value> make_xs_hexBinary() = 0;
-	virtual rchandle<atomic_value> make_xs_ID() = 0;
-	virtual rchandle<atomic_value> make_xs_IDREF() = 0;
-	virtual rchandle<atomic_value> make_xs_IDREFS() = 0;
+	virtual rchandle<atomic_value> make_xs_hexBinary(const void*,uint32_t) = 0;
+	virtual rchandle<atomic_value> make_xs_ID(const std::string&) = 0;
+	virtual rchandle<atomic_value> make_xs_IDREF(const std::string&) = 0;
+	virtual rchandle<atomic_value> make_xs_IDREFS(const std::string&) = 0;
 	virtual rchandle<atomic_value> make_xs_language(const string&) = 0;
 	virtual rchandle<atomic_value> make_xs_NCName(const string&) = 0;
 	virtual rchandle<atomic_value> make_xs_NMTOKEN(const string&) = 0;
@@ -68,7 +68,7 @@ public:
 	virtual rchandle<atomic_value> make_xs_time() = 0;
 	virtual rchandle<atomic_value> make_xs_token(const string&) = 0;
 	virtual rchandle<atomic_value> make_xs_unsignedByte(unsigned char) = 0;
-	virtual rchandle<atomic_value> make_xs_unsignedInt(unsigned int) = 0;
+	virtual rchandle<atomic_value> make_xs_unsignedInt(int) = 0;
 	virtual rchandle<atomic_value> make_xs_unsignedLong(unsigned long) = 0;
 	virtual rchandle<atomic_value> make_xs_unsignedShort(uint16_t) = 0;
 	
