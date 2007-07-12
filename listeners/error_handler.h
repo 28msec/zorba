7c156f97dceedfb2ac69c520b0155f77765fde9d
dmuresa@D.ETHZ.CH <>
2007-07-12 06:41:08 +0000

    Added tab-width: 2 to listeners/

--- listeners/error_handler.h
+++ listeners/error_handler.h
@@ -1,5 +1,5 @@
 
-/* -*- mode: c++; indent-tabs-mode: nil -*-
+/* -*- mode: c++; indent-tabs-mode: nil; tab-width: 2 -*-
  *
  *  $Id: error_handler.h,v 1.1 2006/10/09 07:07:59 Paul Pedersen Exp $
  *
