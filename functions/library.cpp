0d469c790e4783bcf679593dfc1b7fe2b74f3f74
dmuresa@D.ETHZ.CH <>
2007-09-12 19:49:00 +0000

    Simplified registration of library operators/functions.

--- functions/library.cpp
+++ functions/library.cpp
@@ -19,6 +19,7 @@
 #include "runtime/zorba.h"
 #include "types/sequence_type.h"
 #include "util/tracer.h"
+#include "util/list.h"
 // #include "zorba/zorba_qname.h"
 #include "../store/api/item.h"
 #include "../store/native/atomic_items.h"
@@ -36,166 +37,113 @@ bool library::static_init = false;
 
 // TODO adapt to new store
 
-// Numerics
-op_numeric_add  op_add(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"op","add"),
-		xs_decimal,					// arg[1] type
-		xs_decimal,					// arg[2] type
-		xs_decimal					// return type
-	)
-);
+  class initializer;
+  static list<const initializer *> initializers;
 
-op_numeric_subtract  op_subtract(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"op","subtract"),
-		xs_decimal,					// arg[1] type
-		xs_decimal,					// arg[2] type
-		xs_decimal					// return type
-	)
-);
+  class initializer {
+    const function &f;
+    qnamekey_t &k;
 
-op_numeric_multiply op_multiply(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"op","multiply"),
-		xs_decimal,
-		xs_decimal,
-		xs_decimal
-	)
-);
- 
-op_numeric_divide op_divide(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"op","divide"),
-		xs_decimal,
-		xs_decimal,
-		xs_decimal
-	)
-);
- 
-// op_numeric_integer_divide
-// 
-op_numeric_mod op_mod(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"op","mod"),
-		xs_decimal,
-		xs_decimal,
-		xs_decimal
-	)
-);
- 
-// op_numeric_unary_plus
-// 
-// op_numeric_unary_minus
+  public:
+    initializer (const function &f_, qnamekey_t &k_) : f (f_), k (k_)
+    { initializers.push_front (this); }
+    const function &run () const {
+      k = f.get_fname()->getQNameKey();
+      return f;
+    }
+  };
 
-op_numeric_equal op_equal(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"op","equal"),
-		xs_decimal,
-		xs_decimal,
-		xs_boolean
-	)
-);
+#define DECL( key, type, sig )                                          \
+  qnamekey_t library::key##_key;                                        \
+  type key##_tmp_obj (signature sig);                                   \
+  initializer key##_tmp_init (key##_tmp_obj, library::key##_key)
+  
+  // Numerics
+    
+  // op_numeric_unary_plus
+  // 
+  // op_numeric_unary_minus
+    
+  DECL (op_add, op_numeric_add,
+        (new QNameItem (XQUERY_FN_NS,"op","add"),
+         xs_decimal, xs_decimal, xs_decimal));
+  DECL (op_subtract, op_numeric_subtract,
+        (new QNameItem (XQUERY_FN_NS,"op","subtract"),
+         xs_decimal, xs_decimal, xs_decimal));
+  DECL (op_mul, op_numeric_multiply,
+        (new QNameItem (XQUERY_FN_NS,"op","multiply"),
+         xs_decimal, xs_decimal, xs_decimal));
+  DECL (op_div, op_numeric_divide,
+        (new QNameItem (XQUERY_FN_NS,"op","divide"),
+         xs_decimal, xs_decimal, xs_decimal));
+  DECL (op_mod, op_numeric_mod,
+        (new QNameItem (XQUERY_FN_NS,"op","mod"),
+         xs_decimal, xs_decimal, xs_decimal));
 
+  DECL (op_val_eq, op_numeric_equal,
+        (new QNameItem(XQUERY_FN_NS,"op","equal"),
+         xs_decimal, xs_decimal,
+         xs_boolean));
 
-fn_abs fn_abs_sig(
-	signature(
-		new QNameItem(XQUERY_FN_NS, "fn", "abs"),
-		xs_decimal,
-		xs_decimal
-	)
-);
+  DECL (fn_abs, fn_abs,
+        (new QNameItem(XQUERY_FN_NS, "fn", "abs"),
+         xs_decimal,
+         xs_decimal));
 
-// Sequences
-fn_doc_func fn_doc(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"fn","doc"),
-		xs_string,					// arg[1] type
-		documentNode				// return type
-	)
-);
+  // Sequences
+  DECL (fn_doc, fn_doc_func,
+        (new QNameItem(XQUERY_FN_NS,"fn","doc"),
+         xs_string,
+         documentNode));
 
-// Comparisons
-op_greater op_greater_func(
-	signature(
-		new QNameItem(XQUERY_OP_NS,"op","greater"),
-		xs_anyType,
-		xs_anyType,
-		xs_boolean
-	)
-);
+  // Comparisons
+  DECL (op_gt, op_greater,
+        (new QNameItem(XQUERY_OP_NS,"op","greater"),
+         xs_anyType, xs_anyType,
+         xs_boolean));
 
+  // Strings
 
-// Strings
+  DECL (fn_codepoints_to_string, fn_codepoints_to_string,
+        (new QNameItem(XQUERY_FN_NS,"fn","codepoints-to-string"),
+         xs_integer,
+         xs_string));
 
-fn_codepoints_to_string fn_codepoints_to_string_func(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"fn","codepoints-to-string"),
-		xs_integer,					// arg[0] type
-		xs_string						// return type
-	)
-); 
+  DECL (fn_string_to_codepoints, fn_string_to_codepoints,
+        (new QNameItem(XQUERY_FN_NS,"fn","string-to-codepoints"),
+         xs_string,
+         xs_long));
 
-fn_string_to_codepoints fn_string_to_codepoints_func(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"fn","string-to-codepoints"),
-		xs_string,					// arg[0] type
-		xs_long							// return type
-	)
-);
+  DECL (fn_codepoint_equal, fn_codepoint_equal,
+        (new QNameItem(XQUERY_FN_NS,"fn","codepoint-equal"),
+         xs_string, xs_string,
+         xs_boolean));
 
-fn_codepoint_equal fn_codepoint_equal_func(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"fn","codepoint-equal"),
-		xs_string,					// arg[0] type
-		xs_string,					// arg[1] type
-		xs_boolean					// return type
-	)
-);
+  DECL (fn_concat, fn_concat,
+        (new QNameItem(XQUERY_FN_NS,"fn","concat"),
+         xs_anyType,
+         xs_boolean));
 
-fn_concat fn_concat_func(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"fn","concat"),
-		xs_anyType,					// args type
-		xs_boolean					// return type
-	)
-);
+  DECL (fn_string_join, fn_string_join,
+        (new QNameItem(XQUERY_FN_NS,"fn","string-join"),
+         xs_string, xs_string,
+         xs_string));
 
-fn_string_join fn_string_join_func(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"fn","string-join"),
-		xs_string,					// arg[0] type
-		xs_string,					// arg[1] type
-		xs_string						// return type
-	)
-);
+  DECL (fn_boolean, fn_boolean,
+        (new QNameItem(XQUERY_FN_NS,"fn","boolean"),
+         xs_anyType,
+         xs_boolean));
 
-fn_boolean fn_boolean_func(
-	signature(
-		new QNameItem(XQUERY_FN_NS,"fn","boolean"),
-		xs_anyType,
-		xs_boolean
-	)
-);
+// TODO
 
-qnamekey_t library::op_add_key;
-qnamekey_t library::op_subtract_key;
-qnamekey_t library::op_mul_key;
-qnamekey_t library::op_div_key;
 qnamekey_t library::op_idiv_key;
-qnamekey_t library::op_mod_key;
-
-qnamekey_t library::fn_abs_key;
-
 
 // Comparison operators
 qnamekey_t library::op_eq_key;
 qnamekey_t library::op_ne_key;
 qnamekey_t library::op_lt_key;
 qnamekey_t library::op_le_key;
-qnamekey_t library::op_gt_key;
 qnamekey_t library::op_ge_key;
-qnamekey_t library::op_val_eq_key;
 qnamekey_t library::op_val_ne_key;
 qnamekey_t library::op_val_lt_key;
 qnamekey_t library::op_val_le_key;
@@ -211,73 +159,19 @@ qnamekey_t library::op_concatenate_key;
 qnamekey_t library::op_union_key;
 qnamekey_t library::op_intersect_key;
 qnamekey_t library::op_except_key;
-qnamekey_t library::fn_doc_key;
-
-// Strings
-qnamekey_t library::fn_codepoints_to_string_key;
-qnamekey_t library::fn_string_to_codepoints_key;
-qnamekey_t library::fn_codepoint_equal_key;
-qnamekey_t library::fn_concat_key;
-qnamekey_t library::fn_string_join_key;
-
-qnamekey_t library::fn_boolean_key;
 
 // initializer
 
 void library::init(
 	ItemFactory* vf_p)
 {
+  
 	if (!library::static_init) {
-  	
-		// Numeric functions
-		put(&op_add);
-		put(&op_subtract);
-		put(&op_multiply);
-		put(&op_divide);
-		put(&op_mod);
-		put(&op_equal);
-		put(&fn_abs_sig);
-
-		op_add_key = op_add.get_fname()->getQNameKey();
-		op_subtract_key = op_subtract.get_fname()->getQNameKey();
-		op_mul_key = op_multiply.get_fname()->getQNameKey();
-		op_div_key = op_divide.get_fname()->getQNameKey();
-		op_mod_key = op_mod.get_fname()->getQNameKey();
-		op_val_eq_key = op_equal.get_fname()->getQNameKey();
-
-		fn_abs_key = fn_abs_sig.get_fname()->getQNameKey();
-
-		
-		// Sequences functions
-		put(&fn_doc);
-
-		
-		fn_doc_key = fn_doc.get_fname()->getQNameKey();
-		cout << TRACE << " : fn_doc_key = " << fn_doc_key << endl;
-		
-		// Comparison functions
-		put(&op_greater_func);
-		op_gt_key = op_greater_func.get_fname()->getQNameKey();
-		
-
-		// String functions
-		put(&fn_codepoints_to_string_func);
-		put(&fn_string_to_codepoints_func);
-		put(&fn_codepoint_equal_func);
-		put(&fn_concat_func);
-		put(&fn_string_join_func);
-		
-		fn_codepoints_to_string_key = fn_codepoints_to_string_func.get_fname()->getQNameKey();
-		fn_string_to_codepoints_key = fn_string_to_codepoints_func.get_fname()->getQNameKey();
-		fn_codepoint_equal_key = fn_codepoint_equal_func.get_fname()->getQNameKey();
-		fn_concat_key = fn_concat_func.get_fname()->getQNameKey();
-		fn_string_join_key = fn_string_join_func.get_fname()->getQNameKey();
-
-		// Boolean functions
-		put(&fn_boolean_func);
-		fn_boolean_key = fn_boolean_func.get_fname()->getQNameKey();
-
-		library::static_init = true;///don't initialize again
+    while (initializers.size () > 0) {
+      const function &f = initializers.pop_front ()->run ();
+      put (& f);
+    }
+    static_init = true; //don't initialize again
 	}
 }
 
@@ -320,4 +214,3 @@ const function* library::get(qnamekey_t fun_key)
 }
 
 } /* namespace xqp */
-
