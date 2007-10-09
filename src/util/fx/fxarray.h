cad85667cd4121c2ef78bf379cca7aaaf517f848
brantmat@D.ETHZ.CH <>
2007-10-09 14:07:42 +0000

    moved efficient byte libraries into separate folder

---
+++ src/util/fx/fxarray.h
@@ -0,0 +1,268 @@
+/* -*- mode: c++; indent-tabs-mode: nil; tab-width: 2 -*-
+ *
+ *  $Id: fxarray.h,v 1.2 2006/10/22 01:32:22 Paul Pedersen Exp $
+ *
+ *  Copyright 2006-2007 FLWOR Foundation.  All Rights Reserved.
+ *
+ *	Author: Paul Pedersen
+ *
+ */
+
+#ifndef XQP_FXARRAY_H
+#define XQP_FXARRAY_H
+
+#ifdef WIN32
+#else
+#include <sys/mman.h>
+#endif
+
+#ifndef _WIN32_WCE
+	#include <sys/types.h>
+	#include <errno.h>
+	#include <fcntl.h>
+#else
+	#include <windows.h>
+	#include <types.h>
+#endif
+#include <string>
+#include <sstream>
+#include <iostream>
+
+#include "mmfile.h"
+#include "../errors/Error.h"
+
+using namespace std;
+namespace xqp {
+
+#define DEFAULT_SIZE 4096
+
+
+/*_____________________________________________________________
+|
+|  Flex-array.
+|
+|  Given a path name, this will create a memory-mapped
+|  array of type T.
+|
+|  With no path name given, this will create a simple
+|  in-memory array of type T.
+|______________________________________________________________*/
+
+template<typename T>
+class fxarray
+{
+public:		// state
+	mmfile * mmf_p;		// memory-mapped case, may be NULL
+	char * src;				// raw view: either mmf_p->data or heap pointer
+	uint32_t eofoff;	// raw view: sentinel byte offset
+
+  T * _begin;      	// element view: pointer to first
+  T * _end;					// element view: pointer to sentinel
+
+public:		// ctor,dtor
+	/**
+   ** Create a memory-mapped fxarray with a given backing file.
+	 **
+   ** @param path - backing file pathname 
+	 ** @param size - initial size
+  */
+  fxarray(string const& path, uint32_t initial_size=DEFAULT_SIZE);
+
+	/**
+   ** Create a fxarray in memory.
+	 **
+	 ** @param size - initial size
+   */
+  fxarray(uint32_t initial_size=DEFAULT_SIZE);
+
+	/**
+   ** Deallocate, possibly unmap and delete backing file.
+   */
+  ~fxarray();
+
+	/**
+   ** Destroy all the elements.
+	 **	In the mmfile case, unmap and close backing file.
+   */
+	void destroy() throw (xqp_exception);
+
+public: 	// array interface
+	/**
+   ** Subscript access to the data contained in the fxarray.
+	 **
+   ** @param n - index of the element for which data should be accessed.
+   ** @return  Read/write reference to data.
+   ** @note data access with this operator is unchecked and
+   **       out-of-range lookups are not defined.
+   */
+  T& operator[](size_t n) { return *(_begin + n); }
+
+	/**
+	 ** Return the size of this array.
+	 ** @return array size
+	 */
+  uint32_t size() const { return (eofoff / sizeof(T)); }
+
+	/**
+	 ** Double the array (and its file).
+	 **
+	 ** @param init - if true then initialize the new entries to 0
+	 */
+  void expand(bool init = false) throw (xqp_exception);
+
+	/**
+	 ** Fill array with an initial byte value.
+	 **
+	 ** @param initval the initial value to fill with
+	 */
+  void fill(char initval);
+
+	/**
+	 **	Return mmfile
+   */
+	mmfile * get_mmfile() const { return mmf_p; }
+
+
+public:		// iterator interface
+	/**
+	 ** Return the first element.
+	 **
+	 ** @return first element
+	 */
+  T * begin() { return _begin; }
+
+	/**
+	 ** Return the sentinel element.
+	 **
+	 ** @return sentinel element
+	 */
+  T * end() { return _end; }
+
+
+public:		// memory-mapped only, nops in the non-mm case
+	/**
+	 ** Unmap the backing file.
+	 */
+	void unmap() throw (xqp_exception);
+
+	/**
+	 ** Return the path of the backing file.
+	 **
+	 ** @return path of backing file
+	 */
+	string get_path() const { return mmf_p ? mmf_p->get_path() : ""; }
+
+	/**
+	 ** Rename the backing file.
+	 */
+  void rename_backing_file(string const& new_path) throw (xqp_exception);
+	/**
+	 ** Return data array pointer.
+	 **
+	 ** @return data array pointer
+	 */
+	char * get_data() const { return src; }
+
+	/**
+	 ** Return the data array sentinel.
+	 **
+	 ** @return data array sentinel
+	 */
+	uint32_t get_eofoff() const { return eofoff; }
+
+};
+
+
+
+
+template<typename T>
+fxarray<T>::fxarray(string const& path, uint32_t size)
+:
+	mmf_p(new mmfile(path, size*sizeof(T)))
+{
+	eofoff = mmf_p->get_eofoff();
+	src = mmf_p->get_data();
+  _begin = reinterpret_cast<T*>(src);
+  _end   = reinterpret_cast<T*>(src+eofoff);
+}
+
+
+template<typename T>
+fxarray<T>::fxarray(uint32_t size)
+:
+	mmf_p(NULL)
+{
+	eofoff = size*sizeof(T);
+	src = new char[eofoff];
+  _begin = reinterpret_cast<T*>(src);
+  _end   = reinterpret_cast<T*>(src+eofoff);
+}
+
+
+template<typename T>
+fxarray<T>::~fxarray()
+{
+	if (mmf_p) {
+		delete mmf_p;
+	}
+	else if (src) {
+		delete[] src;
+	}
+}
+
+
+template<typename T>
+void fxarray<T>::fill(char initval)
+{
+  memset(src, initval, eofoff);
+}
+
+
+template<typename T>
+void fxarray<T>::expand(bool init)
+throw (xqp_exception)
+{
+	if (mmf_p) {
+		mmf_p->expand(init);
+		src = mmf_p->get_data();
+		eofoff = mmf_p->get_eofoff();
+	}
+	else {
+		char * src0 = new char[eofoff<<1];
+		memcpy(src0, src, eofoff);
+		eofoff <<= 1;
+		delete[] src;
+		src = src0;
+	}
+
+  _begin = reinterpret_cast<T*>(src);
+  _end   = reinterpret_cast<T*>(src+eofoff);
+
+}
+
+
+template<typename T>
+void fxarray<T>::unmap()
+throw (xqp_exception)
+{
+	if (mmf_p) mmf_p->unmap();
+}
+
+template<typename T>
+void fxarray<T>::rename_backing_file(const string& new_path)
+throw (xqp_exception)
+{
+	if (mmf_p) mmf_p->rename_backing_file(new_path);
+}
+
+template<typename T>
+void fxarray<T>::destroy()
+throw (xqp_exception)
+{
+	if (mmf_p) mmf_p->destroy();
+}
+
+
+}  /* namespace xqp */
+#endif  /* XQP_FXARRAY_H */
+
---
+++ src/util/fx/fxvector_test.cpp
@@ -0,0 +1,127 @@
+/* -*- mode: c++; indent-tabs-mode: nil; tab-width: 2 -*-
+ *
+ *  $Id: persistent_vector_test.cpp,v 1.1 2006/10/09 07:08:01 Paul Pedersen Exp $
+ *
+ *  Copyright 2006-2007 Paul Pedersen.  All Rights Reserved.
+ *
+ *	Author: Paul Pedersen
+ *
+ */
+
+#include "fxvector.h"
+
+#include <iostream>
+#include <fstream>
+
+#include "errors/xqp_exception.h"
+#include "tokenbuf.h"
+
+using namespace std;
+using namespace xqp;
+
+
+int main(int argc, char* argv[])
+{
+  try {
+		fxvector<uint32_t> * fxvec_p;
+		if (argc > 1) {
+    	fxvec_p = new fxvector<uint32_t>(string(argv[1],0,strlen(argv[1])));
+		}
+		else {
+    	fxvec_p = new fxvector<uint32_t>();
+		}
+		fxvector<uint32_t> & fxvec = *fxvec_p;
+  
+    // check metadata
+    cout << "v.size() = " << fxvec.size() << endl;
+    cout << "v.capacity() = " << fxvec.capacity() << endl;
+
+    // command loop
+		string cmdline, cmd, arg1, arg2;
+		uint32_t arg_count = 0;
+
+    while (true) {
+			getline(cin, cmdline);
+			if (cmdline.length()==0) continue;
+			
+			tokenbuf tokbuf(cmdline.c_str()," ");
+			tokbuf.set_lowercase(false);
+			tokbuf.set_return_delims(false);
+
+			tokenbuf::token_iterator it = tokbuf.begin();
+			tokenbuf::token_iterator end = tokbuf.end();
+			
+			cmd = *it;
+			if (it!=end) { ++it; arg1 = *it; ++arg_count; }
+			if (it!=end) { ++it; arg2 = *it; ++arg_count; }
+
+			cout << "cmd = " << cmd << endl;
+			if (arg_count>0) cout << "arg1 = " << arg1 << endl;
+			if (arg_count>1) cout << "arg2 = " << arg2 << endl;
+
+      if (cmd=="quit" || cmd=="exit") {
+				delete fxvec_p;
+				break;
+			}
+      else if (cmd=="capacity") {
+        cout << "capacity = " << fxvec.capacity() << endl;
+      }
+      else if (cmd=="help") {
+				cout << "alloc N       - allocate N bytes off end of vector\n";
+        cout << "at N          - return the N-th element of v\n";
+        cout << "cap           - print capacity\n";
+        cout << "clear         - clear the vector\n";
+				cout << "cp STRING     - raw byte copy\n";
+        cout << "help          - this help function\n";
+        cout << "iter          - run the vector iterator\n";
+				cout << "load FNAME    - load file\n";
+        cout << "size          - return the size of this vector\n";
+				cout << "uniq          - set the entries to a uniq sequence\n";
+			}
+			else if (cmd=="alloc") {
+        unsigned n = strtol(arg1.c_str(),NULL,10);
+				fxvec.alloc(n);
+			}
+			else if (cmd=="cp") {
+				uint32_t n = arg1.length();
+				char *p = fxvec.raw_copy(arg1.c_str(), n);
+				cout << string(p,0,n) << endl;
+      }
+      else if (cmd=="iter") {
+        fxvector<uint32_t>::const_iterator it = fxvec.begin();
+        for (; it!=fxvec.end(); ++it) {
+          cout << *it << endl;
+        }
+      }
+      else if (cmd=="uniq") {
+        for (unsigned k=0; k<fxvec.size(); ++k) {
+          fxvec[k] = k;
+        }
+      }
+      else if (cmd=="at") {
+        unsigned n = strtol(arg1.c_str(),NULL,10);
+        uint32_t e = fxvec[n];
+        cout << "v[" << n << "] = " << e << endl;
+      }
+      else if (cmd=="size") {
+        cout << "size = " << fxvec.size() << endl;
+      }
+      else if (cmd=="clear") {
+				fxvec.clear();
+        cout << "size = " << fxvec.size() << endl;
+      }
+			else if (cmd=="load") {
+				ifstream in(arg1.c_str(),ios::in);
+				string line;
+				while (!in.eof()) {
+					getline(in, line);
+					fxvec.push_back(strtoul(line.c_str(),NULL,10));
+				}
+			}
+    }
+  } catch (xqp_exception& e) {
+    cout << "Application exception: " << e.what() << '\t' << e.get_msg() << endl;
+  }
+  return 0;
+}
+
