8ffbaba19bdccf33cf1ec572718b1981cde4147d
brantmat@D.ETHZ.CH <>
2007-10-09 11:46:09 +0000

    moved the win32 directory into the src/utils directory

--- win32/win_mobile/errno.cpp
+++
@@ -1,5 +0,0 @@
-
-#include "errno.h"
-
-int		errno = 0;
-
--- win32/win_mobile/errno.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef ERRNO_EMULATION_FOR_WINDOWS_CE_09_AUG_2007
-#define ERRNO_EMULATION_FOR_WINDOWS_CE_09_AUG_2007
-
-extern int		errno;
-
-#endif
