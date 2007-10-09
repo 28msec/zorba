4b38898fdb74ecedf8dd98abe658b5288380b3ea
brantmat@D.ETHZ.CH <>
2007-10-09 11:31:59 +0000

    our own modules used in cmake this contains modules that are not yet available in the cmake distribution they might become obsolete if cmake offers support for them

---
+++ cmake_modules/CMakeUseBison.cmake
@@ -0,0 +1,82 @@
+# - Look for GNU Bison, the parser generator
+# Defines the following:
+#  BISON_EXECUTABLE - path to the bison executable
+#  BISON_FILE - parse a file with bison
+#  BISON_PREFIX_OUTPUTS - Set to true to make BISON_FILE produce prefixed
+#                         symbols in the generated output based on filename.
+#                         So for ${filename}.y, you'll get ${filename}parse(), etc.
+#                         instead of yyparse().
+#  BISON_GENERATE_DEFINES - Set to true to make BISON_FILE output the matching
+#                           .h file for a .c file. You want this if you're using
+#                           flex.
+#  BISON_MAJOR_VERSION    - Returns the Bison major version
+#  BISON_MINOR_VERSION    - Returns the Bison minor version 
+#
+
+
+IF(NOT DEFINED BISON_PREFIX_OUTPUTS)
+  SET(BISON_PREFIX_OUTPUTS FALSE)
+ENDIF(NOT DEFINED BISON_PREFIX_OUTPUTS)
+
+IF(NOT DEFINED BISON_GENERATE_DEFINES)
+  SET(BISON_GENERATE_DEFINES FALSE)
+ENDIF(NOT DEFINED BISON_GENERATE_DEFINES)
+
+IF(NOT BISON_EXECUTABLE)
+  MESSAGE(STATUS "Looking for bison")
+  FIND_PROGRAM(BISON_EXECUTABLE bison)
+  IF(BISON_EXECUTABLE)
+    EXEC_PROGRAM(
+	    	${BISON_EXECUTABLE}  
+        	ARGS --version
+        	OUTPUT_VARIABLE BISON_VERSION)
+
+		# Extract major and minor versions
+        STRING (REGEX REPLACE "[^0-9]*([0-9]+)..*" "\\1" BISON_MAJOR_VERSION_TMP ${BISON_VERSION})
+		STRING (REGEX REPLACE "[^0-9]*[0-9]+\\.([0-9]+).*" "\\1" BISON_MINOR_VERSION_TMP ${BISON_VERSION})
+        MESSAGE(STATUS "Found bison -- ${BISON_EXECUTABLE}, version: " ${BISON_MAJOR_VERSION_TMP} "." ${BISON_MINOR_VERSION_TMP})
+        SET (BISON_MAJOR_VERSION ${BISON_MAJOR_VERSION_TMP} CACHE STRING "The Bison major version")
+        SET (BISON_MINOR_VERSION ${BISON_MINOR_VERSION_TMP} CACHE STRING "The Bison minor version")
+  ELSE (BISON_EXECUTABLE)
+  	SET (BISON_MAJOR_VERSION "0")
+	SET (BISON_MINOR_VERSION "0")
+  ENDIF(BISON_EXECUTABLE)
+ENDIF(NOT BISON_EXECUTABLE)
+
+IF(BISON_EXECUTABLE)
+  MACRO(BISON_FILE FILENAME)
+    GET_FILENAME_COMPONENT(PATH "${FILENAME}" PATH)
+    GET_FILENAME_COMPONENT(HEAD "${FILENAME}" NAME_WE)
+    IF(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/${PATH}")
+      FILE(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${PATH}")
+    ENDIF(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/${PATH}")
+    IF(BISON_PREFIX_OUTPUTS)
+      SET(PREFIX "${HEAD}")
+    ELSE(BISON_PREFIX_OUTPUTS)
+      SET(PREFIX "yy")
+    ENDIF(BISON_PREFIX_OUTPUTS)
+    SET(OUTFILE "${CMAKE_CURRENT_BINARY_DIR}/${PATH}/${HEAD}.cpp")
+    IF(BISON_GENERATE_DEFINES)
+      SET(HEADER "${CMAKE_CURRENT_BINARY_DIR}/${PATH}/${HEAD}.hpp")
+      ADD_CUSTOM_COMMAND(
+        OUTPUT "${OUTFILE}" "${HEADER}"
+        COMMAND "${BISON_EXECUTABLE}"
+        ARGS "--name-prefix=${PREFIX}"
+        "--defines"
+        "--output-file=${OUTFILE}"
+        "${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME}"
+        DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME}")
+      SET_SOURCE_FILES_PROPERTIES("${OUTFILE}" "${HEADER}" PROPERTIES GENERATED TRUE)
+      SET_SOURCE_FILES_PROPERTIES("${HEADER}" PROPERTIES HEADER_FILE_ONLY TRUE)
+    ELSE(BISON_GENERATE_DEFINES)
+      ADD_CUSTOM_COMMAND(
+        OUTPUT "${OUTFILE}"
+        COMMAND "${BISON_EXECUTABLE}"
+        ARGS "--name-prefix=${PREFIX}"
+        "--output-file=${OUTFILE}"
+        "${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME}"
+        DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME}")
+      SET_SOURCE_FILES_PROPERTIES("${OUTFILE}" PROPERTIES GENERATED TRUE)
+    ENDIF(BISON_GENERATE_DEFINES)
+  ENDMACRO(BISON_FILE)
+ENDIF(BISON_EXECUTABLE)
---
+++ cmake_modules/FindICU.cmake
@@ -0,0 +1,90 @@
+# Finds the International Components for Unicode (ICU) Library
+#
+#  ICU_FOUND          - True if ICU found.
+#  ICU_I18N_FOUND     - True if ICU's internationalization library found.
+#  ICU_DATA_FOUND     - True if ICU data library found.
+#
+#  ICU_INCLUDE_DIRS   - Directory to include to get ICU headers
+#                       Note: always include ICU headers as, e.g., 
+#                       unicode/utypes.h
+#  ICU_LIBRARIES      - Libraries to link against for the common ICU
+#  ICU_I18N_LIBRARIES - Libraries to link against for ICU internationaliation
+#                       (note: in addition to ICU_LIBRARIES)
+#  ICU_DATA_LIBRARIES - Libraries to link against for ICU data
+#
+
+# Look for the header file.
+find_path(
+	ICU_INCLUDE_DIR
+	NAMES unicode/utypes.h
+	DOC "Include directory for the ICU library")
+mark_as_advanced(ICU_INCLUDE_DIR)
+
+# Look for the library.
+find_library(
+	ICU_LIBRARY
+	NAMES icuuc cygicuuc cygicuuc32 cygicuuc38
+	DOC "Libraries to link against for the common parts of ICU")
+mark_as_advanced(ICU_LIBRARY)
+
+if (ICU_LIBRARY)
+	MESSAGE("-- Found ICU library -- " ${ICU_LIBRARY})
+else (ICU_LIBRARY)
+	MESSAGE("-- Could not find ICU library")
+endif (ICU_LIBRARY)
+
+
+# Copy the results to the output variables.
+if(ICU_INCLUDE_DIR AND ICU_LIBRARY)
+	set(ICU_FOUND 1)
+	set(ICU_LIBRARIES ${ICU_LIBRARY})
+	set(ICU_INCLUDE_DIRS ${ICU_INCLUDE_DIR})
+
+	# Look for the ICU internationalization libraries
+	find_library(
+		ICU_I18N_LIBRARY
+		NAMES icuin icui18n cygicuin cygicuin32 cygicuin38
+		DOC "Libraries to link against for ICU internationalization")
+	mark_as_advanced(ICU_I18N_LIBRARY)
+	if (ICU_I18N_LIBRARY)
+		MESSAGE("-- Found ICU internationalization library -- " ${ICU_I18N_LIBRARY})
+		set(ICU_I18N_FOUND 1)
+		set(ICU_I18N_LIBRARIES ${ICU_I18N_LIBRARY})
+	else (ICU_I18N_LIBRARY)
+		MESSAGE("-- Could not find ICU internationalization library")
+		set(ICU_I18N_FOUND 0)
+		set(ICU_I18N_LIBRARIES)
+	endif (ICU_I18N_LIBRARY)
+
+	# On cygwin, the data library is called icudt
+	if (CYGWIN)
+		SET (ICU_DATA_NAMES icudt cygicudt cygicudt38)
+	else (CYGWIN)
+		SET (ICU_DATA_NAMES icudata)
+	endif (CYGWIN)
+
+	# Look for ICU data library
+	find_library(
+		ICU_DATA_LIBRARY
+		NAMES ${ICU_DATA_NAMES}
+		DOC "icudata library")
+	mark_as_advanced(ICU_DATA_LIBRARY)
+	if (ICU_DATA_LIBRARY)
+		MESSAGE("-- Found ICU data library -- " ${ICU_DATA_LIBRARY})
+		SET(ICU_DATA_FOUND 1)
+		SET(ICU_DATA_LIBRARIES ${ICU_DATA_LIBRARY})
+	else (ICU_DATA_LIBRARY)
+		MESSAGE("-- Could not find ICU data library")
+		SET(ICU_DATA_FOUND 0)
+		SET(ICU_DATA_LIBRARIES)
+	endif (ICU_DATA_LIBRARY)
+	
+else(ICU_INCLUDE_DIR AND ICU_LIBRARY)
+  set(ICU_FOUND 0)
+  set(ICU_I18N_FOUND 0)
+  set(ICU_DATA_FOUND 0)
+  set(ICU_LIBRARIES)
+  set(ICU_I18N_LIBRARIES)
+  set(ICU_DATA_LIBRARIES)
+  set(ICU_INCLUDE_DIRS)
+endif(ICU_INCLUDE_DIR AND ICU_LIBRARY)
