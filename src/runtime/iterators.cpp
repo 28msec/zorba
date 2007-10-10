a02e387fa08d5ee734da3b85b9565fb30a56b905
markosza@D.ETHZ.CH <>
2007-10-10 07:49:35 +0000

    finished work in iterators.h/cpp files

--- src/runtime/iterators.cpp
+++ src/runtime/iterators.cpp
@@ -1,179 +1,235 @@
-#include "runtime/iterators.h"
+
+#include "runtime/iterators/iterators.h"
+#include "runtime/path_iterators.h"
 
 namespace xqp {
-	/* begin class UnaryBaseIterator */
-	template <class IterType>
-	UnaryBaseIterator::UnaryBaseIterator
-		(const yy::location& loc, Iterator_t& arg)
+
+/* begin class UnaryBaseIterator */
+template <class IterType>
+UnaryBaseIterator<IterType>::UnaryBaseIterator(const yy::location& loc, Iterator_t& arg)
 	:
-		Batcher<IterType>(loc), iter(arg) 
-	{}
+  Batcher<IterType>(loc), theChild(arg) 
+{
+}
+
 	
-	template <class IterType>
-	UnaryBaseIterator<IterType>::~UnaryBaseIterator() {}
+template <class IterType>
+UnaryBaseIterator<IterType>::~UnaryBaseIterator()
+{
+}
+
 	
-	template <class IterType>
-	void 
-	UnaryBaseIterator<IterType>::resetImpl(IteratorTreeStateBlock& stateBlock)
-	{
-		BasicIterator::BasicIteratorState* state;
-		GET_STATE(BasicIterator::BasicIteratorState, state, stateBlock);
-		state->reset();
+template <class IterType>
+void 
+UnaryBaseIterator<IterType>::resetImpl(int8_t* stateBlock)
+{
+  BasicIterator::BasicIteratorState* state;
+  GET_STATE(BasicIterator::BasicIteratorState, state, stateBlock);
+  state->reset();
 		
-		this->resetChild ( this->iter0, stateBlock );
-	}
+  this->resetChild ( theChild, stateBlock );
+}
 
-	template <class IterType>
-	void 
-	UnaryBaseIterator<IterType>::releaseResourcesImpl(IteratorTreeStateBlock& stateBlock)
-	{
-		this->releaseChildResources ( this->iter, stateBlock );
-	}
 
-	template <class IterType>
-	int32_t
-	UnaryBaseIterator<IterType>::getStateSize() {
-		return sizeof(BasicIterator::BasicIteratorState);
-	}
+template <class IterType>
+void 
+UnaryBaseIterator<IterType>::releaseResourcesImpl(int8_t* stateBlock)
+{
+  this->releaseChildResources ( theChild, stateBlock );
+}
+
+
+template <class IterType>
+int32_t
+UnaryBaseIterator<IterType>::getStackSize()
+{
+  return sizeof(BasicIterator::BasicIteratorState);
+}
+
 	
-	template <class IterType>
-	int32_t
-	UnaryBaseIterator<IterType>::getStateSizeOfSubtree() {
-		return this->iter->getStateSizeOfSubtree()
-						+ this->getStateSize();
-	}
+template <class IterType>
+int32_t
+UnaryBaseIterator<IterType>::getStackSizeOfSubtree()
+{
+  return theChild->getStackSizeOfSubtree() + getStackSize();
+}
+
 	
-	template <class IterType>
-	void
-	UnaryBaseIterator<IterType>::setOffset(int32_t& offset) {
-		this->stateOffset = offset;
-		offset += this->getStateSize();
+template <class IterType>
+void
+UnaryBaseIterator<IterType>::setOffset(int32_t& offset)
+{
+  this->stateOffset = offset;
+  offset += getStackSize();
+	
+  theChild->setOffset(offset);
+}
+/* end class UnaryBaseIterator */
+
+	
+/* begin class BinaryBaseIterator */
+template <class IterType>
+BinaryBaseIterator<IterType>::BinaryBaseIterator(
+    const yy::location& loc,
+    Iterator_t& arg0,
+    Iterator_t& arg1 )
+  :
+  Batcher<IterType> ( loc ), theChild0 ( arg0 ), theChild1 ( arg1 ) 
+{
+}
+
+
+template <class IterType>
+BinaryBaseIterator<IterType>::~BinaryBaseIterator()
+{
+}
+
+
+template <class IterType>
+void 
+BinaryBaseIterator<IterType>::resetImpl(int8_t* stateBlock)
+{
+  BasicIterator::BasicIteratorState* state;
+  GET_STATE(BasicIterator::BasicIteratorState, state, stateBlock);
+  state->reset();
 		
-		this->iter->setOffset(offset);
-	}
-	/* end class UnaryBaseIterator */
+  this->resetChild ( theChild0, stateBlock );
+  this->resetChild ( theChild1, stateBlock );
+}
+
+
+template <class IterType>
+void 
+BinaryBaseIterator<IterType>::releaseResourcesImpl(int8_t* stateBlock)
+{
+  this->releaseChildResources ( theChild0, stateBlock );
+  this->releaseChildResources ( theChild1, stateBlock );
+}
+
+
+template <class IterType>
+int32_t
+BinaryBaseIterator<IterType>::getStackSize()
+{
+  return sizeof(BasicIterator::BasicIteratorState);
+}
+
 	
-	/* begin class BinaryBaseIterator */
-	template <class IterType>
-	BinaryBaseIterator<IterType>::BinaryBaseIterator
-		( const yy::location& loc, Iterator_t& arg0, Iterator_t& arg1 )
-	:
-		Batcher<IterType> ( loc ),iter0 ( arg0 ),iter1 ( arg1 ) 
-	{}
+template <class IterType>
+int32_t
+BinaryBaseIterator<IterType>::getStackSizeOfSubtree()
+{
+  return theChild0->getStackSizeOfSubtree()
+       + theChild1->getStackSizeOfSubtree()
+       + this->getStackSize();
+}
 
-	template <class IterType>
-	BinaryBaseIterator<IterType>::~BinaryBaseIterator() {}
-
-	template <class IterType>
-	void 
-	BinaryBaseIterator<IterType>::resetImpl(IteratorTreeStateBlock& stateBlock)
-	{
-		BasicIterator::BasicIteratorState* state;
-		GET_STATE(BasicIterator::BasicIteratorState, state, stateBlock);
-		state->reset();
+	
+template <class IterType>
+void
+BinaryBaseIterator<IterType>::setOffset(int32_t& offset)
+{
+  this->stateOffset = offset;
+  offset += this->getStackSize();
 		
-		this->resetChild ( this->iter0, stateBlock );
-		this->resetChild ( this->iter1, stateBlock );
-	}
+  theChild0->setOffset(offset);
+  theChild1->setOffset(offset);
+}
+/* end class BinaryBaseIterator */
 
-	template <class IterType>
-	void 
-	BinaryBaseIterator<IterType>::releaseResourcesImpl(IteratorTreeStateBlock& stateBlock)
-	{
-		this->releaseChildResources ( this->iter0, stateBlock );
-		this->releaseChildResources ( this->iter1, stateBlock );
-	}
+	
+/* begin class NaryBaseIterator */
+template <class IterType>
+NaryBaseIterator<IterType>::NaryBaseIterator(
+    const yy::location& loc,
+    std::vector<Iterator_t>& args)
+  :
+  Batcher<IterType> ( loc ), theChildren ( args )
+{
+}
 
-	template <class IterType>
-	int32_t
-	BinaryBaseIterator<IterType>::getStateSize() {
-		return sizeof(BasicIterator::BasicIteratorState);
-	}
-	
-	template <class IterType>
-	int32_t
-	BinaryBaseIterator<IterType>::getStateSizeOfSubtree() {
-		return this->iter0->getStateSizeOfSubtree()
-						+ this->iter1->getStateSizeOfSubtree()
-						+ this->getStateSize();
-	}
-	
-	template <class IterType>
-	void
-	BinaryBaseIterator<IterType>::setOffset(int32_t& offset) {
-		this->stateOffset = offset;
-		offset += this->getStateSize();
+
+template <class IterType>
+NaryBaseIterator<IterType>::~NaryBaseIterator()
+{
+}
+
+
+template <class IterType>
+void 
+NaryBaseIterator<IterType>::resetImpl(int8_t* stateBlock)
+{
+  BasicIterator::BasicIteratorState* state;
+  GET_STATE(BasicIterator::BasicIteratorState, state, stateBlock);
+  state->reset();
 		
-		this->iter0->setOffset(offset);
-		this->iter1->setOffset(offset);
-	}
-	/* end class BinaryBaseIterator */
-	
-	/* begin class NaryBaseIterator */
-	template <class IterType>
-	NaryBaseIterator<IterType>::NaryBaseIterator
-		( const yy::location& loc, std::vector<Iterator_t>& args)
-	:
-		Batcher<IterType> ( loc ),iters ( args )
-	{}
+  std::vector<Iterator_t>::iterator iter = theChildren.begin();
+  for(; iter != theChildren.end(); ++iter) {
+    this->resetChild(*iter, stateBlock);
+  }
+}
 
-	template <class IterType>
-	NaryBaseIterator<IterType>::~NaryBaseIterator() {}
 
-	template <class IterType>
-	void 
-	NaryBaseIterator<IterType>::resetImpl(IteratorTreeStateBlock& stateBlock)
-	{
-		BasicIterator::BasicIteratorState* state;
-		GET_STATE(BasicIterator::BasicIteratorState, state, stateBlock);
-		state->reset();
-		
-		std::vector<Iterator_t>::iterator iter = this->iters.begin();
-		for(; iter != this->iters.end(); ++iter) {
-			this->resetChild(*iter, stateBlock);
-		}
-	}
+template <class IterType>
+void 
+NaryBaseIterator<IterType>::releaseResourcesImpl(int8_t* stateBlock)
+{
+  std::vector<Iterator_t>::iterator iter = theChildren.begin();
+  for(; iter != theChildren.end(); ++iter) {
+    this->releaseChildResources(*iter, stateBlock);
+  }
+}
 
-	template <class IterType>
-	void 
-	BinaryBaseIterator<IterType>::releaseResourcesImpl(IteratorTreeStateBlock& stateBlock)
-	{
-		std::vector<Iterator_t>::iterator iter = this->iters.begin();
-		for(; iter != this->iters.end(); ++iter) {
-			this->releaseChildResources(*iter, stateBlock);
-		}
-	}
+  
+template <class IterType>
+int32_t
+NaryBaseIterator<IterType>::getStackSize()
+{
+  return sizeof(BasicIterator::BasicIteratorState);
+}
 
-	template <class IterType>
-	int32_t
-	BinaryBaseIterator<IterType>::getStateSize() {
-		return sizeof(BasicIterator::BasicIteratorState);
-	}
 	
-	template <class IterType>
-	int32_t
-	BinaryBaseIterator<IterType>::getStateSizeOfSubtree() {
-		int32_t size = 0;
+template <class IterType>
+int32_t
+NaryBaseIterator<IterType>::getStackSizeOfSubtree()
+{
+  int32_t size = 0;
 	
-		std::vector<Iterator_t>::const_iterator iter = this->argv.begin();
-		for(; iter != this->iters.end(); ++iter) {
-			size += (*iter)->getStateSizeOfSubtree();
-		}
-		
-		return this->getStateSize() + size;
-	}
+  std::vector<Iterator_t>::const_iterator iter = theChildren.begin();
+  for(; iter != theChildren.end(); ++iter) {
+    size += (*iter)->getStackSizeOfSubtree();
+  }
 	
-	template <class IterType>
-	void
-	BinaryBaseIterator<IterType>::setOffset(int32_t& offset) {
-		this->stateOffset = offset;
-		offset += this->getStateSize();
-		
-		std::vector<Iterator_t>::iterator iter = this->argv.begin();
-		for(; iter != this->iters.end(); ++iter) {
-			(*iter)->setOffset(offset);
-		}
-	}
-	/* end class NaryBaseIterator */
+  return this->getStackSize() + size;
+}
+
+	
+template <class IterType>
+void
+NaryBaseIterator<IterType>::setOffset(int32_t& offset)
+{
+  this->stateOffset = offset;
+  offset += this->getStackSize();
+	
+  std::vector<Iterator_t>::iterator iter = theChildren.begin();
+  for(; iter != theChildren.end(); ++iter) {
+    (*iter)->setOffset(offset);
+  }
+}
+/* end class NaryBaseIterator */
+
+
+template class AxisIterator<SelfAxisIterator>;
+template class AxisIterator<AttributeAxisIterator>;
+template class AxisIterator<ChildAxisIterator>;
+template class AxisIterator<ParentAxisIterator>;
+template class AxisIterator<DescendantAxisIterator>;
+template class AxisIterator<DescendantSelfAxisIterator>;
+template class AxisIterator<AncestorAxisIterator>;
+template class AxisIterator<AncestorSelfAxisIterator>;
+template class AxisIterator<FollowingAxisIterator>;
+template class AxisIterator<PrecedingAxisIterator>;
+template class AxisIterator<RSiblingAxisIterator>;
+template class AxisIterator<LSiblingAxisIterator>;
 	
 } /* namespace xqp */
--- src/runtime/iterators.h
+++ src/runtime/iterators.h
@@ -1,65 +1,309 @@
+/**
+ *
+ * @copyright
+ * ========================================================================
+ *	Copyright 2007 FLWOR Foundation
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *	
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *	
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ * ========================================================================
+ *
+ * @file runtime/iterators/iterators.h
+ *
+ */
+
 #ifndef XQP_ITERATORS_H
 #define XQP_ITERATORS_H
 
+#include "runtime/batching.h"
+
+
 namespace xqp {
 	
-	template <class IterType>
-	class UnaryBaseIterator : public Batcher<IterType>
-	{
-		protected:
-			Iterator_t iter;
+template <class IterType>
+class UnaryBaseIterator : public Batcher<IterType>
+{
+ protected:
+  Iterator_t theChild;
 		
-		public:
-			UnaryBaseIterator
-				( const yy::location& loc, Iterator_t& arg);
-			virtual ~UnaryBaseIterator();
+ public:
+  UnaryBaseIterator(const yy::location& loc, Iterator_t& arg);
+  virtual ~UnaryBaseIterator();
 
-			void resetImpl(IteratorTreeStateBlock& stateBlock);
-			void releaseResourcesImpl(IteratorTreeStateBlock& stateBlock);
+  void resetImpl(int8_t* stateBlock);
+  void releaseResourcesImpl(int8_t* stateBlock);
 			
-			virtual int32_t getStateSize();
-			virtual int32_t getStateSizeOfSubtree();
-			virtual void setOffset(int32_t& offset);
-	}; /* class UnaryBaseIterator */
+  virtual int32_t getStackSize();
+  virtual int32_t getStackSizeOfSubtree();
+  virtual void setOffset(int32_t& offset);
+}; /* class UnaryBaseIterator */
+
 	
-	template <class IterType>
-	class BinaryBaseIterator : public Batcher<IterType>
-	{
-		protected:
-			Iterator_t iter0;
-			Iterator_t iter1;
+template <class IterType>
+class BinaryBaseIterator : public Batcher<IterType>
+{
+ protected:
+  Iterator_t theChild0;
+  Iterator_t theChild1;
 			
-		public:
-			BinaryBaseIterator
-				( const yy::location& loc, Iterator_t& arg0, Iterator_t& arg1 );
-			virtual ~BinaryBaseIterator();
+ public:
+  BinaryBaseIterator(const yy::location& loc, Iterator_t& arg0, Iterator_t& arg1);
+  virtual ~BinaryBaseIterator();
 
-			void resetImpl(IteratorTreeStateBlock& stateBlock);
-			void releaseResourcesImpl(IteratorTreeStateBlock& stateBlock);
+  void resetImpl(int8_t* stateBlock);
+  void releaseResourcesImpl(int8_t* stateBlock);
 			
-			virtual int32_t getStateSize();
-			virtual int32_t getStateSizeOfSubtree();
-			virtual void setOffset(int32_t& offset);
-	}; /* class BinaryBaseIterator */
+  virtual int32_t getStackSize();
+  virtual int32_t getStackSizeOfSubtree();
+  virtual void setOffset(int32_t& offset);
+}; /* class BinaryBaseIterator */
 	
-	template <class IterType>
-	class NaryBaseIterator : public Batcher<IterType>
-	{
-		protected:
-			std::vector<Iterator_t> iters;
-			
-		public:
-			NaryBaseIterator
-				( const yy::location loc, Iterator_t arg0, Iterator_t arg1 );
-			virtual ~NaryBaseIterator();
 
-			void resetImpl(IteratorTreeStateBlock& stateBlock);
-			void releaseResourcesImpl(IteratorTreeStateBlock& stateBlock);
+template <class IterType>
+class NaryBaseIterator : public Batcher<IterType>
+{
+ protected:
+  std::vector<Iterator_t> theChildren;
 			
-			virtual int32_t getStateSize();
-			virtual int32_t getStateSizeOfSubtree();
-			virtual void setOffset(int32_t& offset);
-	}; /* class BinaryBaseIterator */
+ public:
+  NaryBaseIterator(const yy::location& loc, std::vector<Iterator_t>& args);
+  virtual ~NaryBaseIterator();
+
+  void resetImpl(int8_t* stateBlock);
+  void releaseResourcesImpl(int8_t* stateBlock);
+			
+  virtual int32_t getStackSize();
+  virtual int32_t getStackSizeOfSubtree();
+  virtual void setOffset(int32_t& offset);
+}; /* class BinaryBaseIterator */
+
+
+
+/* begin class UnaryBaseIterator */
+template <class IterType>
+UnaryBaseIterator<IterType>::UnaryBaseIterator(const yy::location& loc, Iterator_t& arg)
+	:
+  Batcher<IterType>(loc), theChild(arg) 
+{
+}
+
+	
+template <class IterType>
+UnaryBaseIterator<IterType>::~UnaryBaseIterator()
+{
+}
+
+	
+template <class IterType>
+void 
+UnaryBaseIterator<IterType>::resetImpl(int8_t* stateBlock)
+{
+  BasicIterator::BasicIteratorState* state;
+  GET_STATE(BasicIterator::BasicIteratorState, state, stateBlock);
+  state->reset();
+		
+  this->resetChild ( theChild, stateBlock );
+}
+
+
+template <class IterType>
+void 
+UnaryBaseIterator<IterType>::releaseResourcesImpl(int8_t* stateBlock)
+{
+  this->releaseChildResources ( theChild, stateBlock );
+}
+
+
+template <class IterType>
+int32_t
+UnaryBaseIterator<IterType>::getStackSize()
+{
+  return sizeof(BasicIterator::BasicIteratorState);
+}
+
+	
+template <class IterType>
+int32_t
+UnaryBaseIterator<IterType>::getStackSizeOfSubtree()
+{
+  return theChild->getStackSizeOfSubtree() + getStackSize();
+}
+
+	
+template <class IterType>
+void
+UnaryBaseIterator<IterType>::setOffset(int32_t& offset)
+{
+  this->stateOffset = offset;
+  offset += getStackSize();
+	
+  theChild->setOffset(offset);
+}
+/* end class UnaryBaseIterator */
+
+	
+/* begin class BinaryBaseIterator */
+template <class IterType>
+BinaryBaseIterator<IterType>::BinaryBaseIterator(
+    const yy::location& loc,
+    Iterator_t& arg0,
+    Iterator_t& arg1 )
+  :
+  Batcher<IterType> ( loc ), theChild0 ( arg0 ), theChild1 ( arg1 ) 
+{
+}
+
+
+template <class IterType>
+BinaryBaseIterator<IterType>::~BinaryBaseIterator()
+{
+}
+
+
+template <class IterType>
+void 
+BinaryBaseIterator<IterType>::resetImpl(int8_t* stateBlock)
+{
+  BasicIterator::BasicIteratorState* state;
+  GET_STATE(BasicIterator::BasicIteratorState, state, stateBlock);
+  state->reset();
+		
+  this->resetChild ( theChild0, stateBlock );
+  this->resetChild ( theChild1, stateBlock );
+}
+
+
+template <class IterType>
+void 
+BinaryBaseIterator<IterType>::releaseResourcesImpl(int8_t* stateBlock)
+{
+  this->releaseChildResources ( theChild0, stateBlock );
+  this->releaseChildResources ( theChild1, stateBlock );
+}
+
+
+template <class IterType>
+int32_t
+BinaryBaseIterator<IterType>::getStackSize()
+{
+  return sizeof(BasicIterator::BasicIteratorState);
+}
+
+	
+template <class IterType>
+int32_t
+BinaryBaseIterator<IterType>::getStackSizeOfSubtree()
+{
+  return theChild0->getStackSizeOfSubtree()
+       + theChild1->getStackSizeOfSubtree()
+       + this->getStackSize();
+}
+
+	
+template <class IterType>
+void
+BinaryBaseIterator<IterType>::setOffset(int32_t& offset)
+{
+  this->stateOffset = offset;
+  offset += this->getStackSize();
+		
+  theChild0->setOffset(offset);
+  theChild1->setOffset(offset);
+}
+/* end class BinaryBaseIterator */
+
+	
+/* begin class NaryBaseIterator */
+template <class IterType>
+NaryBaseIterator<IterType>::NaryBaseIterator(
+    const yy::location& loc,
+    std::vector<Iterator_t>& args)
+  :
+  Batcher<IterType> ( loc ), theChildren ( args )
+{
+}
+
+
+template <class IterType>
+NaryBaseIterator<IterType>::~NaryBaseIterator()
+{
+}
+
+
+template <class IterType>
+void 
+NaryBaseIterator<IterType>::resetImpl(int8_t* stateBlock)
+{
+  BasicIterator::BasicIteratorState* state;
+  GET_STATE(BasicIterator::BasicIteratorState, state, stateBlock);
+  state->reset();
+		
+  std::vector<Iterator_t>::iterator iter = theChildren.begin();
+  for(; iter != theChildren.end(); ++iter) {
+    this->resetChild(*iter, stateBlock);
+  }
+}
+
+
+template <class IterType>
+void 
+NaryBaseIterator<IterType>::releaseResourcesImpl(int8_t* stateBlock)
+{
+  std::vector<Iterator_t>::iterator iter = theChildren.begin();
+  for(; iter != theChildren.end(); ++iter) {
+    this->releaseChildResources(*iter, stateBlock);
+  }
+}
+
+  
+template <class IterType>
+int32_t
+NaryBaseIterator<IterType>::getStackSize()
+{
+  return sizeof(BasicIterator::BasicIteratorState);
+}
+
+	
+template <class IterType>
+int32_t
+NaryBaseIterator<IterType>::getStackSizeOfSubtree()
+{
+  int32_t size = 0;
+	
+  std::vector<Iterator_t>::const_iterator iter = theChildren.begin();
+  for(; iter != theChildren.end(); ++iter) {
+    size += (*iter)->getStackSizeOfSubtree();
+  }
+	
+  return this->getStackSize() + size;
+}
+
+	
+template <class IterType>
+void
+NaryBaseIterator<IterType>::setOffset(int32_t& offset)
+{
+  this->stateOffset = offset;
+  offset += this->getStackSize();
+	
+  std::vector<Iterator_t>::iterator iter = theChildren.begin();
+  for(; iter != theChildren.end(); ++iter) {
+    (*iter)->setOffset(offset);
+  }
+}
+/* end class NaryBaseIterator */
+
+
 } /* namespace xqp */
+
 
 #endif	/* XQP_ITERATORS_H */
