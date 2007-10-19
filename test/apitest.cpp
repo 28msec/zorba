5bb62c9d0a223f1a6f13b054f243a3a62e14a9f5
daturca@D.ETHZ.CH <>
2007-10-19 08:22:38 +0000

    The executable for testing the API.

---
+++ test/apitest.cpp
@@ -0,0 +1,46 @@
+
+
+////using the C++ API
+
+#include "zorba_api/zorba_api.h"
+#include "store/native/basic_item_factory.h"
+
+#ifndef _WIN32_WCE
+int main(int argc, char* argv[])
+#else
+int _tmain(int argc, _TCHAR* argv[])
+#endif
+{
+	BasicItemFactory basicItemFactory;
+	SimpleStore simpleStore;
+	ZorbaFactory	zorba_factory(&basicItemFactory);
+	
+
+	///thread specific
+
+	zorba_factory.InitThread(&basicItemFactory);
+
+
+
+	XQuery*		query;
+	query = zorba_factory.CreateQuery("1+1");
+
+	query->compile();
+
+	XQueryResult		*result;
+
+	result = query->execute();
+
+	Item_t		it;
+
+	it = result->next();
+
+	cout << it->show() << endl;
+
+	delete result;
+	delete query;
+
+	zorba_factory.UninitThread();
+
+}
+
