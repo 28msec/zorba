7a3a0625d1eb621a8d1fb7209b2fb9f8b9a64ea9
markosza@D.ETHZ.CH <>
2007-10-31 21:27:02 +0000

    use Iterator instead of PlanIter

--- src/store/naive/simple_collection.h
+++ src/store/naive/simple_collection.h
@@ -7,23 +7,34 @@
 #ifndef XQP_SIMPLE_COLLECTION_H
 #define XQP_SIMPLE_COLLECTION_H
 
+#include <vector>
 #include "store/api/collection.h"
 
 namespace xqp
 {
-	class SimpleCollection : public Collection
-	{
-		public:
-			SimpleCollection();
-			virtual ~SimpleCollection();
+template <class Object> class rchandle;
 
-			virtual PlanIter_t getIterator ( bool isNeeded );
-			virtual void addToCollection(Item_t item, int32_t position = -1);
-			virtual void addToCollection(PlanIter_t& items, int32_t position = -1);
-			virtual void addToCollection(std::iostream& stream, int32_t position = -1);
-			virtual void deleteFromCollection ( int32_t position );
-			virtual Item_t getURI();
-	}; /* class SimpleCollection */
+class NodeItem;
+typedef rchandle<class NodeItem> Node_t;
+
+
+class SimpleCollection : public Collection
+{
+protected:
+  std::vector<Node_t>  theNodes;
+
+public:
+  SimpleCollection();
+  virtual ~SimpleCollection();
+
+  virtual Iterator_t getIterator ( bool isNeeded );
+  virtual void addToCollection(Item_t item, int32_t position = -1);
+  virtual void addToCollection(Iterator_t& items, int32_t position = -1);
+  virtual void addToCollection(std::iostream& stream, int32_t position = -1);
+  virtual void deleteFromCollection ( int32_t position );
+  virtual Item_t getURI();
+}; /* class SimpleCollection */
+
 } /* namespace xqp */
 
 #endif /* XQP_SIMPLE_COLLECTION_H */
