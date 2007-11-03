e498e5abea0d7b10202a9bc2b4ce7d687572e530
markosza@D.ETHZ.CH <>
2007-11-03 14:30:01 +0000

    ItemFactory belongs to the Store

--- src/store/naive/simple_store.h
+++ src/store/naive/simple_store.h
@@ -8,6 +8,7 @@
 #define XQP_SIMPLE_STORE_H
 
 #include "store/api/store.h"
+#include "store/naive/basic_item_factory.h"
 
 namespace xqp
 {
@@ -24,25 +25,33 @@ class Requester;
 
 class SimpleStore : public Store
 {
+protected:
+  BasicItemFactory  theItemFactory;
+
 public:
   SimpleStore();
   virtual ~SimpleStore();
 
-  virtual TempSeq_t createTempSeq ( Iterator_t&, bool lazy = true );
-  virtual void setGarbageCollectionStrategy ( const xqp_string& garbageCollectionStrategy );
-  virtual void apply ( PUL_t pendingUpdateList );
-  virtual void apply ( PUL_t pendingUpdateList, Requester requester );
-  virtual Item_t getReference ( Item_t );
-  virtual Item_t getFixedReference ( Item_t, Requester requester, Timetravel timetravel );
-  virtual Item_t getNodeByReference ( Item_t );
-  virtual Item_t getNodeByReference ( Item_t, Requester requester, Timetravel timetravel );
-  virtual int32_t compare ( Item_t item1, Item_t item2 );
-  virtual Iterator_t sort ( Iterator_t iterator, bool ascendent, bool duplicateElemination );
-  virtual Iterator_t distinctNodeStable ( Iterator_t );
-  virtual Collection_t getCollection ( Item_t uri );
-  virtual Collection_t createCollection ( Item_t uri );
+  virtual ItemFactory& getItemFactory() { return theItemFactory; }
+
+  virtual TempSeq_t createTempSeq(Iterator_t&, bool lazy = true);
+  virtual void setGarbageCollectionStrategy(const xqp_string& strategy);
+  virtual void apply(PUL_t pendingUpdateList);
+  virtual void apply(PUL_t pendingUpdateList, Requester requester);
+
+  virtual Item_t getReference(Item_t);
+  virtual Item_t getFixedReference(Item_t, Requester requester, Timetravel timetravel);
+  virtual Item_t getNodeByReference(Item_t);
+  virtual Item_t getNodeByReference(Item_t, Requester requester, Timetravel timetravel);
+
+  virtual int32_t compare(Item_t item1, Item_t item2);
+  virtual Iterator_t sort(Iterator_t iterator, bool ascendent, bool duplicateElemination);
+  virtual Iterator_t distinctNodeStable(Iterator_t);
+
+  virtual Collection_t getCollection(Item_t uri);
+  virtual Collection_t createCollection(Item_t uri);
   virtual Collection_t createCollection();
-  virtual void deleteCollection ( Item_t uri );
+  virtual void deleteCollection(Item_t uri);
   virtual Item_t createURI();
   
 }; /* class SimpleStore */
