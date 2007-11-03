cb45fdb72dbd9cdd57333ccd40a413baba02cc1f
markosza@D.ETHZ.CH <>
2007-11-03 14:30:41 +0000

    timing

--- src/store/naive/test_qname_pool.cpp
+++ src/store/naive/test_qname_pool.cpp
@@ -1,11 +1,26 @@
 
+#include <iostream>
+#include <sys/time.h>
+
 #include "store/naive/qname_pool.h"
 
 int main(int argc, const char * argv[])
 {
-  xqp::QNamePool* pool = new xqp::QNamePool(16);
+  double startTime;
+  double endTime;
+  struct timeval stime;
+  struct timeval etime;
+
+  gettimeofday(&stime, NULL);
+  startTime=(double)stime.tv_sec+(1.e-6)*stime.tv_usec;
+
+  xqp::QNamePool* pool = new xqp::QNamePool(xqp::QNamePool::MAX_CACHE_SIZE);
 
   xqp::QNameImpl::setQNamePool(pool);
+
+  gettimeofday(&etime, NULL);
+  endTime=(double)etime.tv_sec+(1.e-6)*etime.tv_usec;
+  std::cout << "construction time : " << (double)(endTime-startTime) << std::endl;
 
   xqp::QNameImpl* qn = NULL;
 
@@ -13,7 +28,14 @@ int main(int argc, const char * argv[])
 
   qn->free();
 
+  gettimeofday(&stime, NULL);
+  startTime=(double)stime.tv_sec+(1.e-6)*stime.tv_usec;
+
   delete pool;
+
+  gettimeofday(&etime, NULL);
+  endTime=(double)etime.tv_sec+(1.e-6)*etime.tv_usec;
+  std::cout << "destruction time : " << (double)(endTime-startTime) << std::endl;
 
   return 0;
 }
