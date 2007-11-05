2896d76f479e54db97c217c194c9b29d2e09dba3
dagraf@D.ETHZ.CH <>
2007-11-05 08:01:12 +0000

    warning in FLWOR corrected

--- src/runtime/core/item_iterator.cpp
+++ src/runtime/core/item_iterator.cpp
@@ -297,7 +297,7 @@ Item_t FLWORIterator::nextImpl(PlanState& planState) {
 	Item_t curItem;	
 	STACK_INIT2(PlanIteratorState, state, planState);
 	varBindingState = new int[forLetClauses.size()];
-	for (int i = 0; i < forLetClauses.size(); i++) {
+	for (uint i = 0; i < forLetClauses.size(); i++) {
 		varBindingState[i] = 0;
 	}
 	while (true) {
