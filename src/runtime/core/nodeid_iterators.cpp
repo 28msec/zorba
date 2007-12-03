5ae48f08d64d90a175bb2dc7304f5364ca269096
markosza@D.ETHZ.CH <>
2007-12-03 10:38:16 +0000

    duplicate node elimination

---
+++ src/runtime/core/nodeid_iterators.cpp
@@ -0,0 +1,57 @@
+
+#include "runtime/core/nodeid_iterators.h"
+
+namespace xqp 
+{
+
+/*******************************************************************************
+
+********************************************************************************/
+Item_t NodeDistinctIterator::nextImpl(PlanState& planState)
+{
+  Item_t contextNode;
+
+  NodeDistinctState* state;
+  GET_STATE(NodeDistinctState, state, planState);
+
+  while (true)
+  {
+    contextNode = consumeNext(theChild, planState);
+    if (contextNode == NULL)
+      return NULL;
+
+    if (state->theHashSet.insert(contextNode).second)
+      return contextNode;
+  }
+}
+
+
+void NodeDistinctIterator::resetImpl(PlanState& planState)
+{
+  UnaryBaseIterator<NodeDistinctIterator>::reset(planState);
+
+  NodeDistinctState* state;
+  GET_STATE(NodeDistinctState, state, planState);
+  state->theHashSet.clear();
+}
+
+
+void NodeDistinctIterator::releaseResourcesImpl(PlanState& planState)
+{
+  UnaryBaseIterator<NodeDistinctIterator>::releaseResourcesImpl(planState);
+
+  NodeDistinctState* state;
+  GET_STATE(NodeDistinctState, state, planState);
+  state->theHashSet.clear();
+}
+
+
+void NodeDistinctIterator::setOffset(PlanState& planState, int32_t& offset)
+{
+  UnaryBaseIterator<NodeDistinctIterator>::setOffset(planState, offset);
+
+  NodeDistinctState* state = new (planState.block + stateOffset) NodeDistinctState;
+}
+
+
+}
---
+++ src/runtime/core/nodeid_iterators.h
@@ -0,0 +1,104 @@
+#ifndef XQP_NODEID_ITERATORS_H
+#define XQP_NODEID_ITERATORS_H
+
+
+#include "util/rchandle.h"
+#include "util/logging/logging.hh"
+#include "compiler/expression/expr_consts.h"
+#include "runtime/base/iterator.h"
+#include "runtime/base/unarybase.h"
+#include "store/api/store_consts.h"
+
+#include <ext/hash_set>
+namespace std {
+using __gnu_cxx::hash;
+using __gnu_cxx::hashtable;
+using __gnu_cxx::hash_set;
+using __gnu_cxx::hash_multiset;
+}
+
+
+namespace xqp 
+{
+
+/*******************************************************************************
+
+********************************************************************************/
+class NodeDistinctIterator : public UnaryBaseIterator<NodeDistinctIterator>
+{
+ protected:
+  class NodeDistinctHash
+  {
+  public:
+    size_t operator()(const Item_t& node) const
+    {
+      std::hash<unsigned long> hashfun;
+      return hashfun((unsigned long)(node.get_ptr()));
+    }
+  };
+
+  typedef std::hash_set<Item_t, NodeDistinctHash>  NodeDistinctHashSet;
+
+  class NodeDistinctState : public PlanIterator::PlanIteratorState
+  {
+  public:
+    NodeDistinctHashSet  theHashSet;
+  };
+
+
+public:
+  NodeDistinctIterator(
+        const yy::location& loc,
+        PlanIter_t input)
+    :
+    UnaryBaseIterator<NodeDistinctIterator>(loc, input)
+  {
+  }
+
+  ~NodeDistinctIterator() { }
+
+  Item_t nextImpl(PlanState& planState);
+  void resetImpl(PlanState& planState);
+  void releaseResourcesImpl(PlanState& planState);
+
+  int32_t getStateSize() { return sizeof(NodeDistinctState); }
+
+  void setOffset(PlanState& planState, int32_t& offset);
+
+  virtual void accept(PlanIterVisitor&) const;
+};
+
+#if 0
+/*******************************************************************************
+
+********************************************************************************/
+class NodeSortIterator : public UnaryBaseIterator<NodeSortIterator>
+{
+ protected:
+
+ public:
+  NodeSortIterator(
+        const yy::location& loc,
+        PlanIter_t input)
+    :
+    UnaryBaseIterator<NodeSortIterator>(loc, input)
+  {
+  }
+
+  ~NodeSortIterator() { }
+
+  Item_t nextImpl(PlanState& planState);
+  void resetImpl(PlanState& planState);
+  void releaseResourcesImpl(PlanState& planState);
+
+  int32_t getStateSize() { return sizeof(NodeSortState); }
+
+  void setOffset(PlanState& planState, int32_t& offset);
+
+  virtual void accept(PlanIterVisitor&) const;
+};
+#endif
+
+}
+
+#endif
