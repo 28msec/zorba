e0c89e19b3cd81d85abf82082b2f6248b913ec19
brantmat@D.ETHZ.CH <>
2007-12-07 14:00:22 +0000

    configuration mechanism to specify the configuration zorba should be build with. see CMakeConfiguration.txt for a brief documentation

--- include/zorba/config.h.cmake
+++ include/zorba/config.h.cmake
@@ -1,3 +1,19 @@
+#ifndef ZORBA_CONFIG_H
+#define ZORBA_CONFIG_H
+
+/* This header is configured by Zorba's build process.  */
+
+/* Zorba Configuration */
+
+/* general zorba configuration parameters */
+
+
+/* zorba runtime configuration parameters */
+#define ZORBA_BATCHING_TYPE    ${ZORBA_BATCHING_TYPE}
+#define ZORBA_BATCHING_BATCHSIZE    ${ZORBA_BATCHING_BATCHSIZE}
+
+
+/* Platform checks */
 #cmakedefine HAVE_SYS_MOUNT_H       1
 #cmakedefine HAVE_UTYPES_H          1
 #cmakedefine HAVE_COLL_H            1
@@ -9,3 +25,6 @@
 #cmakedefine HAVE_LIMITS_H          1
 #cmakedefine HAVE_SYS_TYPES_H       1
 #cmakedefine HAVE_ICONV_H           1
+
+
+#endif
