210cd7fd60cc162286d6f3c1b3ac3a246606c8b1
dmuresa@D.ETHZ.CH <>
2007-12-18 18:12:44 +0000

    A bit of STL magic (stacks -> generators adaptor)

---
+++ src/util/stl_extra.h
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+namespace xqp {
+
+  template <typename adaptable_unary> class binderonly {
+  public:
+    typedef typename adaptable_unary::argument_type argument_type;
+    typedef typename adaptable_unary::result_type result_type;
+  private:
+    argument_type &arg;
+    adaptable_unary &f;
+
+  public:
+    binderonly (adaptable_unary &f_, argument_type &arg_)
+      : f (f_), arg (arg_)
+    {}
+    result_type operator () () {
+      return f (arg);
+    }
+  };
+  
+  template <typename adaptable_unary> binderonly<adaptable_unary> bindonly (adaptable_unary &f, typename adaptable_unary::argument_type &arg) {
+    return binderonly<adaptable_unary> (f, arg);
+  }
+
+  template <typename T> class stack_generator {
+    stack<T> &stk;
+  public:
+    stack_generator (stack<T> &stk_) : stk (stk_) {}
+    T operator () () {
+      T x = stk.top ();
+      stk.pop ();
+      return x;
+    }
+  };
+
+  template <typename T> stack_generator<T> stack_to_generator (stack<T> &stk) {
+    return stack_generator<T> (stk);
+  }
+
+  // Not officially in namespace std, so we put it in xqp
+  template <class InputIterator, class Size, class OutputIterator>
+  OutputIterator copy_n(InputIterator first, Size count, 
+                        OutputIterator result) {
+    for (Size n = 0; n < count; n++, *result++ = *first++);
+    return result;
+  }
+
+};
+  
+/*
+ * Local variables:
+ * mode: c++
+ * End:
+ */
