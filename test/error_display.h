c367fab4ba23e53adb0ad5d824782ecfb292f042
dmuresa@D.ETHZ.CH <>
2008-01-22 12:16:52 +0000

    Fixed useless forward declarations by including proper header file

--- test/error_display.h
+++ test/error_display.h
@@ -1,14 +1,7 @@
+#include "errors/errors.h"
 
 #ifndef ERROR_DISPLAY_H
 #define ERROR_DISPLAY_H
-
-struct xqp::ZorbaAlert;
-struct xqp::ZorbaError;
-struct xqp::ZorbaWarning;
-struct xqp::ZorbaNotify;
-struct xqp::ZorbaAskUser;
-struct xqp::ZorbaFnError;
-struct xqp::ZorbaFnTrace;
 
 extern void DisplayErrorListForCurrentThread();
 
@@ -16,7 +9,7 @@ extern void DisplayOneAlert(const xqp::ZorbaAlert *alert);
 extern void DisplayError(const xqp::ZorbaError *err);
 extern void DisplayWarning(const xqp::ZorbaWarning *warn);
 extern void DisplayNotification(const xqp::ZorbaNotify *notif);
-extern int DisplayAskUser(const xqp::ZorbaAskUser *askuser);
+extern int  DisplayAskUser(const xqp::ZorbaAskUser *askuser);
 extern void DisplayFnUserError(const xqp::ZorbaFnError *fn_err);
 extern void DisplayFnUserTrace(const xqp::ZorbaFnTrace *fn_trace);
 
