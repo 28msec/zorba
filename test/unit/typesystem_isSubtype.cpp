b451589b85324f2ee793a7d57374b644f84c0e8c
brantmat@D.ETHZ.CH <>
2008-02-08 13:26:14 +0000

    memory fixes and suppressions

--- test/unit/typesystem_isSubtype.cpp
+++ test/unit/typesystem_isSubtype.cpp
@@ -9,19 +9,24 @@
 using namespace xqp;
 
 int typesystem_isSubtype(int argc, char* argv[]) {
-  Store& store = Store::getInstance();
-  Item_t lInteger = store.getItemFactory().createInteger((int32_t)1);
-  Item_t lInt = store.getItemFactory().createInt(1);
-  Item_t lDecimal = store.getItemFactory().createDecimal((int32_t)1);
-  
-  TypeSystem::xqtref_t lIntegerType = GENV_TYPESYSTEM.create_type(lInteger->getType(), TypeSystem::QUANT_ONE);
-  TypeSystem::xqtref_t lIntType = GENV_TYPESYSTEM.create_type(lInt->getType(), TypeSystem::QUANT_ONE);
-  TypeSystem::xqtref_t lDecimalType = GENV_TYPESYSTEM.create_type(lDecimal->getType(), TypeSystem::QUANT_ONE);
-  
-  assert(GENV_TYPESYSTEM.is_atomic(*lIntType));
-  assert(GENV_TYPESYSTEM.is_subtype(*lIntType, *lIntegerType));
-  assert(GENV_TYPESYSTEM.is_subtype(*lIntType, *lDecimalType));
-  assert(GENV_TYPESYSTEM.is_subtype(*lIntegerType, *lDecimalType));
+
+  {
+    Store& store = Store::getInstance();
+    Item_t lInteger = store.getItemFactory().createInteger((int32_t)1);
+    Item_t lInt = store.getItemFactory().createInt(1);
+    Item_t lDecimal = store.getItemFactory().createDecimal((int32_t)1);
+
+    TypeSystem::xqtref_t lIntegerType = GENV_TYPESYSTEM.create_type(lInteger->getType(), TypeSystem::QUANT_ONE);
+    TypeSystem::xqtref_t lIntType = GENV_TYPESYSTEM.create_type(lInt->getType(), TypeSystem::QUANT_ONE);
+    TypeSystem::xqtref_t lDecimalType = GENV_TYPESYSTEM.create_type(lDecimal->getType(), TypeSystem::QUANT_ONE);
+
+    assert(GENV_TYPESYSTEM.is_atomic(*lIntType));
+    assert(GENV_TYPESYSTEM.is_subtype(*lIntType, *lIntegerType));
+    assert(GENV_TYPESYSTEM.is_subtype(*lIntType, *lDecimalType));
+    assert(GENV_TYPESYSTEM.is_subtype(*lIntegerType, *lDecimalType));
+  }
+
+  GlobalEnvironment::destroy();
   
   return 0;
 }
