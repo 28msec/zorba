3c7468eb1712a175666476d67c73422f2df00400
markosza@D.ETHZ.CH <>
2008-02-15 11:23:51 +0000

    put typedef for ulong

--- src/util/win32/compatib_defs.h
+++ src/util/win32/compatib_defs.h
@@ -5,7 +5,7 @@
 
 #include "util/win32/stdint.h"
 
-// typedef unsigned long unsigned long;
+typedef unsigned long ulong;
 // typedef __int64		int64_t;
 // typedef unsigned __int64	uint64_t;
 // typedef unsigned short		uint16_t;
