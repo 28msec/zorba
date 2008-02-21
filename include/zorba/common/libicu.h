3e5a6cc3889b3aff7aebc9f85e4652ba5fc7e8c1
brantnem@D.ETHZ.CH <>
2008-02-21 21:20:23 +0000

    added missing file

---
+++ include/zorba/common/libicu.h
@@ -0,0 +1,15 @@
+#if defined CYGWIN
+
+#  define U_HAVE_INTTYPES 0
+#  define U_HAVE_INT8_T 1
+#  define U_HAVE_INT32_T 1
+#  define U_HAVE_UINT32_T 1
+
+#endif
+
+#include <unicode/utypes.h>
+#include <unicode/coll.h>
+#include <unicode/ustring.h>
+#include <unicode/stsearch.h>
+#include <unicode/ucnv.h>
+#include <unicode/normlzr.h>
