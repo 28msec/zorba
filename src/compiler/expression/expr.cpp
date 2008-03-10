b3a32c0a892debed8f3356906de5fd6a05f69a80
dmuresa@D.ETHZ.CH <>
2008-03-10 10:53:20 +0000

    Forgot to commit expr.cpp

--- src/compiler/expression/expr.cpp
+++ src/compiler/expression/expr.cpp
@@ -228,9 +228,9 @@ string var_expr::decode_var_kind(
   }
 }
 
-var_expr::var_expr(const QueryLoc& loc, store::Item_t name) : expr (loc), varname_h (name), type (GENV_TYPESYSTEM.UNTYPED_TYPE) {}
+var_expr::var_expr(const QueryLoc& loc, store::Item_t name) : expr (loc), varname_h (name), type (NULL) {}
 
-var_expr::var_expr(const QueryLoc& loc, var_kind k, store::Item_t name) : expr (loc), kind (k), varname_h (name), type (GENV_TYPESYSTEM.UNTYPED_TYPE) {}  // TODO
+var_expr::var_expr(const QueryLoc& loc, var_kind k, store::Item_t name) : expr (loc), kind (k), varname_h (name), type (NULL) {}  // TODO
 
 store::Item_t var_expr::get_varname() const { return varname_h; }
 xqtref_t var_expr::get_type() const { return type; }
