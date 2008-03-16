6490233cf37a79aef10ff9537c0851fee3eae981
dmuresa@D.ETHZ.CH <>
2008-03-16 08:14:52 +0000

    Small refactoring

--- src/compiler/rewriter/rules/type_rules.cpp
+++ src/compiler/rewriter/rules/type_rules.cpp
@@ -7,17 +7,21 @@
 #define LOOKUP_FN( pfx, local, arity ) static_cast<function *> (rCtx.getStaticContext()->lookup_fn (pfx, local, arity))
 #define LOOKUP_OP1( local ) static_cast<function *> (rCtx.getStaticContext()->lookup_builtin_fn (":" local, 1))
 
+using namespace std;
+
 namespace zorba {
 
 RULE_REWRITE_PRE(EliminateTypeEnforcingOperations)
 {
+  TypeManager *ts = rCtx.getStaticContext()->get_typemanager();
   fo_expr *fo;
+
   if ((fo = dynamic_cast<fo_expr *>(node)) != NULL) {
     function *fnboolen = LOOKUP_FN("fn", "boolean", 1);
     if (fo->get_func() == fnboolen) {
       expr_t arg = (*fo)[0];
       xqtref_t arg_type = arg->return_type(rCtx.getStaticContext());
-      if (rCtx.getStaticContext()->get_typemanager()->is_subtype(*arg_type, *GENV_TYPESYSTEM.BOOLEAN_TYPE_ONE)) {
+      if (ts->is_subtype(*arg_type, *GENV_TYPESYSTEM.BOOLEAN_TYPE_ONE)) {
         return arg;
       }
     }
@@ -25,7 +29,7 @@ RULE_REWRITE_PRE(EliminateTypeEnforcingOperations)
     if (fo->get_func() == fndata) {
       expr_t arg = (*fo)[0];
       xqtref_t arg_type = arg->return_type(rCtx.getStaticContext());
-      if (rCtx.getStaticContext()->get_typemanager()->is_subtype(*arg_type, *GENV_TYPESYSTEM.ANY_ATOMIC_TYPE_STAR)) {
+      if (ts->is_subtype(*arg_type, *GENV_TYPESYSTEM.ANY_ATOMIC_TYPE_STAR)) {
         return arg;
       }
     }
@@ -34,7 +38,8 @@ RULE_REWRITE_PRE(EliminateTypeEnforcingOperations)
   if ((pe = dynamic_cast<cast_base_expr *>(node)) != NULL) {
     expr_t arg = pe->get_input();
     xqtref_t arg_type = arg->return_type(rCtx.getStaticContext());
-    if (rCtx.getStaticContext()->get_typemanager()->is_subtype(*arg_type, *pe->get_target_type())) {
+    cout << "is_sub " << ts->toString (*arg_type) << " of " <<  ts->toString (*pe->get_target_type()) << (ts->is_subtype(*arg_type, *pe->get_target_type()) ? " 1" : " 0") << endl;
+    if (ts->is_subtype(*arg_type, *pe->get_target_type())) {
       return arg;
     }
   }
