01105b229777babcfd0b4af8b78cfed2ec837e5f
dmuresa@D.ETHZ.CH <>
2008-03-16 10:04:42 +0000

    type_rules rewriter: eliminate castable's

--- src/compiler/rewriter/rules/type_rules.cpp
+++ src/compiler/rewriter/rules/type_rules.cpp
@@ -17,31 +17,40 @@ RULE_REWRITE_PRE(EliminateTypeEnforcingOperations)
   fo_expr *fo;
 
   if ((fo = dynamic_cast<fo_expr *>(node)) != NULL) {
-    function *fnboolen = LOOKUP_FN("fn", "boolean", 1);
-    if (fo->get_func() == fnboolen) {
+    function *fnboolean = LOOKUP_FN("fn", "boolean", 1);
+    if (fo->get_func() == fnboolean) {
       expr_t arg = (*fo)[0];
       xqtref_t arg_type = arg->return_type(rCtx.getStaticContext());
-      if (ts->is_subtype(*arg_type, *GENV_TYPESYSTEM.BOOLEAN_TYPE_ONE)) {
+      if (ts->is_subtype(*arg_type, *GENV_TYPESYSTEM.BOOLEAN_TYPE_ONE))
         return arg;
-      }
+      else return NULL;
     }
     function *fndata = LOOKUP_FN("fn", "data", 1);
     if (fo->get_func() == fndata) {
       expr_t arg = (*fo)[0];
       xqtref_t arg_type = arg->return_type(rCtx.getStaticContext());
-      if (ts->is_subtype(*arg_type, *GENV_TYPESYSTEM.ANY_ATOMIC_TYPE_STAR)) {
+      if (ts->is_subtype(*arg_type, *GENV_TYPESYSTEM.ANY_ATOMIC_TYPE_STAR))
         return arg;
-      }
+      else return NULL;
     }
   }
   cast_base_expr *pe;
   if ((pe = dynamic_cast<cast_base_expr *>(node)) != NULL) {
     expr_t arg = pe->get_input();
     xqtref_t arg_type = arg->return_type(rCtx.getStaticContext());
-    if (ts->is_subtype(*arg_type, *pe->get_target_type())) {
+    if (ts->is_subtype(*arg_type, *pe->get_target_type()))
       return arg;
-    }
+    else return NULL;
   }
+  castable_base_expr *cbe;
+  if ((cbe = dynamic_cast<castable_base_expr *>(node)) != NULL) {
+    expr_t arg = cbe->get_expr();
+    xqtref_t arg_type = arg->return_type(rCtx.getStaticContext());
+    if (ts->is_subtype(*arg_type, *cbe->get_type()))
+      return new const_expr (node->get_loc (), true);
+    else return NULL;
+  }
+
   return NULL;
 }
 
