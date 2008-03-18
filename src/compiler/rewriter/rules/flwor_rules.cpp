d1ea0686c71daf7e704953097deb3768c5897b1c
dmuresa@D.ETHZ.CH <>
2008-03-18 08:24:07 +0000

    FLWOR rules: FOR variables: unused, $x in ()

--- src/compiler/rewriter/rules/flwor_rules.cpp
+++ src/compiler/rewriter/rules/flwor_rules.cpp
@@ -7,14 +7,30 @@ namespace zorba {
 
 RULE_REWRITE_PRE(EliminateUnusedLetVars)
 {
+  static_context *sctx = rCtx.getStaticContext();
+  TypeManager *ts = sctx->get_typemanager();
   flwor_expr *flwor = dynamic_cast<flwor_expr *>(node);
   if (flwor != NULL) {
     flwor_expr::clause_list_t::iterator i = flwor->clause_begin();
     while(i != flwor->clause_end()) {
       flwor_expr::forletref_t ref = *i;
       forlet_clause::varref_t vref = ref->get_var();
-      if (vref->get_kind() == var_expr::let_var && count_variable_uses(flwor, &*vref) == 0) {
-        i = flwor->remove_forlet_clause(i);
+      bool is_let = vref->get_kind() == var_expr::let_var;
+      int quant_cnt = 2;
+      if (! is_let) {
+        xqtref_t ctype = ref->get_expr ()->return_type (sctx);
+        if (ts->is_equal (*ctype, *ts->create_empty_type ()))
+          quant_cnt = 0;
+        else if (ts->quantifier (*ctype) == TypeConstants::QUANT_ONE)
+          quant_cnt = 1;
+      }
+      if (is_let || quant_cnt < 2) {
+        if (quant_cnt == 0) return new fo_expr (node->get_loc (), LOOKUP_OPN ("concatenate"));
+        int uses = count_variable_uses(flwor, &*vref);
+        if (uses >= 1)  // TODO: substitute for uses == 1
+          ++i;
+        else
+          i = flwor->remove_forlet_clause(i);
       } else {
         ++i;
       }
