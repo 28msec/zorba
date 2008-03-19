819402e06417260fe6d55f9b33bd885fe0f1e533
dmuresa@D.ETHZ.CH <>
2008-03-19 19:38:10 +0000

    FLWOR rewriter iterates to fixed point

--- src/compiler/rewriter/rules/flwor_rules.cpp
+++ src/compiler/rewriter/rules/flwor_rules.cpp
@@ -10,6 +10,8 @@ using namespace std;
 
 namespace zorba {
 
+#define MODIFY( expr ) do { modified = true; expr; } while (0)
+
   class SubstVars : public RewriteRule {
   protected:
     var_expr *var;
@@ -38,70 +40,70 @@ namespace zorba {
 
 RULE_REWRITE_PRE(EliminateUnusedLetVars)
 {
+  flwor_expr *flwor = dynamic_cast<flwor_expr *>(node);
+  if (flwor == NULL) return NULL;
+
+  bool modified = false;
   static_context *sctx = rCtx.getStaticContext();
   TypeManager *ts = sctx->get_typemanager();
-  flwor_expr *flwor = dynamic_cast<flwor_expr *>(node);
-  if (flwor != NULL) {
-    flwor_expr::clause_list_t::iterator i = flwor->clause_begin();
-    while(i != flwor->clause_end()) {
-      flwor_expr::forletref_t ref = *i;
-      expr *cexpr = ref->get_expr ();
-      forlet_clause::varref_t vref = ref->get_var();
-      bool is_let = vref->get_kind() == var_expr::let_var;
-      int quant_cnt = 2;  // cardinality of for clause: 0, 1 or more
-      forlet_clause::varref_t pvref = ref->get_pos_var ();
-      if (pvref != NULL && count_variable_uses(flwor, &*pvref, 1) == 0)
-        ref->set_pos_var (NULL);
-      if (! is_let) {
-        xqtref_t ctype = cexpr->return_type (sctx);
-        if (ts->is_equal (*ctype, *ts->create_empty_type ()))
-          quant_cnt = 0;
-        else if (ts->quantifier (*ctype) == TypeConstants::QUANT_ONE)
-          quant_cnt = 1;
-      }
-      if (is_let || quant_cnt < 2) {
-        if (quant_cnt == 0) return new fo_expr (node->get_loc (), LOOKUP_OPN ("concatenate"));
-        // otherwise is_let || quant_cnt == 1
-        if (pvref != NULL)
-          subst_vars (rCtx, pvref.getp (), new const_expr (node->get_loc (), xqp_integer::parseInt (1)));
-        int uses = count_variable_uses(flwor, &*vref, 2);
-        if (uses > 1) {
-          if (cexpr->get_expr_kind () == const_expr_kind) {
-              i = flwor->remove_forlet_clause (i);
-              subst_vars (rCtx, vref, cexpr);
-          } else ++i;
-        } else {
-          if (uses == 1) {
-            if (flwor->forlet_count () == 1 // TODO: if cardinality FLWOR result = 1...
-                || cexpr->get_annotation (AnnotationKey::UNFOLDABLE_OP) != TSVAnnotationValue::TRUE_VALUE) 
+  flwor_expr::clause_list_t::iterator i = flwor->clause_begin();
+  while(i != flwor->clause_end()) {
+    flwor_expr::forletref_t ref = *i;
+    expr *cexpr = ref->get_expr ();
+    forlet_clause::varref_t vref = ref->get_var();
+    bool is_let = vref->get_kind() == var_expr::let_var;
+    int quant_cnt = 2;  // cardinality of for clause: 0, 1 or more
+    forlet_clause::varref_t pvref = ref->get_pos_var ();
+    if (pvref != NULL && count_variable_uses(flwor, &*pvref, 1) == 0)
+      MODIFY (ref->set_pos_var (NULL));
+    if (! is_let) {
+      xqtref_t ctype = cexpr->return_type (sctx);
+      if (ts->is_equal (*ctype, *ts->create_empty_type ()))
+        quant_cnt = 0;
+      else if (ts->quantifier (*ctype) == TypeConstants::QUANT_ONE)
+        quant_cnt = 1;
+    }
+    if (is_let || quant_cnt < 2) {
+      if (quant_cnt == 0) return new fo_expr (node->get_loc (), LOOKUP_OPN ("concatenate"));
+      // otherwise is_let || quant_cnt == 1
+      if (pvref != NULL)
+        MODIFY (subst_vars (rCtx, pvref.getp (), new const_expr (node->get_loc (), xqp_integer::parseInt (1))));
+      int uses = count_variable_uses(flwor, &*vref, 2);
+      if (uses > 1) {
+        if (cexpr->get_expr_kind () == const_expr_kind) {
+          MODIFY (i = flwor->remove_forlet_clause (i));
+          subst_vars (rCtx, vref, cexpr);
+        } else ++i;
+      } else {
+        if (uses == 1) {
+          if (flwor->forlet_count () == 1 // TODO: if cardinality FLWOR result = 1...
+              || cexpr->get_annotation (AnnotationKey::UNFOLDABLE_OP) != TSVAnnotationValue::TRUE_VALUE) 
             {
-              
-              i = flwor->remove_forlet_clause (i);
+              MODIFY (i = flwor->remove_forlet_clause (i));
               subst_vars (rCtx, vref, cexpr);
             }
-            else ++i;
-          } else {
-            i = flwor->remove_forlet_clause(i);
-          }
+          else ++i;
+        } else {
+          MODIFY (i = flwor->remove_forlet_clause(i));
         }
-      } else {
-        ++i;
       }
-    }
-    if (flwor->forlet_count() == 0) {
-      expr_t where = flwor->get_where();
-      expr_t result = flwor->get_retval();
-      if (where != NULL) {
-        rchandle<if_expr> ite(new if_expr(where->get_loc()));
-        ite->set_cond_expr(where);
-        ite->set_then_expr(result);
-        ite->set_else_expr(new fo_expr(where->get_loc(), LOOKUP_OPN("concatenate")));
-        result = &*ite;
-      }
-      return result;
+    } else {
+      ++i;
     }
   }
-  return NULL;
+  if (flwor->forlet_count() == 0) {
+    expr_t where = flwor->get_where();
+    expr_t result = flwor->get_retval();
+    if (where != NULL) {
+      rchandle<if_expr> ite(new if_expr(where->get_loc()));
+      ite->set_cond_expr(where);
+      ite->set_then_expr(result);
+      ite->set_else_expr(new fo_expr(where->get_loc(), LOOKUP_OPN("concatenate")));
+      result = &*ite;
+    }
+    return result;
+  }
+  return modified ? node : NULL;
 }
 
 RULE_REWRITE_POST(EliminateUnusedLetVars)
