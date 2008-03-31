8003f0b6224f2472103359254cb2918168f7251e
nbrinza@D.ETHZ.CH <>
2008-03-31 07:02:14 +0000

    Fixed a bug in serializer

--- src/api/serialization/serializer.cpp
+++ src/api/serialization/serializer.cpp
@@ -171,8 +171,8 @@ xqpString toHexString(unsigned char ch)
 {
   static const char hex[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
   xqpString result;
-  if ((ch>>8) > 0)
-    result += hex[ch >> 8];
+  if ((ch>>4) > 0)
+    result += hex[ch >> 4];
   result += hex[ch & 0xF];
   return result;
 }
