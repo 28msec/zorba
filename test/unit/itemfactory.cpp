75138571602d41d353ccc2f80fd91bf07a8fb8af
brantmat@D.ETHZ.CH <>
2008-04-07 11:31:39 +0000

    forgot to add the file

---
+++ test/unit/itemfactory.cpp
@@ -0,0 +1,129 @@
+#include <cassert>
+#include <iostream>
+
+
+#include <zorba/zorba.h>
+
+using namespace zorba;
+
+bool
+checkType(const Item& aItem, const String& aLocalname)
+{
+  try {
+    return (aItem.getPrefix() == "xs" && 
+            aItem.getLocalName() == aLocalname && 
+            aItem.getNamespace() == "http://www.w3.org/2001/XMLSchema");
+  } catch (ZorbaException &e) {
+    std::cerr << e << std::endl;
+    return false;
+  }
+}
+
+#define CHECK_NOT_IMPLEMENTED(object, member) \
+  try { \
+    object.member;\
+    assert(false); \
+  } catch (SystemException &e) { \
+    assert (e.getErrorCode() == ZorbaError::XQP0024_FUNCTION_NOT_IMPLEMENTED_FOR_ITEMTYPE); \
+  }
+
+
+
+int itemfactory(int argc, char* argv[]) 
+{
+  Zorba* lZorba = Zorba::getInstance();
+
+  ItemFactory* lFactory = lZorba->getItemFactory();
+
+  Item lItem;
+  try {
+    /** String */
+    lItem = lFactory->createString("abc");
+    assert ( checkType(lItem.getType(), "string") );
+    assert ( lItem.isAtomic() );
+    assert ( lItem.getStringValue() == "abc" );
+    CHECK_NOT_IMPLEMENTED(lItem, isNaN() );
+
+    /** AnyURI */
+    lItem = lFactory->createAnyURI("http://www.flworfound.org/");
+    assert ( checkType(lItem.getType(), "anyURI") );
+    assert ( lItem.isAtomic() );
+    assert ( lItem.getStringValue() == "http://www.flworfound.org/");
+
+    /** Integer */
+    lItem = lFactory->createInteger(5);
+    assert( checkType(lItem.getType(), "int") ); // TODO type should be integer not int
+    assert( lItem.isAtomic() );
+    assert( lItem.getStringValue() == "5" );
+    assert( ! lItem.isNaN() );
+    CHECK_NOT_IMPLEMENTED(lItem, isPosOrNegInf());
+    CHECK_NOT_IMPLEMENTED(lItem, getBooleanValue() );
+
+    /** QName */
+    lItem = lFactory->createQName("http://www.flworfound.org", "flwor", "name");
+    assert ( checkType(lItem.getType(), "QName") );
+    assert ( lItem.isAtomic() );
+    assert ( lItem.getStringValue() == "flwor:name" );
+    assert ( lItem.getNamespace() == "http://www.flworfound.org" );
+
+    lItem = lFactory->createQName("http://www.flworfound.org", "name");
+    assert ( checkType(lItem.getType(), "QName") );
+    assert ( lItem.isAtomic() );
+    assert ( lItem.getStringValue() == "name" );
+    assert ( lItem.getPrefix().length() == 0 );
+    assert ( lItem.getPrefix().empty() );
+    assert ( lItem.getNamespace() == "http://www.flworfound.org" );
+
+    /** NCName */
+    lItem = lFactory->createNCName("ncname");
+    assert ( checkType(lItem.getType(), "NCName") );
+    assert ( lItem.isAtomic() );
+    assert ( lItem.getStringValue() == "ncname" );
+    CHECK_NOT_IMPLEMENTED (lItem, getPrefix());
+    CHECK_NOT_IMPLEMENTED (lItem, getLocalName());
+    CHECK_NOT_IMPLEMENTED (lItem, getNamespace());
+    CHECK_NOT_IMPLEMENTED (lItem, getBooleanValue() );
+
+    /** Base64Binary */
+    lItem = lFactory->createBase64Binary("", 0);
+    assert ( checkType(lItem.getType(), "base64Binary") );
+    assert ( lItem.isAtomic() );
+    assert ( lItem.getStringValue() == "" );
+    assert ( lItem.getStringValue().length() == 0 );
+
+    lItem = lFactory->createBase64Binary("cmxjZ3R4c3JidnllcmVuZG91aWpsbXV5Z2NhamxpcmJkaWFhbmFob2VsYXVwZmJ1Z2dmanl2eHlzYmhheXFtZXR0anV2dG1q", 96);
+    assert ( checkType(lItem.getType(), "base64Binary") );
+    assert ( lItem.isAtomic() );
+    assert ( lItem.getStringValue() == "cmxjZ3R4c3JidnllcmVuZG91aWpsbXV5Z2NhamxpcmJkaWFhbmFob2VsYXVwZmJ1Z2dmanl2eHlzYmhheXFtZXR0anV2dG1q" );
+    assert ( lItem.getStringValue().length() == 96 );
+    CHECK_NOT_IMPLEMENTED (lItem, getBooleanValue() );
+
+    /** Boolean */
+    lItem = lFactory->createBoolean(true);
+    assert ( checkType(lItem.getType(), "boolean") );
+    assert ( lItem.isAtomic() );
+    assert ( lItem.getStringValue() == "true" );
+    assert ( lItem.getEBV().getStringValue() == "true" ); 
+    assert ( lItem.getBooleanValue() );
+
+    lItem = lFactory->createBoolean(false);
+    assert ( checkType(lItem.getType(), "boolean") );
+    assert ( lItem.isAtomic() );
+    assert ( lItem.getStringValue() == "false" );
+    assert ( lItem.getEBV().getStringValue() == "false" ); 
+    assert ( ! lItem.getBooleanValue() );
+
+    /** Decimal */
+//  lItem = lFactory->createDecimal(12678967);
+//  assert ( checkType(lItem.getType(), "decimal") );
+//  assert ( lItem.isAtomic() );
+//  assert ( lItem.getStringValue() = "12678967" );
+//  assert ( !lItem.getEBV().getBooleanValue() );
+
+  } catch (ZorbaException &e) {
+    std::cerr << e << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
