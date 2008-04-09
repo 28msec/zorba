7c1e90cbf411f37cfc8b6516591ca0a40189ccf4
dmuresa@D.ETHZ.CH <>
2008-04-09 06:30:21 +0000

    Unwrap single-step relpath expressions

--- src/compiler/rewriter/rules/path_rules.cpp
+++ src/compiler/rewriter/rules/path_rules.cpp
@@ -8,31 +8,34 @@ RULE_REWRITE_PRE(EliminateExtraneousPathSteps)
 {
   relpath_expr *re = dynamic_cast<relpath_expr *>(node);
   if (re != NULL) {
-      ulong numSteps = re->size();
-
-      for (ulong i = 0; i < numSteps; i++)
+    ulong numSteps = re->size();
+    
+    for (ulong i = 0; i < numSteps - 1; i++) {
+      axis_step_expr* axisStep = dynamic_cast<axis_step_expr*>((*re)[i].getp());
+      
+      if (axisStep != NULL &&
+          axisStep->getAxis() == axis_kind_descendant_or_self &&
+          axisStep->numPreds() == 0 &&
+          axisStep->getTest()->getTestKind() == match_anykind_test)
       {
-          axis_step_expr* axisStep = dynamic_cast<axis_step_expr*>((*re)[i].getp());
-
-          if (axisStep != NULL &&
-                  i < numSteps - 1 &&
-                  axisStep->getAxis() == axis_kind_descendant_or_self &&
-                  axisStep->numPreds() == 0 &&
-                  axisStep->getTest()->getTestKind() == match_anykind_test)
-          {
-              axis_step_expr* nextStep = dynamic_cast<axis_step_expr*>((*re)[i+1].getp());
-              if (nextStep != NULL &&
-                      nextStep->getAxis() == axis_kind_child &&
-                      axisStep->numPreds() == 0)
-              {
-                  nextStep->setAxis(axis_kind_descendant);
-                  (*re).erase(i);
-                  numSteps--;
-                  i--;
-              }
-          }   
+        axis_step_expr* nextStep = dynamic_cast<axis_step_expr*>((*re)[i+1].getp());
+        if (nextStep != NULL &&
+            nextStep->getAxis() == axis_kind_child &&
+            axisStep->numPreds() == 0)
+        {
+          nextStep->setAxis(axis_kind_descendant);
+          (*re).erase(i);
+          numSteps--;
+          i--;
+        }
       }
+    }
+
+    if (numSteps == 1) {
+      return (*re) [0];
+    }
   }
+
   return NULL;
 }
 
