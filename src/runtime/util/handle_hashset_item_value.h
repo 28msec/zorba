ac4f3ea0616d5f55da9a0456cf0864be07fde533
markosza@D.ETHZ.CH <>
2008-04-09 18:13:35 +0000

    merged hashset and hashmap + bug fixes

--- src/runtime/util/handle_hashset_item_value.h
+++ src/runtime/util/handle_hashset_item_value.h
@@ -3,7 +3,7 @@
 
 #include "util/hashfun.h"
 
-#include "store/util/handle_hashset.h"
+#include "store/util/hashset.h"
 #include "runtime/booleans/BooleanImpl.h"
 
 
@@ -32,11 +32,11 @@ template <class T, class E>
 class Externals<T, E, ValueCompareParam>
 {
 public:
-  static bool equal(const T* t1, const T* t2, ValueCompareParam* aCompareParam)
+  static bool equal(const T& t1, const T& t2, ValueCompareParam* aCompareParam)
   {
     return E::equal(t1, t2, aCompareParam);
   }
-  static uint32_t hash(const T* t1, ValueCompareParam* aCompareParam)
+  static uint32_t hash(const T& t1, ValueCompareParam* aCompareParam)
   {
     return E::hash(t1, aCompareParam);
   }
@@ -46,51 +46,59 @@ template <class T, class E>
 class Externals<T, E, ValueCollCompareParam>
 {
 public:
-  static bool equal(const T* t1, const T* t2, ValueCollCompareParam* aCompareParam)
+  static bool equal(const T& t1, const T& t2, ValueCollCompareParam* aCompareParam)
   {
     return E::equal(t1, t2, aCompareParam);
   }
-  static uint32_t hash(const T* t1, ValueCollCompareParam* aCompareParam)
+  static uint32_t hash(const T& t1, ValueCollCompareParam* aCompareParam)
   {
     return E::hash(t1, aCompareParam);
   }
 };
 
-class ItemValueHandleHashSet : public HandleSet<Item, ItemValueHandleHashSet, ValueCompareParam>
+
+class ItemValueHandleHashSet : public HashSet<Item_t,
+                                              ItemValueHandleHashSet,
+                                              ValueCompareParam>
 {
 public:
   ItemValueHandleHashSet(ValueCompareParam* lValueCompareParam, long size = 1024)
-  : HandleSet<Item, ItemValueHandleHashSet, ValueCompareParam>(lValueCompareParam, size)
+    :
+    HashSet<Item_t, ItemValueHandleHashSet, ValueCompareParam>(lValueCompareParam, size)
   {}
 
-  static bool equal(const Item* t1, const Item* t2, ValueCompareParam* aCompareParam)
+  static bool equal(const Item_t& t1, const Item_t& t2, ValueCompareParam* aCompareParam)
   {
-    const store::Item_t l1 = const_cast<Item*>(t1);
-    const store::Item_t l2 = const_cast<Item*>(t2);
-    return CompareIterator::valueEqual(aCompareParam->theRuntimeCB, l1, l2) == 0; 
+    return CompareIterator::valueEqual(aCompareParam->theRuntimeCB, t1, t2) == 0; 
   }
 
-  static uint32_t hash(const Item* t, ValueCompareParam* aCompareParam)
+  static uint32_t hash(const Item_t& t, ValueCompareParam* aCompareParam)
   {
     return t->hash(NULL);
   }
 };
 
-class ItemValueCollHandleHashSet : public HandleSet<Item, ItemValueCollHandleHashSet, ValueCollCompareParam>
+
+class ItemValueCollHandleHashSet : public HashSet<Item_t,
+                                                  ItemValueCollHandleHashSet,
+                                                  ValueCollCompareParam>
 {
 public:
-  ItemValueCollHandleHashSet(ValueCollCompareParam* lValueCompareParamColl, long size = 1024)
-  : HandleSet<Item, ItemValueCollHandleHashSet, ValueCollCompareParam>(lValueCompareParamColl, size)
+  ItemValueCollHandleHashSet(
+        ValueCollCompareParam* lValueCompareParamColl,
+        long size = 1024)
+  :
+  HashSet<Item_t, ItemValueCollHandleHashSet, ValueCollCompareParam>(lValueCompareParamColl, size)
   {}
 
-  static bool equal(const Item* t1, const Item* t2, ValueCollCompareParam* aCompareParam)
+  static bool equal(const Item_t& t1, const Item_t& t2, ValueCollCompareParam* aCompareParam)
   {
-    const store::Item_t l1 = const_cast<Item*>(t1);
-    const store::Item_t l2 = const_cast<Item*>(t2);
-    return CompareIterator::valueEqual(aCompareParam->theRuntimeCB, l1, l2, (aCompareParam->theCollator)) == 0; 
+    return CompareIterator::valueEqual(aCompareParam->theRuntimeCB,
+                                       t1, t2,
+                                       (aCompareParam->theCollator)) == 0; 
   }
 
-  static uint32_t hash(const Item* t, ValueCollCompareParam* aCompareParam)
+  static uint32_t hash(const Item_t& t, ValueCollCompareParam* aCompareParam)
   {
     return t->hash(aCompareParam->theRuntimeCB, aCompareParam->theCollator);
   }
