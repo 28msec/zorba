3ab7086c4504658cb8444f579362ea34d5afec0f
nbrinza@D.ETHZ.CH <>
2008-04-09 13:45:14 +0000

    Generated files

--- src/compiler/parser/FlexLexer.h
+++ src/compiler/parser/FlexLexer.h
@@ -1,4 +1,3 @@
-
 // -*-C++-*-
 // FlexLexer.h -- define interfaces for lexical analyzer classes generated
 // by flex
@@ -100,12 +99,12 @@ protected:
 };
 
 }
-#endif
+#endif // FLEXLEXER_H
 
 #if defined(yyFlexLexer) || ! defined(yyFlexLexerOnce)
 // Either this is the first time through (yyFlexLexerOnce not defined),
 // or this is a repeated include to define a different flavor of
-// yyFlexLexer, as discussed in the flex man page.
+// yyFlexLexer, as discussed in the flex manual.
 #define yyFlexLexerOnce
 
 extern "C++" {
@@ -123,11 +122,12 @@ public:
 	void yy_delete_buffer( struct yy_buffer_state* b );
 	void yyrestart( FLEX_STD istream* s );
 
-    void yypush_buffer_state( struct yy_buffer_state* new_buffer );
-    void yypop_buffer_state(void);
+	void yypush_buffer_state( struct yy_buffer_state* new_buffer );
+	void yypop_buffer_state();
 
 	virtual int yylex();
-	virtual void switch_streams( FLEX_STD istream* new_in, FLEX_STD ostream* new_out );
+	virtual void switch_streams( FLEX_STD istream* new_in, FLEX_STD ostream* new_out = 0 );
+	virtual int yywrap();
 
 protected:
 	virtual int LexerInput( char* buf, int max_size );
@@ -173,10 +173,10 @@ protected:
 	int yy_did_buffer_switch_on_eof;
 
 
-    size_t yy_buffer_stack_top; /**< index of top of stack. */
-    size_t yy_buffer_stack_max; /**< capacity of stack. */
-    struct yy_buffer_state ** yy_buffer_stack; /**< Stack as an array. */
-    void yyensure_buffer_stack(void);
+	size_t yy_buffer_stack_top; /**< index of top of stack. */
+	size_t yy_buffer_stack_max; /**< capacity of stack. */
+	struct yy_buffer_state ** yy_buffer_stack; /**< Stack as an array. */
+	void yyensure_buffer_stack(void);
 
 	// The following are not always needed, but may be depending
 	// on use of certain flex features (like REJECT or yymore()).
@@ -202,4 +202,5 @@ protected:
 
 }
 
-#endif
+#endif // yyFlexLexer || ! yyFlexLexerOnce
+
