3ac930cf6e4360ba67e920c43b566835ffb7deb9
sonasoi@D.ETHZ.CH <>
2008-04-11 14:28:51 +0000

    Solved an error in the fn:document-uri iterator.

--- src/runtime/accessors/AccessorsImpl.cpp
+++ src/runtime/accessors/AccessorsImpl.cpp
@@ -177,6 +177,8 @@ store::Item_t FnDocumentUriIterator::nextImpl(PlanState& planState) const
   PlanIteratorState *state;
   DEFAULT_STACK_INIT(PlanIteratorState, state, planState);
 
+  inNode = consumeNext(theChildren[0].getp(), planState);
+
   if (inNode != NULL)
   {
     if (!inNode->isNode())
