308c20371a08b13dd580d8dbe5507b2417d78c38
dmuresa@D.ETHZ.CH <>
2008-04-22 14:18:24 +0000

    Forgot to commit common/shared_types.h

--- src/common/shared_types.h
+++ src/common/shared_types.h
@@ -160,7 +160,7 @@ class FTWordsValue;
 
 
 // Expression nodes
-class expr; class var_expr; class order_modifier; class flwor_expr;
+class expr; class sequential_expr; class var_expr; class order_modifier; class flwor_expr;
 class case_clause; class promote_expr; class trycatch_expr; class eval_expr; class typeswitch_expr;
 class if_expr; class fo_expr; class ft_select_expr; class ft_contains_expr;
 class instanceof_expr; class treat_expr; class castable_expr; class cast_expr; class name_cast_expr;
