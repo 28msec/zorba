b4fd160e52e982420818b98a794491b3360093b9
dmuresa@D.ETHZ.CH <>
2008-04-24 17:07:11 +0000

    ns_consts.h was not committed

---
+++ src/context/ns_consts.h
@@ -0,0 +1,18 @@
+#ifndef ZORBA_NS_CONSTS_H
+#define ZORBA_NS_CONSTS_H
+
+// TODO make these static members of the context
+#define XQUERY_FN_NS  "http://www.w3.org/2005/xpath-functions"
+#define XQUERY_OP_NS  "http://www.w3.org/2005/xpath-functions"
+#define XML_NS "http://www.w3.org/XML/1998/namespace"
+#define XML_SCHEMA_NS "http://www.w3.org/2001/XMLSchema"
+#define XSI_NS "http://www.w3.org/2001/XMLSchema-instance"
+#define XQUERY_LOCAL_FN_NS "http://www.w3.org/2005/xquery-local-functions"
+#define XQUERY_ERR_NS  "http://www.w3.org/2005/xqt-errors"
+
+#endif
+/*
+ * Local variables:
+ * mode: c++
+ * End:
+ */
