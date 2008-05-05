a93e9fbfaa382ba769178af5371d8ab415f1e085
spungi <>
2008-05-05 06:23:44 +0000

    Test

--- src/runtime/accessors/AccessorsImpl.cpp
+++ src/runtime/accessors/AccessorsImpl.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 #include "store/api/item.h"
 #include "store/api/iterator.h"
 #include "errors/error_manager.h"
