5732f3240849f754322cbf46ace682ac26dd05d2
wcandillon <>
2008-05-16 10:14:40 +0000

    Refactor the socket API. Switch on installation of debugger external API.

--- src/debugger/socket.cpp
+++ src/debugger/socket.cpp
@@ -16,20 +16,20 @@
 #include "debugger/socket.h"
 
 #if defined WIN32 || defined WINCE
-  #include <winsock2.h>         // For socket(), connect(), send(), and recv()
+#include <winsock2.h>
   typedef int socklen_t;
-  typedef char raw_type;       // Type used for raw data on this platform
+  typedef char raw_type;
 #else
-  #include <sys/types.h>       // For data types
-  #include <sys/socket.h>      // For socket(), connect(), send(), and recv()
-  #include <netdb.h>           // For gethostbyname()
-  #include <arpa/inet.h>       // For inet_addr()
-  #include <unistd.h>          // For close()
-  #include <netinet/in.h>      // For sockaddr_in
-  typedef void raw_type;       // Type used for raw data on this platform
+  #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netdb.h>
+  #include <arpa/inet.h>
+  #include <unistd.h>
+  #include <netinet/in.h>
+  typedef void raw_type;
 #endif
 
-#include <errno.h>             // For errno
+#include <errno.h>
 
 using namespace std;
 
@@ -269,109 +269,6 @@ TCPSocket *TCPServerSocket::accept() throw(SocketException) {
 void TCPServerSocket::setListen(int queueLen) throw(SocketException) {
   if (listen(sockDesc, queueLen) < 0) {
     throw SocketException("Set listening socket failed (listen())", true);
-  }
-}
-
-// UDPSocket Code
-
-UDPSocket::UDPSocket() throw(SocketException) : CommunicatingSocket(SOCK_DGRAM,
-    IPPROTO_UDP) {
-  setBroadcast();
-}
-
-UDPSocket::UDPSocket(unsigned short localPort)  throw(SocketException) : 
-    CommunicatingSocket(SOCK_DGRAM, IPPROTO_UDP) {
-  setLocalPort(localPort);
-  setBroadcast();
-}
-
-UDPSocket::UDPSocket(const string &localAddress, unsigned short localPort) 
-     throw(SocketException) : CommunicatingSocket(SOCK_DGRAM, IPPROTO_UDP) {
-  setLocalAddressAndPort(localAddress, localPort);
-  setBroadcast();
-}
-
-void UDPSocket::setBroadcast() {
-  // If this fails, we'll hear about it when we try to send.  This will allow 
-  // system that cannot broadcast to continue if they don't plan to broadcast
-  int broadcastPermission = 1;
-  setsockopt(sockDesc, SOL_SOCKET, SO_BROADCAST, 
-             (raw_type *) &broadcastPermission, sizeof(broadcastPermission));
-}
-
-void UDPSocket::disconnect() throw(SocketException) {
-  sockaddr_in nullAddr;
-  memset(&nullAddr, 0, sizeof(nullAddr));
-  nullAddr.sin_family = AF_UNSPEC;
-
-  // Try to disconnect
-  if (::connect(sockDesc, (sockaddr *) &nullAddr, sizeof(nullAddr)) < 0) {
-   #ifdef WIN32
-    if (errno != WSAEAFNOSUPPORT) {
-   #else
-    if (errno != EAFNOSUPPORT) {
-   #endif
-      throw SocketException("Disconnect failed (connect())", true);
-    }
-  }
-}
-
-void UDPSocket::sendTo(const void *buffer, int bufferLen, 
-    const string &foreignAddress, unsigned short foreignPort) 
-    throw(SocketException) {
-  sockaddr_in destAddr;
-  fillAddr(foreignAddress, foreignPort, destAddr);
-
-  // Write out the whole buffer as a single message.
-  if (sendto(sockDesc, (raw_type *) buffer, bufferLen, 0,
-             (sockaddr *) &destAddr, sizeof(destAddr)) != bufferLen) {
-    throw SocketException("Send failed (sendto())", true);
-  }
-}
-
-int UDPSocket::recvFrom(void *buffer, int bufferLen, string &sourceAddress,
-    unsigned short &sourcePort) throw(SocketException) {
-  sockaddr_in clntAddr;
-  socklen_t addrLen = sizeof(clntAddr);
-  int rtn;
-  if ((rtn = recvfrom(sockDesc, (raw_type *) buffer, bufferLen, 0, 
-                      (sockaddr *) &clntAddr, (socklen_t *) &addrLen)) < 0) {
-    throw SocketException("Receive failed (recvfrom())", true);
-  }
-  sourceAddress = inet_ntoa(clntAddr.sin_addr);
-  sourcePort = ntohs(clntAddr.sin_port);
-
-  return rtn;
-}
-
-void UDPSocket::setMulticastTTL(unsigned char multicastTTL) throw(SocketException) {
-  if (setsockopt(sockDesc, IPPROTO_IP, IP_MULTICAST_TTL, 
-                 (raw_type *) &multicastTTL, sizeof(multicastTTL)) < 0) {
-    throw SocketException("Multicast TTL set failed (setsockopt())", true);
-  }
-}
-
-void UDPSocket::joinGroup(const string &multicastGroup) throw(SocketException) {
-  struct ip_mreq multicastRequest;
-
-  multicastRequest.imr_multiaddr.s_addr = inet_addr(multicastGroup.c_str());
-  multicastRequest.imr_interface.s_addr = htonl(INADDR_ANY);
-  if (setsockopt(sockDesc, IPPROTO_IP, IP_ADD_MEMBERSHIP, 
-                 (raw_type *) &multicastRequest, 
-                 sizeof(multicastRequest)) < 0) {
-    throw SocketException("Multicast group join failed (setsockopt())", true);
-  }
-}
-
-void UDPSocket::leaveGroup(const string &multicastGroup) throw(SocketException) {
-  struct ip_mreq multicastRequest;
-
-  multicastRequest.imr_multiaddr.s_addr = inet_addr(multicastGroup.c_str());
-  multicastRequest.imr_interface.s_addr = htonl(INADDR_ANY);
-  if (setsockopt(sockDesc, IPPROTO_IP, IP_DROP_MEMBERSHIP, 
-                 (raw_type *) &multicastRequest, 
-                 sizeof(multicastRequest)) < 0) {
-    throw SocketException("Multicast group leave failed (setsockopt())", true);
   }
 }
 }//end of namespace
--- src/debugger/socket.h
+++ src/debugger/socket.h
@@ -17,8 +17,8 @@
 #ifndef ZORBA_DEBUGGER_SOCKET_H
 #define ZORBA_DEBUGGER_SOCKET_H
 
-#include <string>            // For string
-#include <exception>         // For exception class
+#include <string>
+#include <exception>
 
 namespace zorba{
 /**
@@ -248,91 +248,6 @@ public:
 
 private:
   void setListen(int queueLen) throw(SocketException);
-};
-
-/**
-  *   UDP socket class
-  */
-class UDPSocket : public CommunicatingSocket {
-public:
-  /**
-   *   Construct a UDP socket
-   *   @exception SocketException thrown if unable to create UDP socket
-   */
-  UDPSocket() throw(SocketException);
-
-  /**
-   *   Construct a UDP socket with the given local port
-   *   @param localPort local port
-   *   @exception SocketException thrown if unable to create UDP socket
-   */
-  UDPSocket(unsigned short localPort) throw(SocketException);
-
-  /**
-   *   Construct a UDP socket with the given local port and address
-   *   @param localAddress local address
-   *   @param localPort local port
-   *   @exception SocketException thrown if unable to create UDP socket
-   */
-  UDPSocket(const std::string &localAddress, unsigned short localPort) 
-      throw(SocketException);
-
-  /**
-   *   Unset foreign address and port
-   *   @return true if disassociation is successful
-   *   @exception SocketException thrown if unable to disconnect UDP socket
-   */
-  void disconnect() throw(SocketException);
-
-  /**
-   *   Send the given buffer as a UDP datagram to the
-   *   specified address/port
-   *   @param buffer buffer to be written
-   *   @param bufferLen number of bytes to write
-   *   @param foreignAddress address (IP address or name) to send to
-   *   @param foreignPort port number to send to
-   *   @return true if send is successful
-   *   @exception SocketException thrown if unable to send datagram
-   */
-  void sendTo(const void *buffer, int bufferLen, const std::string &foreignAddress,
-            unsigned short foreignPort) throw(SocketException);
-
-  /**
-   *   Read read up to bufferLen bytes data from this socket.  The given buffer
-   *   is where the data will be placed
-   *   @param buffer buffer to receive data
-   *   @param bufferLen maximum number of bytes to receive
-   *   @param sourceAddress address of datagram source
-   *   @param sourcePort port of data source
-   *   @return number of bytes received and -1 for error
-   *   @exception SocketException thrown if unable to receive datagram
-   */
-  int recvFrom(void *buffer, int bufferLen, std::string &sourceAddress, 
-               unsigned short &sourcePort) throw(SocketException);
-
-  /**
-   *   Set the multicast TTL
-   *   @param multicastTTL multicast TTL
-   *   @exception SocketException thrown if unable to set TTL
-   */
-  void setMulticastTTL(unsigned char multicastTTL) throw(SocketException);
-
-  /**
-   *   Join the specified multicast group
-   *   @param multicastGroup multicast group address to join
-   *   @exception SocketException thrown if unable to join group
-   */
-  void joinGroup(const std::string &multicastGroup) throw(SocketException);
-
-  /**
-   *   Leave the specified multicast group
-   *   @param multicastGroup multicast group address to leave
-   *   @exception SocketException thrown if unable to leave group
-   */
-  void leaveGroup(const std::string &multicastGroup) throw(SocketException);
-
-private:
-  void setBroadcast();
 };
 }//end of namespace
 #endif
