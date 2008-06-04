f6f411d4ac6d7e7cd57a44695573882cf00d3a92
dan-muresan <>
2008-06-04 09:37:38 +0000

    Fixed fn:doc() breakage

--- src/runtime/sequences/SequencesImpl.cpp
+++ src/runtime/sequences/SequencesImpl.cpp
@@ -1416,7 +1416,7 @@ static store::Item_t get_doc(xqpStringStore_t& uriString, const char **err)
   if (doc == NULL)
   {
     xqpStringStore_t lowercaseUri = uriString->lowercase();
-    if (lowercaseUri->byteStartsWith ("http://") == 0)
+    if (lowercaseUri->byteStartsWith ("http://"))
     {
 #ifdef HAVE_CURL_H
       // retrieve web file
