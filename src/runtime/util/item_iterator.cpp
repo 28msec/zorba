1ae9f37e4c40670a6c8f099011610c62bbbe8add
markosza <>
2008-06-05 03:54:42 +0000

    forgot to add files

---
+++ src/runtime/util/item_iterator.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2006-2008 The FLWOR Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "runtime/util/item_iterator.h"
+#include "store/api/item.h"
+
+namespace zorba { 
+
+ItemIterator::ItemIterator(std::vector<store::Item_t> aItems) 
+  :
+  theItems(aItems)
+{
+}
+
+
+ItemIterator::ItemIterator(store::Item_t aItem) 
+{
+  theItems.push_back(aItem);
+}
+
+
+ItemIterator::ItemIterator() 
+{
+}
+
+
+void
+ItemIterator::open() 
+{
+  theIterator = theItems.begin();
+}
+
+
+bool
+ItemIterator::next(store::Item_t& result) 
+{
+  if (theIterator == theItems.end()) 
+  {
+    result = NULL;
+    return false;
+  }
+  else
+  {
+    result = *(theIterator++);
+    return true;
+  }
+}
+
+
+void
+ItemIterator::reset() 
+{
+  theIterator = theItems.begin();
+}
+
+
+void
+ItemIterator::close() 
+{
+}
+
+} // namespace zorba
---
+++ src/runtime/util/item_iterator.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2006-2008 The FLWOR Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef ZORBA_RUNTIME_ITEM_ITERATOR_H
+#define ZORBA_RUNTIME_ITEM_ITERATOR_H
+
+#include <vector>
+#include "store/api/iterator.h"
+#include "common/shared_types.h"
+
+namespace zorba { 
+
+class ItemIterator : public store::Iterator
+{
+private:
+  std::vector<store::Item_t> theItems;
+  std::vector<store::Item_t>::iterator theIterator;
+
+public:
+  ItemIterator(std::vector<store::Item_t> aItems);
+  ItemIterator(store::Item_t aItem);
+  ItemIterator(); // construct the empty sequence
+
+  virtual ~ItemIterator(){}
+
+  virtual void open();
+  virtual bool next(store::Item_t&);
+  virtual void reset();
+  virtual void close();
+};
+
+} // namespace zorba
+
+#endif
