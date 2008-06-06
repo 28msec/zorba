43841ed66d449dae19b17bcd7c8afeacb2c6e57f
davidagraf <>
2008-06-06 09:13:09 +0000

    reverted introduction of string_utils

--- src/zorbatypes/collation_manager.cpp
+++ src/zorbatypes/collation_manager.cpp
@@ -30,6 +30,34 @@
 
 namespace zorba {
 
+  /**
+   * Method splits the passed string into tokes. 
+   * Delimiters are all characters passed in the variable delims.
+   */
+  std::vector<std::string> std_string_tokenize(const std::string& str,
+      const std::string& delims)
+  {
+    // Skip delims at beginning, find start of first token
+    std::string::size_type lastPos = str.find_first_not_of(delims, 0);
+    // Find next delimiter @ end of token
+    std::string::size_type pos     = str.find_first_of(delims, lastPos);
+
+    // output vector
+    std::vector<std::string> tokens;
+
+    while (std::string::npos != pos || std::string::npos != lastPos)
+    {
+      // Found a token, add it to the vector.
+      tokens.push_back(str.substr(lastPos, pos - lastPos));
+      // Skip delims.  Note the "not_of". this is beginning of token
+      lastPos = str.find_first_not_of(delims, pos);
+      // Find next delimiter at end of token.
+      pos     = str.find_first_of(delims, lastPos);
+    }
+
+    return tokens;
+  }
+                                          
 
   XQPCollator::XQPCollator(void* aCollator)
   : theCollator((Collator*)aCollator) {}
