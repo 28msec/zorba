25ce6ed2b4330145e139d9e6f8476a569cbc42aa
spungi <>
2008-06-17 07:50:21 +0000

    Added a new test that creates multiple queries all using a single document loaded into the store.

--- test/unit/multithread.cpp
+++ test/unit/multithread.cpp
@@ -25,8 +25,15 @@ using namespace zorba;
 
 void* query_thread_1(void *param);
 void* query_thread_2(void *param);
+void* query_thread_3(void *param);
 
 #define NR_THREADS  20
+
+struct data {
+  Zorba*  lZorba;
+  Item    lItem;
+} dataObj;
+
 static  XQuery_t    lQuery;
 
 #ifdef ZORBA_HAVE_PTHREAD_H
@@ -98,12 +105,39 @@ multithread_example_2(Zorba* aZorba)
 
 
 /*
-Create 2 separate threads that are working with the same document.
+Create separate queries that are working with the same document loaded in store.
 */
 bool
-multithread_example_3(Zorba* zZorba)
+multithread_example_3(Zorba* aZorba)
 {
-  return false;
+  unsigned int  i;
+  pthread_t     pt[NR_THREADS];
+
+  try {
+    std::stringstream lInStream("<books><book>Book 1</book><book>Book 2</book><book>Book 3</book></books>");
+
+    Item aItem = aZorba->getXmlDataManager()->loadDocument("books.xml", lInStream);
+
+    dataObj.lZorba = aZorba;
+    dataObj.lItem = aItem;
+    
+    for(i=0; i<NR_THREADS; i++)
+    {
+      pthread_create(&pt[i], NULL, query_thread_3, (void*)(&dataObj));
+    }
+
+    for(i=0;i<NR_THREADS;i++)
+    {
+      void  *thread_result;
+      pthread_join(pt[i], &thread_result);
+    }
+
+    return true;
+  }
+  catch (ZorbaException &e) {
+    std::cerr << "some exception " << e << std::endl;
+    return false;
+  }
 }
 
 int
@@ -133,15 +167,15 @@ multithread(int argc, char* argv[])
   }
   else std::cout << "Passed" << std::endl;
 
-//   std::cout << std::endl  << "executing multithread test 3 : ";
-//   res = multithread_example_3(lZorba);
-//   if (!res) {
-//     std::cout << "Failed" << std::endl;
-//     lZorba->shutdown();
-//     inmemorystore::InMemoryStore::shutdown(lStore);
-//     return 1;
-//   }
-//   else std::cout << "Passed" << std::endl;
+  std::cout << std::endl  << "executing multithread test 3 : ";
+  res = multithread_example_3(lZorba);
+  if (!res) {
+    std::cout << "Failed" << std::endl;
+    lZorba->shutdown();
+    inmemorystore::InMemoryStore::shutdown(lStore);
+    return 1;
+  }
+  else std::cout << "Passed" << std::endl;
   
   lZorba->shutdown();
   inmemorystore::InMemoryStore::shutdown(lStore);
@@ -183,4 +217,20 @@ void* query_thread_2(void *param)
   return (void*)0;
 }
 
+
+void* query_thread_3(void *param)
+{
+  data* var = (data*)param;
+
+  XQuery_t lQuery = var->lZorba->compileQuery("declare variable $var external; doc('books.xml')//book");
+
+  DynamicContext* lCtx = lQuery->getDynamicContext();
+
+  lCtx->setVariable("var", var->lItem);
+
+  std::ostringstream os;
+  os << lQuery << std::endl;
+
+  return (void*)0;
+}
 #endif
