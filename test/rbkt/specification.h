5d7a2f42791aab594499585a068d5706649d706b
wcandillon <>
2008-08-28 14:19:41 +0000

    Remove boost:spirit parser.

--- test/rbkt/specification.h
+++ test/rbkt/specification.h
@@ -18,16 +18,12 @@
 
 #include <vector>
 #include <string>
-
-#include <boost/spirit.hpp>
-#include <boost/bind.hpp>
+#include <fstream>
 
 class Specification
 {
   typedef char char_t;
-  typedef boost::spirit::file_iterator <char_t> iterator_t;
-  typedef boost::spirit::scanner<iterator_t> scanner_t;
-  typedef boost::spirit::rule<scanner_t> rule_t;
+  typedef std::string::iterator iterator_t;
 
 public:
   struct Variable {
@@ -44,7 +40,7 @@ private:
   std::vector<std::string> theErrors;
   std::string theDate, theTimezone;
 
-  void setInline(iterator_t, iterator_t) {
+  void setInline() {
     theInline = true;
   }
 
@@ -56,7 +52,7 @@ private:
     theVarValue = std::string(str, end);
   }
 
-  void addVariable(iterator_t str, iterator_t end) {
+  void addVariable() {
     Variable args = { theInline, theVarName, theVarValue};
     theVariables.push_back(args);
   }
@@ -109,56 +105,82 @@ public:
     return theTimezone;
   }
 
-
-  bool
-  parseFile(std::string str)
+  void tokenize(const std::string& str,
+                std::vector<std::string>& tokens,
+                const std::string& delimiters)
   {
-    rule_t props
-      = (+(boost::spirit::graph_p & ~boost::spirit::ch_p(':') & ~boost::spirit::ch_p('=')))[bind(&Specification::setVarName, this, _1, _2)]
-      >> boost::spirit::str_p("=")[bind(&Specification::setInline, this, _1, _2)]
-      >> (+boost::spirit::graph_p)[bind(&Specification::setVarValue, this, _1, _2)];
+    // Skip delimiters at beginning.
+    std::string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+    // Find first "non-delimiter".
+    std::string::size_type pos     = str.find_first_of(delimiters, lastPos);
 
-    rule_t propsInline
-      = (+(boost::spirit::graph_p & ~boost::spirit::ch_p(':') & ~boost::spirit::ch_p('=')))[bind(&Specification::setVarName, this, _1, _2)]
-      >> boost::spirit::str_p(":=") 
-      >> (+boost::spirit::graph_p)[bind(&Specification::setVarValue, this, _1, _2)];
-    
-    rule_t argument 
-      =  boost::spirit::str_p("Args:") 
-      >> +(
-            boost::spirit::blank_p
-         && boost::spirit::str_p("-x")
-         && boost::spirit::blank_p
-         && (props | propsInline)[bind(&Specification::addVariable, this, _1, _2)]
-         )
-      >> !boost::spirit::eol_p;
+    while (std::string::npos != pos || std::string::npos != lastPos)
+    {
+      // Found a token, add it to the vector.
+      tokens.push_back(str.substr(lastPos, pos - lastPos));
+      // Skip delimiters.  Note the "not_of"
+      lastPos = str.find_first_not_of(delimiters, pos);
+      // Find next "non-delimiter"
+      pos = str.find_first_of(delimiters, lastPos);
+    }
+  }
 
-    rule_t error  
-      =  boost::spirit::str_p("Error:") 
-      >> !boost::spirit::blank_p
-      >> (*boost::spirit::print_p)[bind(&Specification::addError, this, _1, _2)] 
-      >> !boost::spirit::eol_p;
-
-    rule_t date
-      =  boost::spirit::str_p("Date:") 
-      >> !boost::spirit::blank_p
-      >> (*boost::spirit::print_p)[bind(&Specification::setDate, this, _1, _2)] 
-      >> !boost::spirit::eol_p;
-    rule_t timezone
-      =  boost::spirit::str_p("Timezone:") 
-      >> !boost::spirit::blank_p
-      >> (*boost::spirit::print_p)[bind(&Specification::setTimezone, this, _1, _2)] 
-      >> !boost::spirit::eol_p;
-
-    rule_t defs = *( argument | error | date | timezone );
-
-    iterator_t first(str.c_str());
-    if (!first)
-      return false;
-
-    iterator_t last = first.make_end();
-
-    return boost::spirit::parse(first, last, defs).full; 
+  bool parseFile(std::string str)
+  {
+    std::ifstream lFile(str.c_str(), std::ifstream::in);
+    std::stringstream lContent;
+    while(lFile.good()){
+      lContent << (char)lFile.get();
+    }
+    lFile.close();
+    std::vector<std::string> lines;
+    std::vector<std::string>::iterator it;
+    tokenize(lContent.str(), lines, "\n");
+    for(it=lines.begin(); it!=lines.end(); ++it)
+    {
+      std::vector<std::string> tokens;
+      std::vector<std::string>::iterator lIter;
+      tokenize(*it, tokens, " ");
+      for(lIter=tokens.begin(); lIter!=tokens.end(); ++lIter)
+      {      
+        if( *lIter == "Args:" )
+        {
+          for(++lIter;lIter!=tokens.end(); ++lIter)
+          {
+            if(*lIter != "-x"){ return false; }
+            ++lIter;
+            if(lIter->find(':') == std::string::npos){ setInline(); }
+            if(lIter->find('=') == std::string::npos){ return false; }
+            if( theInline ){
+              setVarName(lIter->begin(), lIter->begin()+lIter->find('='));
+            } else {
+              setVarName(lIter->begin(), lIter->begin()+lIter->find(':'));
+            }
+            setVarValue(lIter->begin()+lIter->find("=")+1, lIter->end());
+            addVariable();
+          }
+          break;
+        } else if ( *lIter == "Error:" ) { 
+          ++lIter;
+          addError(lIter->begin(), lIter->end());
+        } else if( *lIter == "Date:" ) {
+          ++lIter;
+          setDate( lIter->begin(), lIter->end() );
+        } else if ( *lIter == "Timezone:" ) {
+          ++lIter;
+          setTimezone(lIter->begin(), lIter->end());
+        } else if ( lIter->find("Error:") != std::string::npos ) {
+          addError(lIter->begin()+lIter->find(":")+1, lIter->end());
+        } else if ( lIter->find("Date:") != std::string::npos  ) {
+          setDate(lIter->begin()+lIter->find(":")+1, lIter->end());
+        } else if ( lIter->find("Timezone:") != std::string::npos ) {
+          setTimezone(lIter->begin()+lIter->find(":")+1, lIter->end());
+        } else {
+          break;
+        }
+      }
+    }
+    return true;
   }
 };
 #endif
