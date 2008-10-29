a3b0ee3715bb232bfab3a584cb735728bbfcc3ce
markosza <>
2008-10-29 19:37:54 +0000

    temp fix for windows

--- src/zorbautils/lock.h
+++ src/zorbautils/lock.h
@@ -78,11 +78,16 @@ public:
   }
 
   ~Lock();
-    
+
+#ifdef ZORBA_HAVE_PTHREAD_H    
   void rlock();
   void wlock();
-
   void unlock();
+#else
+  void rlock() {}
+  void wlock() {}
+  void unlock() {}
+#endif
 
  private:
   Lock(const Lock &);
