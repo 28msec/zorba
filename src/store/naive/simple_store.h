d652cb79a96d9f470621086655784279109a548d
danielturcanu <>
2008-10-30 16:22:07 +0000

    Fixed simple_store.h for single threaded mode.

--- src/store/naive/simple_store.h
+++ src/store/naive/simple_store.h
@@ -102,7 +102,7 @@ protected:
   CollectionSet                 theCollections;
   checked_vector<store::Item_t> theItemUris;
 
-  Lock                          theGlobalLock;
+  SYNC_CODE(Lock                          theGlobalLock;)
 
   long                          theTraceLevel;
 
@@ -123,7 +123,7 @@ public:
   StringPool& getNamespacePool() const    { return *theNamespacePool; }
   QNamePool& getQNamePool() const         { return *theQNamePool; }
 
-  Lock& getGlobalLock()                   { return theGlobalLock; }
+  SYNC_CODE(Lock& getGlobalLock()                   { return theGlobalLock; })
 
   long getTraceLevel() const              { return theTraceLevel; }
 
