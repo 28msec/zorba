0137d0590cdedbabf38332709465ef32132b7263
mbrantner <>
2008-10-30 15:40:28 +0000

    fix for finding oxs libraries

--- scripts/find_macosx_libraries.sh
+++ scripts/find_macosx_libraries.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 file=$1
-echo $file
 while [ -L $file ]
 do
   currentdir=$(dirname $file)
   file=$( readlink "$file" )
-  echo ";$(cd "${currentdir}";pwd)/$(basename "$file")"
 done
-
+echo ";$(cd "${currentdir}";pwd)/$(basename "$file")"
