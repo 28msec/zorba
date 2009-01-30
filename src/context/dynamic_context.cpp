ff032514924536280824dc3838d253dfdd671c51
nbrinza <>
2009-01-30 03:51:26 +0000

    Fixed the implicit timezone. It is now retrieved from libc and preserved in the dynamic context

--- src/context/dynamic_context.cpp
+++ src/context/dynamic_context.cpp
@@ -66,9 +66,9 @@ dynamic_context::dynamic_context(dynamic_context *parent)
     localtime_r(&timebuffer.time, &gmtm); //thread safe localtime on Linux
 #endif
 
-      implicit_timezone = 0;
+    implicit_timezone = -timebuffer.timezone / 60;
 
-      GENV_ITEMFACTORY->createDateTime(current_date_time_item, gmtm.tm_year + 1900, gmtm.tm_mon + 1, gmtm.tm_mday, 
+    GENV_ITEMFACTORY->createDateTime(current_date_time_item, gmtm.tm_year + 1900, gmtm.tm_mon + 1, gmtm.tm_mday, 
 		  gmtm.tm_hour, gmtm.tm_min, gmtm.tm_sec + timebuffer.millitm/1000.0, implicit_timezone);
     
 	  ctxt_position = 0;
