3cf349ea6b49a3a4b7636785c7644d4bcd7bb036
pfkeb <>
2009-03-06 00:49:54 +0000

    Most of the java example infrastruture, not complete

---
+++ doc/java/examples/Test_Zorba.java
@@ -0,0 +1,8 @@
+public class Test_Zorba {
+    static {
+	System.loadLibrary ( "zorba_api" );
+    }
+    public static void main ( String argv[] ) {
+	System.out.println ( "Hello World" );
+    }
+}
