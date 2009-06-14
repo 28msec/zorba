df42cc65df29cc90f987d492155071eac3e2d991
vinayakb <>
2009-06-14 20:41:52 +0000

    Added a comment to describe the next steps for indexes.

--- src/compiler/indexing/index_tools.cpp
+++ src/compiler/indexing/index_tools.cpp
@@ -49,6 +49,14 @@ static bool isHoistableCollection(expr *e, static_context *sCtx)
   return isConstant(arg);
 }
 
+static xqpStringStore *getCollectionName(expr *e)
+{
+  fo_expr *fo = static_cast<fo_expr *>(e);
+  const_expr *arg = static_cast<const_expr *>((*fo)[0].getp());
+  store::Item *val = arg->get_val();
+  return val->getStringValueP();
+}
+
 typedef std::vector<std::pair<expr_t, expr_t> > hoisted_collections_t;
 
 static rchandle<var_expr> createTempLetVar(const QueryLoc& loc, int counter)
@@ -74,6 +82,7 @@ static expr_t hoistCollectionSources(expr_t e, static_context *sCtx, hoisted_col
   expr_iterator i = e->expr_begin();
   while(!i.done()) {
     *i = hoistCollectionSources(*i, sCtx, h, count);
+    ++i;
   }
   return e;
 }
@@ -104,6 +113,7 @@ static bool isMapInVar(expr *e, var_expr *var)
             isMap = isMap || isMapInVar(static_cast<for_clause *>(clause)->get_expr(), var);
 
           case flwor_clause::let_clause:
+          case flwor_clause::where_clause:
           case flwor_clause::order_clause:
             continue;
 
@@ -135,16 +145,42 @@ void IndexTools::inferIndexCreators(ValueIndex *vi)
   if (h.size() > 1) {
     return;
   }
+  /*
+
+    Commenting out for now. But these are the things that need to happen here:
+      0. Create an expression that is of the following form:
+        let $t := $externalVar
+        for $de in $t
+        let $k1 := key1($de)
+        let $k2 := key1($de)
+        .
+        .
+        .
+        return createTuple($k1, $k2, ... $de)
+
+      1. For each substituted variable that the DE is a map in, replace it
+      with an external variable
+      2. Create an IndexCreator that is capable of evaluating this expression
+      with the external variable set to the given item in the appendIndexEntries() call.
+      3. For each tuple that is returned by this plan, create an IndexEntry containing
+      the contents of the tuple.
+
+
+
   expr_t e = new flwor_expr(de->get_loc(), true);
   flwor_expr *f = static_cast<flwor_expr *>(e.getp());
   for(hoisted_collections_t::iterator i = h.begin(); i != h.end(); ++i) {
     var_expr *var = static_cast<var_expr *>(i->first.getp());
+    expr *ce = i->second.getp();
+    xpqStringStore *cName = getCollectionName(ce);
+
     let_clause_t lc = new let_clause(i->first->get_loc(), var, i->second.getp());
     var->set_flwor_clause(lc.getp());
 
     f->add_clause(lc.getp());
   }
   f->set_return_expr(he.getp());
+  */
 }
 
 }
