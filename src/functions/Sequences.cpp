5d1771585962827a16530aadf92879eba82b39b5
markosza <>
2009-07-03 15:50:29 +0000

    cosmetic

--- src/functions/Sequences.cpp
+++ src/functions/Sequences.cpp
@@ -27,7 +27,14 @@
 #include "store/api/iterator.h"
 
 using namespace std;
-namespace zorba {
+
+
+namespace zorba 
+{
+
+
+#define TSV_TRUE TSVAnnotationValue::TRUE_VAL
+#define TSV_TRUE_P TSVAnnotationValue::TRUE_VAL.getp()
 
 
   /*______________________________________________________________________
@@ -446,31 +453,37 @@ PlanIter_t fn_except::codegen (const QueryLoc& loc, std::vector<PlanIter_t>& arg
 #define A_DISTINCT ((a) [2])
 #define A_ASCENDING ((a) [3])
 
-PlanIter_t op_node_sort_distinct::codegen (const QueryLoc& loc, std::vector<PlanIter_t>& argv, AnnotationHolder& ann) const {
-  const bool *a = action ();
-  bool distinct = A_DISTINCT;
-  if (! A_SORT)
-    return distinct ? new NodeDistinctIterator (loc, argv [0], A_ATOMICS)
-      : (A_ATOMICS ? new EitherNodesOrAtomicsIterator (loc, argv) : argv [0]);
-  else
-    return new NodeSortIterator (loc, argv [0], A_ASCENDING, distinct, A_ATOMICS);
-}
 
-void op_node_sort_distinct::compute_annotation (AnnotationHolder *parent, std::vector<AnnotationHolder *> &kids, Annotation::key_t k) const {
-  const bool *a = action ();
+void op_node_sort_distinct::compute_annotation(
+    AnnotationHolder* parent,
+    std::vector<AnnotationHolder *>& kids,
+    Annotation::key_t k) const 
+{
+  const bool*a  = action ();
 
-  switch (k) {
+  switch (k) 
+  {
   case AnnotationKey::IGNORES_SORTED_NODES:
-  case AnnotationKey::IGNORES_DUP_NODES: {
-    bool ignores = (parent->get_annotation (k).getp() == TSVAnnotationValue::TRUE_VAL.getp() || (k == AnnotationKey::IGNORES_SORTED_NODES ? A_SORT : A_DISTINCT));
-    TSVAnnotationValue::update_annotation (kids [src], k, TSVAnnotationValue::from_bool (ignores));
+  case AnnotationKey::IGNORES_DUP_NODES: 
+  {
+    bool ignores = 
+    (parent->get_annotation(k).getp() == TSV_TRUE_P ||
+     (k == AnnotationKey::IGNORES_SORTED_NODES ? A_SORT : A_DISTINCT));
+
+    TSVAnnotationValue::update_annotation(kids[src],
+                                          k,
+                                          TSVAnnotationValue::from_bool(ignores));
     break;
   }
 
   case AnnotationKey::PRODUCES_SORTED_NODES:
-  case AnnotationKey::PRODUCES_DISTINCT_NODES: {
-    bool produces = (kids [0]->get_annotation (k).getp() == TSVAnnotationValue::TRUE_VAL.getp() || (k == AnnotationKey::PRODUCES_SORTED_NODES ? A_SORT : A_DISTINCT));
-    parent->put_annotation (k, TSVAnnotationValue::from_bool (produces));
+  case AnnotationKey::PRODUCES_DISTINCT_NODES: 
+  {
+    bool produces =
+    (kids [0]->get_annotation(k).getp() == TSV_TRUE_P ||
+     (k == AnnotationKey::PRODUCES_SORTED_NODES ? A_SORT : A_DISTINCT));
+
+    parent->put_annotation(k, TSVAnnotationValue::from_bool(produces));
     break;
   }
 
@@ -478,11 +491,41 @@ void op_node_sort_distinct::compute_annotation (AnnotationHolder *parent, std::v
   }
 }
 
-const function *op_node_sort_distinct::op_for_action (const static_context *sctx, const bool *a, const AnnotationHolder *parent, const AnnotationHolder *child, nodes_or_atomics_t noa) {
-#define LOOKUP_OP1( local ) (sctx->lookup_builtin_fn ((xqp_string (":") + local).c_str (), 1))
-  bool distinct = A_DISTINCT 
-    && (parent == NULL || parent->get_annotation (AnnotationKey::IGNORES_DUP_NODES) != TSVAnnotationValue::TRUE_VAL)
-    && (child == NULL || child->get_annotation (AnnotationKey::PRODUCES_DISTINCT_NODES) != TSVAnnotationValue::TRUE_VAL);
+
+PlanIter_t op_node_sort_distinct::codegen(
+    const QueryLoc& loc,
+    std::vector<PlanIter_t>& argv,
+    AnnotationHolder& ann) const 
+{
+  const bool* a = action();
+  bool distinct = A_DISTINCT;
+  if (! A_SORT)
+    return distinct ? new NodeDistinctIterator (loc, argv [0], A_ATOMICS)
+      : (A_ATOMICS ? new EitherNodesOrAtomicsIterator (loc, argv) : argv [0]);
+  else
+    return new NodeSortIterator(loc, argv [0], A_ASCENDING, distinct, A_ATOMICS);
+}
+
+
+const function* op_node_sort_distinct::op_for_action(
+    const static_context* sctx,
+    const bool* a,
+    const AnnotationHolder* parent,
+    const AnnotationHolder* child,
+    nodes_or_atomics_t noa) 
+{
+#define LOOKUP_OP1(local) (sctx->lookup_builtin_fn((xqp_string(":") + local).c_str(), 1))
+
+  Annotation::key_t ignoresSortedNodes = AnnotationKey::IGNORES_SORTED_NODES;
+  Annotation::key_t producesSortedNodes = AnnotationKey::PRODUCES_SORTED_NODES;
+  Annotation::key_t ignoresDupNodes = AnnotationKey::IGNORES_DUP_NODES;
+  Annotation::key_t producesDistinctNodes = AnnotationKey::PRODUCES_DISTINCT_NODES;
+
+  bool distinct = (A_DISTINCT  &&
+                   (parent == NULL ||
+                    parent->get_annotation(ignoresDupNodes) != TSV_TRUE) &&
+                   (child == NULL ||
+                    child->get_annotation(producesDistinctNodes) != TSV_TRUE));
 
   if (A_ATOMICS && noa == ATOMICS)
     return NULL;
@@ -490,26 +533,42 @@ const function *op_node_sort_distinct::op_for_action (const static_context *sctx
   bool atomics = A_ATOMICS && noa == MIXED;
 
 #if 0
-  cout << "op_for_action parent " << parent << " child " << child << " A_SORT " << A_SORT << " parent_ignores_sorted " << (parent != NULL && parent->get_annotation (AnnotationKey::IGNORES_SORTED_NODES) == TSVAnnotationValue::TRUE_VAL) << " child_prod_sorted " << (child != NULL && child->get_annotation (AnnotationKey::PRODUCES_SORTED_NODES) == TSVAnnotationValue::TRUE_VAL) << endl;
+  cout << "op_for_action parent " << parent << " child " << child
+       << " A_SORT " << A_SORT << " parent_ignores_sorted "
+       << (parent != NULL && parent->get_annotation(AnnotationKey::IGNORES_SORTED_NODES) == TSV_TRUE)
+       << " child_prod_sorted " << (child != NULL && child->get_annotation(AnnotationKey::PRODUCES_SORTED_NODES) == TSV_TRUE) << endl;
 #endif
-  if (! A_SORT
-      || (parent != NULL && parent->get_annotation (AnnotationKey::IGNORES_SORTED_NODES).getp() == TSVAnnotationValue::TRUE_VAL.getp())
-      || (child != NULL && child->get_annotation (AnnotationKey::PRODUCES_SORTED_NODES).getp() == TSVAnnotationValue::TRUE_VAL.getp()))
-#if 1  // trust NodeDistinctIterator
-    return distinct ? LOOKUP_OP1 ("distinct-nodes" + (atomics ? "-or-atomics" : ""))
-#else
-    return distinct ? LOOKUP_OP1 ("sort-distinct-nodes-asc" + (atomics ? "-or-atomics" : "ending"))
-#endif
-      : (atomics ? LOOKUP_OP1 ("either-nodes-or-atomics") : NULL);
+
+  if (! A_SORT ||
+      (parent != NULL &&
+       parent->get_annotation(ignoresSortedNodes).getp() == TSV_TRUE_P) ||
+      (child != NULL &&
+       child->get_annotation(producesSortedNodes).getp() == TSV_TRUE_P &&
+       A_ASCENDING))
+  {
+    return (distinct ?
+            LOOKUP_OP1 ("distinct-nodes" + (atomics ? "-or-atomics" : ""))
+            :
+            (atomics ? LOOKUP_OP1 ("either-nodes-or-atomics") : NULL));
+  }
+
   xqp_string part1 = xqpString::concat("sort-", (distinct ? "distinct-" : ""), "nodes-");
-  xqp_string part2 = xqpString::concat( A_ASCENDING ? "asc" : "desc", (atomics ? "-or-atomics" : "ending"));
+  xqp_string part2 = xqpString::concat( A_ASCENDING ? "asc" : "desc", (atomics ? "-or-atomics" : ""));
+
   return LOOKUP_OP1 (part1 + part2);
+
 #undef LOOKUP_OP1
 }
 
-const function *op_node_sort_distinct::min_action (const static_context *sctx, const AnnotationHolder *self, const AnnotationHolder *child, nodes_or_atomics_t noa) const {
-  return op_for_action (sctx, action (), self, child, noa);
+const function *op_node_sort_distinct::min_action(
+    const static_context *sctx,
+    const AnnotationHolder *self,
+    const AnnotationHolder *child,
+    nodes_or_atomics_t noa) const 
+{
+  return op_for_action (sctx, action(), self, child, noa);
 }
+
 
 PlanIter_t fn_unordered::codegen (const QueryLoc& loc, std::vector<PlanIter_t>& argv, AnnotationHolder &) const
 {
@@ -742,7 +801,7 @@ DECL(sctx, op_distinct_nodes_or_atomics,
   GENV_TYPESYSTEM.ITEM_TYPE_STAR));
 
 DECL(sctx, op_sort_nodes_ascending,
-  (createQName(XQUERY_FN_NS,"fn",":sort-nodes-ascending"),
+  (createQName(XQUERY_FN_NS,"fn",":sort-nodes-asc"),
   GENV_TYPESYSTEM.ANY_NODE_TYPE_STAR,
   GENV_TYPESYSTEM.ANY_NODE_TYPE_STAR));
 
@@ -752,7 +811,7 @@ DECL(sctx, op_sort_nodes_asc_or_atomics,
   GENV_TYPESYSTEM.ITEM_TYPE_STAR));
 
 DECL(sctx, op_sort_nodes_descending,
-  (createQName(XQUERY_FN_NS,"fn",":sort-nodes-descending"),
+  (createQName(XQUERY_FN_NS,"fn",":sort-nodes-desc"),
   GENV_TYPESYSTEM.ANY_NODE_TYPE_STAR,
   GENV_TYPESYSTEM.ANY_NODE_TYPE_STAR));
 
@@ -762,7 +821,7 @@ DECL(sctx, op_sort_nodes_desc_or_atomics,
   GENV_TYPESYSTEM.ITEM_TYPE_STAR));
 
 DECL(sctx, op_sort_distinct_nodes_ascending,
-  (createQName(XQUERY_FN_NS,"fn",":sort-distinct-nodes-ascending"),
+  (createQName(XQUERY_FN_NS,"fn",":sort-distinct-nodes-asc"),
   GENV_TYPESYSTEM.ANY_NODE_TYPE_STAR,
   GENV_TYPESYSTEM.ANY_NODE_TYPE_STAR));
 
@@ -772,7 +831,7 @@ DECL(sctx, op_sort_distinct_nodes_asc_or_atomics,
   GENV_TYPESYSTEM.ITEM_TYPE_STAR));
 
 DECL(sctx, op_sort_distinct_nodes_descending,
-  (createQName(XQUERY_FN_NS,"fn",":sort-distinct-nodes-descending"),
+  (createQName(XQUERY_FN_NS,"fn",":sort-distinct-nodes-desc"),
   GENV_TYPESYSTEM.ITEM_TYPE_STAR,
   GENV_TYPESYSTEM.ITEM_TYPE_STAR));
 
