0a546b6a6379666e119edd7094c261aeb2232a69
markosza <>
2009-07-16 16:04:34 +0000

    made loadProlog work again + separated context_var kind to prolog_var and local_var kinds

--- src/context/static_context.cpp
+++ src/context/static_context.cpp
@@ -1085,5 +1085,15 @@ static_context::get_trace_stream() const
 }
 
 
+void static_context::get_global_bindings (std::list<global_binding> &bs) {
+  if (parent != NULL)
+    static_cast<static_context *> (parent)->get_global_bindings (bs);
+  bs.insert (bs.end (), theGlobalVars.begin (), theGlobalVars.end ());
+}
+
+void static_context::set_global_bindings (const std::list<global_binding> &bs) {
+  theGlobalVars = bs;
+}
+
 } /* namespace zorba */
 
