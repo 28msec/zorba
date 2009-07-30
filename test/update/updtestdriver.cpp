d9ecd272c1e4deba81163e019d89c594c444f31e
markosza <>
2009-07-30 19:04:19 +0000

    temporarily disabled the loadProlog to make CDash tests pass

--- test/update/updtestdriver.cpp
+++ test/update/updtestdriver.cpp
@@ -213,10 +213,11 @@ main(int argc, char** argv)
         if (path.find("ValSkip") != std::string::npos ||
             path.find("ValLax") != std::string::npos) 
           lContext->setBoundarySpacePolicy(preserve_space);
-
+#if 0
       	zorba::String lProlog = zorba::String(std::string("import schema 'http://www.w3.org/XML/1998/namespace';\n"));
 	
         lContext->loadProlog(lProlog, getCompilerHints());
+#endif
       }
       
       zorba::XQuery_t lQuery = engine->createQuery();
