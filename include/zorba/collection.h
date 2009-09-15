3e51da15061e10441361887ac079ee8bc10554e2
markosza <>
2009-09-15 07:11:11 +0000

    linking nodes and collections

--- include/zorba/collection.h
+++ include/zorba/collection.h
@@ -33,99 +33,130 @@ namespace zorba {
  */
 class ZORBA_DLL_PUBLIC Collection : public SmartObject
 {
-  public:
-    /** \brief Destructor.
-     */
-    virtual ~Collection() {}
+ public:
+  /**
+   * \brief Destructor.
+   */
+  virtual ~Collection() {}
 
-    /** \brief Get the URI of a collection as an anyURI Item.
-     *
-     * @return Item the anyURI Item of the Collection.
-     */
-    virtual Item
-    getUri() const = 0;
+  /**
+   * \brief Get the URI of the collection as an anyURI Item.
+   *
+   * @return The URI of the Collection as an anyURI Item.
+   */
+  virtual Item
+  getUri() const = 0;
 
-    /** \brief Returns the number of items in the collection.
-     *
-     * @return The number of items in the collection.
-     */
-    virtual unsigned long
-    size() const = 0;
+  /** 
+   * \brief Returns the number of nodes in the collection.
+   *
+   * @return The number of nodes in the collection.
+   */
+  virtual unsigned long
+  size() const = 0;
 
-    /** \brief Adds a Copy of a Node Item to the Collection.
-     *
-     * Default copy behavior is used: construction mode == preserve, copy-namespace modes == preserve & inherit
-     *
-     * @param aNode the Node Item to add.
-     * @param aPosition the position where the node will be inserted.
-     * By default the data will be appended at the end.
-     * @return true if the Node Item was added to the Collection, false otherwise.
-     */
-    virtual bool
-    addNode(const Item& aNode, long aPosition = -1) = 0;
+  /** 
+   * \brief Adds the document retrieved from the given input stream to the Collection.
+   *
+   * @param aInStream The input stream from which to parse the document.
+   * @param aPosition The position where the node will be inserted at. Positions
+   * are numbered starting from 1. If a non-positive position is given, the
+   * document will be appended at the end.
+   * @return True if the document was added to the collection (e.g. was a
+   * valid document), false otherwise.
+   */
+  virtual bool
+  addDocument(std::istream& aInStream, long aPosition = -1) = 0;
+    
+  /**
+   * \brief Adds a copy of a node to the collection. The node must be the root
+   * of an xml tree (i.e., it must not have a parent). The copy is deep, i.e.,
+   * the whole tree rooted at the given node is copied.
+   *
+   * Default copy behavior is used: 
+   * - construction mode == preserve, 
+   * - copy-namespace modes == preserve & inherit
+   *
+   * @param aNode The node to add.
+   * @param aPosition The position where the node will be inserted. Positions
+   * are numbered starting from 1. If a non-positive position is given, the
+   * document will be appended at the end.
+   * @return True if the node was added to the collection, false otherwise.
+   */
+  virtual bool
+  addNode(Item& aNode, long aPosition = -1) = 0;
+  
+  /** \brief Adds a copy of a node to the Collection before or after another
+   * targetNode, which must be in the collection already. The node to add
+   * must be the root of an xml tree (i.e., it must not have a parent). The
+   * copy is deep, i.e., the whole tree rooted at the given node is copied.
+   *
+   * Default copy behavior is used:
+   * - construction mode == preserve,
+   * - copy-namespace modes == preserve & inherit
+   *
+   * @param aNode The node to add.
+   * @param aTargetNode The new node will be added before or after the target node.
+   * @param aBefore If true, the new node will be inserted before the target node.
+   * Otherwise the new node will be inserted after the target node.
+   * @return True if the node was added to the collection, false otherwise.
+   */
+  virtual bool
+  addNode(Item& aNode, const Item& aTargetNode, bool aBefore) = 0;
+  
+  /** 
+   * \brief Adds copies of the nodes retrieved from the given ResultIterator to
+   * the Collection. Each of the nodes to add must be root of an xml tree (i.e.,
+   * it must not have a parent). The copy is deep, i.e., the whole tree rooted
+   * at each retrieved node is copied.
+   *
+   * Default copy behavior is used:
+   * - construction mode == preserve, 
+   * - copy-namespace modes == preserve & inherit
+   *
+   * @param aResultIterator the ResultIterator that produces the nodes to add.
+   * @return true if all the nodes of the given ResultIterator were added to
+   * the Collection, false otherwise.
+   */
+  virtual bool
+  addNodes(const ResultIterator* aResultIterator) = 0;
 
-    /** \brief Adds a Copy of a Node Item to the Collection after the targetNode.
-     *
-     * Default copy behavior is used: construction mode == preserve, copy-namespace modes == preserve & inherit
-     *
-     * @param aNode the Node Item to add.
-     * @param aTargetNode the aNode will be added before the aTargetNode.
-     * @param sOrder if true the aNode will be inserted before the aTarget.
-     * Otherwise aNode will be inserted after aTarget.
-     * @return true if the Node Item was added to the Collection, false otherwise.
-     */
-    virtual bool
-    addNode(const Item& aNode, const Item& aTargetNode, bool aOrder) = 0;
+  /** 
+   * \brief Removes the given node from the collection.
+   *
+   * @param aNode The node to delete
+   * @return true if the given node was in the collection, false otherwise.
+   */
+  virtual bool
+  deleteNode(Item& aNode) = 0;
+  
+  /** 
+   * \brief Removes the node at the given position from the Collection.
+   *
+   * @param aPosition The position of the node that will be removed from collection.
+   * If a non-positive position is given, the last node is removed.
+   * @return true if the given Node Item was deleted, false otherwise.
+   */
+  virtual bool
+  deleteNode(long aPosition = -1) = 0;
+  
+  /** 
+   * \brief Get the node at the given position in the collection as a Node Item.
+   *
+   * @param aPosition The position of the node in the collection.
+   * @return the Node Item at the given position.
+   */
+  virtual Item
+  nodeAt(long aPosition) = 0;
+};
 
-    /** \brief Deletes the given Item from the Collection.
-     *
-     * @param aNode the Node Item to delete
-     * @return true if the given Node Item was deleted, false otherwise.
-     */
-    virtual bool
-    deleteNode(const Item& aNode) = 0;
-
-    /** \brief Deletes the Item at the given position from the Collection.
-     *
-     * @param aPosition the position of the Node that will be removed from collection.
-     * By default the last Node will be removed from collection.
-     * @return true if the given Node Item was deleted, false otherwise.
-     */
-    virtual bool
-    deleteNode(long aPosition = -1) = 0;
-
-    /** \brief Get the node at the given position in the collection as a Node Item.
-     *
-     * @param aPosition the position of the Node in the collection.
-     * @return the Node Item at the given position.
-     */
-    virtual Item
-    nodeAt(long aPosition) = 0;
-
-    /** \brief Adds Copies of the Node Items retrieved from the given ResultIterator to the Collection.
-     *
-     * Default copy behavior is used: construction mode == preserve, copy-namespace modes == preserve & inherit
-     *
-     * @param aResultIterator the ResultIterator that produces the Node Items to add.
-     * @return true if all the Node Items of the given ResultIterator were added to the Collection,
-     *         false otherwise.
-     */
-    virtual bool
-    addNodes(const ResultIterator* aResultIterator) = 0;
-
-    /** \brief Adds the document retrieved from the given input stream to the Collection.
-     *
-     * @param aInStream the input stream from which to parse the document.
-     * @param aPosition the position where the node will be inserted.
-     * By default the data will be appended at the end.
-     * @return true if the document was added to the collection (e.g. was a valid document), 
-     *         false otherwise.
-     */
-    virtual bool
-    addDocument(std::istream& aInStream, long aPosition = -1) = 0;
-
-}; /* class Collection */
-
+  
 } /* namespace zorba */
 
 #endif
+
+/*
+ * Local variables:
+ * mode: c++
+ * End:
+ */
