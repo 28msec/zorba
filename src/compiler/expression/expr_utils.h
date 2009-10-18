1287a1ae37fa59913d336e08e0eb26d494319795
markosza <>
2009-10-18 17:44:17 +0000

    implemented the latest syntax for index declaration

---
+++ src/compiler/expression/expr_utils.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2006-2008 The FLWOR Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ZORBA_COMPILER_EXPR_UTILS
+#define ZORBA_COMPILER_EXPR_UTILS
+
+namespace zorba 
+{
+
+/******************************************************************************
+  Used by the orderspecs of an order by clause, and by the keyspecs of an
+  index declaration.
+********************************************************************************/
+class OrderModifier : public SimpleRCObject
+{
+public:
+  bool         theAscending;
+  bool         theEmptyLeast;
+  std::string  theCollation;
+
+  SERIALIZABLE_ABSTRACT_CLASS(OrderModifier)
+  SERIALIZABLE_CLASS_CONSTRUCTOR2(OrderModifier, SimpleRCObject)
+  void serialize(::zorba::serialization::Archiver& ar)
+  {
+    ar & theAscending;
+    ar & theEmptyLeast;
+    ar & theCollation;
+  }
+
+public:
+  OrderModifier() 
+    :
+    theAscending(true),
+    theEmptyLeast(true)
+  {
+  }
+};
+
+
+}
+#endif
+
+/*
+ * Local variables:
+ * mode: c++
+ * End:
+ */
+/* vim:set ts=2 sw=2: */
+
