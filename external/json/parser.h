ade86829d07ee6ac6441428fa99cb80015ed68d3
spungi <>
2009-12-02 13:11:43 +0000

    Replaced TAB characters with SPACE according to https://sourceforge.net/apps/mediawiki/zorba/index.php?title=Coding_Guidelines#Code_Conventions, the rule "No tabs, use two spaces instead. For indentation, two spaces are used per level of indentation".

--- external/json/parser.h
+++ external/json/parser.h
@@ -5,89 +5,87 @@
 #include "value.h"
 
 namespace json{
-	
-	void UNI_NG_SEARCH_REPLACE(std::wstring &srcstr,const wchar_t *search,const wchar_t *replace);
-	namespace tt{
-		enum tokens{
-			_name,
-			_unquoted,
-			_object,
-			_array,
-			_comment,
-			_string,
-			_literal
-		};
-	}
 
-	struct error{
-		size_t row;
-		size_t column;
-		unsigned _errno;
-	};
-	
-	extern const wchar_t *errstrings[8];
+  void UNI_NG_SEARCH_REPLACE(std::wstring &srcstr,const wchar_t *search,const wchar_t *replace);
+  namespace tt{
+    enum tokens{
+      _name,
+      _unquoted,
+      _object,
+      _array,
+      _comment,
+      _string,
+      _literal
+    };
+  }
 
-	struct inputdata{
-			size_t col;
-			size_t row;
-			bool eof;
-			const void *data;
-			const void *buffer;
-			const void *cursor;	
-			size_t length;
-			wchar_t (*next)(inputdata *);
-			wchar_t (*prev)(inputdata *,int n);
-	};
+  struct error{
+    size_t row;
+    size_t column;
+    unsigned _errno;
+  };
 
-	class parser{
-	private:
-		int abortlevel;
-		
-		// Behaviour flags
-		inputdata si;
-		bool isescaped;
+  extern const wchar_t *errstrings[8];
 
-		wchar_t cursor;
-		wchar_t curchar;
+  struct inputdata{
+      size_t col;
+      size_t row;
+      bool eof;
+      const void *data;
+      const void *buffer;
+      const void *cursor;
+      size_t length;
+      wchar_t (*next)(inputdata *);
+      wchar_t (*prev)(inputdata *,int n);
+  };
 
-		datatype::dt findval();
-		value *readvalue(bool aValidate=true);
+  class parser{
+  private:
+    int abortlevel;
 
-		void readnumber(value *val);
-		void readstring(value *val);
-		void readlit(value *val);
-		void readarray(value *val);
-		void readobject(value *val);
+    // Behaviour flags
+    inputdata si;
+    bool isescaped;
 
-		std::wstring *readname();
+    wchar_t cursor;
+    wchar_t curchar;
 
-		inline void next();
-		inline void rewind(int n=1);
-		void getnextchar(bool,bool,bool,bool nonext=false);
+    datatype::dt findval();
+    value *readvalue(bool aValidate=true);
 
-		inline int readhexquadaswchar();
-		wchar_t escapechar();
-		bool eatcomment();
-		void adderror(int no);
-		
-	public:
-		bool allow_unquoted_names;
-		bool strictjson;
-		int fail();
+    void readnumber(value *val);
+    void readstring(value *val);
+    void readlit(value *val);
+    void readarray(value *val);
+    void readobject(value *val);
+
+    std::wstring *readname();
+
+    inline void next();
+    inline void rewind(int n=1);
+    void getnextchar(bool,bool,bool,bool nonext=false);
+
+    inline int readhexquadaswchar();
+    wchar_t escapechar();
+    bool eatcomment();
+    void adderror(int no);
+
+  public:
+    bool allow_unquoted_names;
+    bool strictjson;
+    int fail();
     std::wstring printerrors();
-		std::vector<error> errors;
-		parser(void);
-		
-		value *parse(const wchar_t *ins,size_t length);
-		value *parse(const char *ins,size_t length);
-		value *parse(const char *ins);
-		value *parse(std::istream &stream);
-		value *parse(std::wistream &stream);
-		~parser(void);
-		
-	private:
+    std::vector<error> errors;
+    parser(void);
 
-	};
+    value *parse(const wchar_t *ins,size_t length);
+    value *parse(const char *ins,size_t length);
+    value *parse(const char *ins);
+    value *parse(std::istream &stream);
+    value *parse(std::wistream &stream);
+    ~parser(void);
+
+  };
 
 
 }
