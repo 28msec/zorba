1f0ff1c6d63582dd20bd17e52bab3e6fcc6034d4
davidagraf <>
2009-12-15 12:37:22 +0000

    Till now, a lot of code had to be duplicated when an additional store was added to zorba. Therefore, I added functionality to create the following store dependent things automatically for each store registered in the CMakeCache.txt file: - creation of store dependent zorbacmd_*.cpp and zorba executable - creation of store dependent apitest_*.cpp and apitest executable - creation of store dependent testdriver_*.cpp and testdriver executable - creation of store dependent updtestdriver_*.cpp and updtestdriver executable - registration of rbkt test - registration of update tests - creation of external module library

---
+++ cmake_modules/ZorbaAddTest.cmake
@@ -0,0 +1,47 @@
+# Adds a test to the project.
+#
+# Syntax:
+#
+#   ZORBA_ADD_TEST_SIMPLE(NAME TARGET ...) 
+#   
+#     NAME - the name of the added test
+#     TARGET - target that will be executed when the test is run
+#     ... - arguments that will be passed to the target
+#
+MACRO(ZORBA_ADD_TEST_SIMPLE NAME TARGET ...)
+	SET(ARGS ${ARGV2} ${ARGN})
+  GET_TARGET_PROPERTY(TARGET_LOCATION ${TARGET} LOCATION)
+  ADD_TEST(${NAME} ${TARGET_LOCATION} ${ARGS})
+  SET_TESTS_PROPERTIES ("${NAME}" PROPERTIES TIMEOUT "${NORMAL_TIMEOUT_VALUE}")
+ENDMACRO(ZORBA_ADD_TEST_SIMPLE)
+
+# This macro easies the process of adding test for store dependent executables 
+# (e.g. if the rbkt tests must be added for the executables testdriver and testdriver_firststore). 
+# It adds one test for each store registered. It has the same naming convection for the target as 
+# the macro that is used to generated store dependent executables (thus, 
+# 'testdriver' must be passed as TARGET if the test should be executed with the executables
+# testdriver and testdriver_firststore).
+#
+# Syntax:
+#
+#   ZORBA_ADD_TEST_SIMPLE(NAME TARGET ...) 
+#   
+#     NAME - the name of the added test
+#     TARGET - target that will be executed when the test is run. For each registered store, 
+#              a suffix will be added to the target 
+#              (similar convention than in ZORBA_GENERATE_STORE_SPECIFIC_EXES)
+#     ... - arguments that will be passed to the target
+#
+MACRO(ZORBA_ADD_TEST_ALLSTORES NAME TARGET ARGS)
+  FOREACH(ZORBA_STORE_NAME ${ZORBA_STORE_NAMES})
+    # simplestore executable doesn't need an extension
+    SET(SUFFIX)
+    IF (NOT ${ZORBA_STORE_NAME} STREQUAL "simplestore")
+      SET(SUFFIX "_${ZORBA_STORE_NAME}")
+    ENDIF (NOT ${ZORBA_STORE_NAME} STREQUAL "simplestore")
+    GET_TARGET_PROPERTY(TARGET_LOCATION "${TARGET}${SUFFIX}" LOCATION)
+    ADD_TEST("${NAME}${SUFFIX}" ${TARGET_LOCATION} ${ARGS})
+    SET_TESTS_PROPERTIES ("${NAME}${SUFFIX}" PROPERTIES TIMEOUT "${NORMAL_TIMEOUT_VALUE}")
+  ENDFOREACH(ZORBA_STORE_NAME ${ZORBA_STORE_NAMES})
+ENDMACRO(ZORBA_ADD_TEST_ALLSTORES)
+
---
+++ cmake_modules/ZorbaGenerateStoreSpecificExes.cmake
@@ -0,0 +1,66 @@
+# This macro helps creating executables that are store dependent (e.g. one zorbacmd version must be 
+# compiled and linked for each registered store). This macro iterates through the CMake variable 
+# ZORBA_STORE_NAMES and creates one executable for each store name.
+# Per convention, the source file that contains the main function of the executable must be located in 
+# the directory where this macro is executabed and must the following name: 'EXE_NAME.cpp.in'. 
+# To enable store dependent code in the source file, the macro sets the cmake variable ZORBA_STORE_NAME. 
+# This variable can be used in the source file by adding the replacement @ZORBA_STORE_NAME@. Logically, 
+# all other available cmake variables can be used from this source file because the cmake command
+# CONFIGURE_FILE is used to generate the store specific sources.
+# The resulting libraries are named by the following convention: Executables that are linked agains a store lib
+# other than simplestore contain the suffix '_${ZORBA_STORE_NAME}'.
+# If the cmake variable ZORBA_BUILD_STATIC_LIBRARY is set to "ON", the macro produces static libraries and add
+# the additional suffix '_static' to them.
+#
+# Synatx:
+# 
+#   ZORBA_GENERATE_STORE_SPECIFIC_EXES(EXE_NAME DEPEND_SRCS DEPEND_LIBS NEW_NAME) 
+#   
+#     EXE_NAME - the name of the executable. Per convention, the file that contains the main function 
+#                and the CMake replacement @ZORBA_STORE_NAME@ must have the name EXE_NAME.cpp.in and
+#                must be located in the directory where the invocation of this function is located.
+#     DEPEND_SRCS - additional source files which will be compiled and linked to the created libraries
+#     DEPEND_LIBS - libraries (exception zorba-store libs) to which the created libraries depend on
+#     NEW_NAME - If this variable is not equal "", the executables will be renamed to NEW_NAME
+#
+#
+MACRO(ZORBA_GENERATE_STORE_SPECIFIC_EXES EXE_NAME DEPEND_SRCS DEPEND_LIBS NEW_NAME)
+  FOREACH(ZORBA_STORE_NAME ${ZORBA_STORE_NAMES})
+    SET(SUFFIX)
+    # simplestore executable doesn't need an extension
+    IF (NOT ${ZORBA_STORE_NAME} STREQUAL "simplestore")
+      SET(SUFFIX "_${ZORBA_STORE_NAME}")
+    ENDIF (NOT ${ZORBA_STORE_NAME} STREQUAL "simplestore")
+
+    CONFIGURE_FILE(
+      ${CMAKE_CURRENT_SOURCE_DIR}/${EXE_NAME}.cpp.in 
+      ${CMAKE_CURRENT_BINARY_DIR}/${EXE_NAME}${SUFFIX}.cpp
+      @ONLY
+    )
+    SET( SRCS
+      ${CMAKE_CURRENT_BINARY_DIR}/${EXE_NAME}${SUFFIX}.cpp
+      ${DEPEND_SRCS}
+    )
+    ADD_EXECUTABLE("${EXE_NAME}${SUFFIX}" ${SRCS})
+    TARGET_LINK_LIBRARIES("${EXE_NAME}${SUFFIX}" 
+                          "zorba_${ZORBA_STORE_NAME}"
+                          ${DEPEND_LIBS})
+    IF (NOT ${NEW_NAME} STREQUAL "")
+      SET_TARGET_PROPERTIES("${EXE_NAME}${SUFFIX}" PROPERTIES OUTPUT_NAME "${NEW_NAME}${SUFFIX}")
+    ENDIF (NOT ${NEW_NAME} STREQUAL "")
+
+    IF (ZORBA_BUILD_STATIC_LIBRARY)
+      ADD_EXECUTABLE("${EXE_NAME}${SUFFIX}_static" ${SRCS})
+      SET_TARGET_PROPERTIES("${EXE_NAME}${SUFFIX}_static" PROPERTIES COMPILE_DEFINITIONS BUILDING_ZORBA_STATIC)
+      TARGET_LINK_LIBRARIES("${EXE_NAME}${SUFFIX}_static" 
+                            "zorba_${ZORBA_STORE_NAME}_static"
+                            ${DEPEND_LIBS})
+      IF (NOT ${NEW_NAME} STREQUAL "")
+        SET_TARGET_PROPERTIES("${EXE_NAME}${SUFFIX}_static" PROPERTIES OUTPUT_NAME "${NEW_NAME}${SUFFIX}_static")
+      ENDIF (NOT ${NEW_NAME} STREQUAL "")
+    ENDIF (ZORBA_BUILD_STATIC_LIBRARY)
+
+  ENDFOREACH(ZORBA_STORE_NAME ${ZORBA_STORE_NAMES})
+ENDMACRO(ZORBA_GENERATE_STORE_SPECIFIC_EXES)
+
+
