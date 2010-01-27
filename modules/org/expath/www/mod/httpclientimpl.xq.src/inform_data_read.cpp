93549bc7c7aea40150702f09d845cac92c0b3b24
mpilman <>
2010-01-27 15:13:37 +0000

    Deleted old version of http-client

--- modules/org/expath/www/mod/httpclientimpl.xq.src/inform_data_read.cpp
+++
@@ -1,7 +0,0 @@
-#include "inform_data_read.h"
-
-namespace zorba { namespace http_client {
-  InformDataRead::~InformDataRead()
-  {
-  }
-}} //namespace zorba, http_client
--- modules/org/expath/www/mod/httpclientimpl.xq.src/request_handler.cpp
+++
@@ -1,7 +0,0 @@
-#include "request_handler.h"
-
-namespace zorba { namespace http_client {
-  RequestHandler::~RequestHandler()
-  {
-  }
-}} //namespace zorba, http_client
