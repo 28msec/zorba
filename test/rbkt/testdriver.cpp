b05d8cf253de394732312af08193415d7060f6ba
spungi <>
2010-06-10 13:11:07 +0000

    If the comparison method is set to ignore (i.e. Comparison = Ignore in the .spec file) the test shoudl be considered successful. Partial fix for bug #2999765, fixed failing tests: w3c_testsuite/XQuery/Construct/ComputeCon/ComputeConAttr/K2-ComputeConAttr-51 w3c_testsuite/XQuery/Construct/ComputeCon/ComputeConAttr/K2-ComputeConAttr-53 w3c_testsuite/XQuery/Construct/ComputeCon/ComputeConAttr/K2-ComputeConAttr-58 w3c_testsuite/XQuery/FLWORExpr/OrderbyExpr/OrderbyExprWithout/K2-OrderbyExprWithout-42

--- test/rbkt/testdriver.cpp
+++ test/rbkt/testdriver.cpp
@@ -421,7 +421,10 @@ main(int argc, char** argv)
       if (lSpec.errorsSize() == 0 && ! lRefFileExists )
       {
         std::cout << "No reference result and no expected errors." << std::endl;
-        return 3;
+        if(lSpec.getComparisonMethod() == "Ignore")
+          std::cout << "Since the comparison method is set to 'Ignore' the test is considered successful." << std::endl;
+        else
+          return 3;
       }
       
       if (errHandler.errors())
