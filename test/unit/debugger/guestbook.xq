f3e04454f7e11cfa9a5eddc2c1f4ddc90de4b9f2
mbrantner <>
2010-08-26 09:38:22 +0000

    dding tests in order to make sure that plan serialization of queries in debug mode (i.e. of the debugger_expr) works

---
+++ test/unit/debugger/guestbook.xq
@@ -0,0 +1,36 @@
+module namespace guestbook = "http://www.28msec.com/template/guestbook/guestbook";
+
+import module namespace xqddf = 'http://www.zorba-xquery.com/modules/xqddf';
+declare ordered collection guestbook:entries as node()*;
+declare variable $guestbook:entries as xs:QName := xs:QName('guestbook:entries');
+
+declare sequential function guestbook:add()
+{
+  xqddf:insert-nodes-last(
+      $guestbook:entries,
+      <entry author='blub' date='{fn:current-date()}' time='{fn:current-time()}'>
+       test
+      </entry>
+  );
+  exit returning guestbook:list();
+};
+
+declare sequential function guestbook:list() {
+  
+  let $entries := xqddf:collection($guestbook:entries)
+  let $num_entries := fn:count($entries)
+  return 
+    if($num_entries = 0)
+    then    
+      <div style='color:green;' class='entry'><b>No entries, yet.</b></div>
+    else
+      for $entry at $pos in $entries[position() gt $num_entries - 5]
+      let $numbering := if ($num_entries lt 5) then $pos else $num_entries - ( 5 - $pos) 
+      return 
+        <div class='entry'>
+          <b style='color:green;'>{fn:data($entry/@date)} -- {fn:data($entry/@time)}</b>{' '}<b>
+             {fn:data($entry/@author)}</b><br/>
+          <span style='float:left;color: #aaaaaa;'>{ $numbering  }</span>
+          {$entry/text()}
+        </div>;
+};
---
+++ test/unit/debugger/test_debugger_serialization.cpp
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2006-2008 The FLWOR Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <iostream>
+#include <sstream>
+
+#include <zorba/zorba.h>
+#include <zorba/external_module.h>
+#include <zorba/external_function.h>
+#include <zorba/serialization_callback.h>
+#include <zorba/store_manager.h>
+
+using namespace zorba;
+
+bool
+debugger_serialization_example_1(Zorba* aZorba)
+{
+  try {
+    // the stringstream used for query materialization
+    std::stringstream lExecutionPlan;
+
+    {
+      XQuery_t lQuery = aZorba->createQuery();
+      lQuery->setDebugMode(true);
+      lQuery->compile("declare function local:foo() { let $i := 1 return ()}; ()"); 
+      lQuery->saveExecutionPlan(lExecutionPlan, ZORBA_USE_BINARY_ARCHIVE);
+      std::cout << lQuery << std::endl;
+    }
+
+    {
+      XQuery_t lQuery = aZorba->createQuery();
+      lQuery->loadExecutionPlan(lExecutionPlan);
+      std::cout << lQuery << std::endl;
+    }
+
+  } catch (ZorbaException& e) {
+    std::cerr << e << std::endl;
+    return false;
+  }
+
+	return true;
+}
+
+bool
+debugger_serialization_example_2(Zorba* aZorba)
+{
+  try {
+    // the stringstream used for query materialization
+    std::stringstream lExecutionPlan;
+    std::ostringstream lQueryStream;
+
+#ifdef UNIX
+    char* lCWD = get_current_dir_name();
+#else
+    char* lCWD = _getcwd(0, 256);
+#endif
+
+    lQueryStream
+      << "import module namespace g = 'http://www.28msec.com/template/guestbook/guestbook'"
+      << " at 'file:///" << lCWD << "/guestbook.xq"
+      << "';" << std::endl
+      << "g:add()";
+
+#ifndef UNIX
+    free(lCWD);
+#endif
+
+    {
+      XQuery_t lQuery = aZorba->createQuery();
+      lQuery->setDebugMode(true);
+      std::cout << lQueryStream.str() << std::endl;
+      lQuery->compile(lQueryStream.str()); 
+      lQuery->saveExecutionPlan(lExecutionPlan, ZORBA_USE_BINARY_ARCHIVE);
+      std::cout << lQuery << std::endl;
+    }
+
+    {
+      XQuery_t lQuery = aZorba->createQuery();
+      lQuery->loadExecutionPlan(lExecutionPlan);
+      std::cout << lQuery << std::endl;
+    }
+
+  } catch (QueryException& qe) {
+    // error "Collection guestbook:entries does not exist." is OK
+    if (qe.getErrorCode() != XDDY0003_COLLECTION_DOES_NOT_EXIST) {
+      std::cerr << qe << std::endl;
+      return false;
+    }
+  } catch (ZorbaException& e) {
+    std::cerr << e << std::endl;
+    return false;
+  }
+
+	return true;
+}
+
+int test_debugger_serialization( int argc, char * argv[] )
+{
+  void* lStore = zorba::StoreManager::getStore();
+  Zorba* lZorba = Zorba::getInstance(lStore);
+  bool res = false;
+
+  std::cout << "executing debugger serialization example 1" << std::endl;
+  res = debugger_serialization_example_1(lZorba);
+  if (!res) return 1; 
+  std::cout << std::endl;
+
+  std::cout << "executing debugger serialization example 2" << std::endl;
+  res = debugger_serialization_example_2(lZorba);
+  if (!res) return 1; 
+  std::cout << std::endl;
+
+  lZorba->shutdown();
+  zorba::StoreManager::shutdownStore(lStore);
+  return 0;
+}
+
