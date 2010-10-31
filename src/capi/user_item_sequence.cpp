fbd505b7ed32bbe29329113c8e538bee72b5d931
pauljlucas <>
2010-10-31 03:40:52 +0000

    Added missing #include <map>.

--- src/capi/user_item_sequence.cpp
+++ src/capi/user_item_sequence.cpp
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#include <map>
+
 #include "capi/error.h"
 #include "capi/csequence.h"
 #include "capi/user_item_sequence.h"
