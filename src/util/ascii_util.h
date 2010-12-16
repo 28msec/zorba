f5fd1a64a8d16b3e0043f718d8332e7571e3e589
pauljlucas <>
2010-12-16 07:56:42 +0000

    Added is_digit().

--- src/util/ascii_util.h
+++ src/util/ascii_util.h
@@ -306,6 +306,23 @@ inline char* itoa( int n, char *buf ) {
 }
 
 /**
+ * Checks whether the given character is a decimal digit.  This function exists
+ * to make a proper function out of the standard isdigit(3) that may be
+ * implemented as a macro.
+ *
+ * @param c The character to check.
+ * @return Returns \c true only if the character is a decimal digit.
+ */
+inline bool is_digit( char c ) {
+#ifdef WIN32
+  // Windows' isdigit() implementation crashes for non-ASCII characters.
+  return __isascii( c ) && isdigit( c );
+#else
+  return isdigit( c );
+#endif
+}
+
+/**
  * Checks whether the given character is a whitespace character.  This function
  * exists to make a proper function out of the standard isspace(3) that may be
  * implemented as a macro.
