79a3831b3c7ed5631d729af1ea3a5d8ebecc0e25
danielturcanu <>
2010-12-28 17:48:53 +0000

    Fixing Windows build on dashboard.

--- src/unit_tests/unit_test_list.h
+++ src/unit_tests/unit_test_list.h
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <iostream>
 
 #ifndef ZORBA_UNIT_TEST_LIST_H
 #define ZORBA_UNIT_TEST_LIST_H
