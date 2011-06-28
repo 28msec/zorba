2a4c550915b681e61ccc25cdfcb0712db4f8e0c0
hybridum <>
2011-06-28 18:51:57 +0000

    Fix for system warning and another fix for precompiled headers

--- src/util/http_util.cpp
+++ src/util/http_util.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "stdafx.h"
 #include <vector>
 #include <zorba/singleton_item_sequence.h>
 #include <zorba/empty_sequence.h>
