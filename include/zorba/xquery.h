4f4af9241f34a7250a9d9a40bf64eaaa552ff158
mbrantner <>
2011-07-25 17:40:50 +0000

    don't install include/zorba/pregenerated + fixing warnings in doxygen comments

--- include/zorba/xquery.h
+++ include/zorba/xquery.h
@@ -329,13 +329,14 @@ class ZORBA_DLL_PUBLIC XQuery : public SmartObject
    *        are ZORBA_USE_BINARY_ARCHIVE and ZORBA_USE_XML_ARCHIVE. The binary
    *        format is much smaller than XML format, but is not human readable.
    * @param save_options Specify some options to the plan serializer.
-   *    <dl>Current possible values are: 
-   *    <dt>DONT_SAVE_UNUSED_FUNCTIONS (default)</dt> 
-   *      <dd>to eliminate unused functions and reduce plan size</dd>
-   *    <dt>SAVE_UNUSED_FUNCTIONS</dt>
-   *      <dd>to save everything, as if the xquery contains an eval instruction.
-   *       This is usefull if you intend to use StaticContext::containsFunction 
-   *       or StaticContext::findFunctions.</dd></dl>
+   *    <ul>Current possible values are: 
+   *      <li>DONT_SAVE_UNUSED_FUNCTIONS (default): 
+   *        to eliminate unused functions and reduce plan size</li>
+   *      <li>SAVE_UNUSED_FUNCTIONS:
+   *        to save everything, as if the xquery contains an eval instruction.
+   *        This is useful if you intend to use StaticContext::containsFunction 
+   *        or StaticContext::findFunctions.</li>
+   *   </ul>
    * @return true if success.
    * @throw ZorbaException if the query has not been compiled or there are
    *        problems serializing the execution plan.
