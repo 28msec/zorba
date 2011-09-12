516fa009bea227f952e19c32783abcc95c12cde7
ceejatec <>
2011-09-12 23:22:24 +0000

    3408089: testdriver fix (didn't correctly diff when expected results file was less than 3 bytes long, including empty files). This revealed a few test bugs and one Zorba bug.

--- test/commons/testdriver_comparator.cpp
+++ test/commons/testdriver_comparator.cpp
@@ -279,7 +279,7 @@ bool fileEquals(
     bom[2] != static_cast<char>(0xBF))
   {
     // Wasn't a BOM; 'unread' it
-    //refStream.seekg(-3, std::ios_base::cur);
+    refStream.clear();
     refStream.seekg(0, std::ios_base::beg);
   }
 
--- test/rbkt/ExpQueryResults/zorba/versioning/schema1.xml.res
+++ test/rbkt/ExpQueryResults/zorba/versioning/schema1.xml.res
@@ -1,1 +1,1 @@
-1
+<sch:a xmlns:sch="http://www.zorba-xquery.com/modules/theschema">1</sch:a>
