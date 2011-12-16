91ef5aea2c25a73742f627f100a3f61c570e2270
Matthias Brantner <matthias.brantner@28msec.com>
2011-12-16 08:48:10 -0800

    fix for building with json

--- src/runtime/json/jsoniq_functions_impl.cpp
+++ src/runtime/json/jsoniq_functions_impl.cpp
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <zorba/config.h>
+
 #ifdef ZORBA_WITH_JSON
 
 #include "stdafx.h"
