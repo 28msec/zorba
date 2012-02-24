3c0b34e3eefec41f9ad6585abb261ad118c641d1
Ghislain Fourny <ghislain.fourny@28msec.com>
2012-02-24 18:10:42 +0100

    Forgot to add header file.

---
+++ src/store/naive/tree_id_generator.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2006-2008 The FLWOR Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef ZORBA_TREE_ID_GENERATOR_H
+#define ZORBA_TREE_ID_GENERATOR_H
+
+#include "store/naive/shared_types.h"
+#include "zorbautils/hashmap_itemh.h"
+
+namespace zorba {
+
+namespace simplestore {
+
+/*
+This class is an abstract class for tree ID generation. It provides
+a creation method, two comparison methods (= and <) as well as a way
+to parse a string back to an ID.
+*/
+class TreeIdGenerator {
+public:
+  virtual ~TreeIdGenerator() {}
+
+  virtual TreeId create() = 0;
+};
+
+/*
+This class allows generation of independent tree ID generators (each of
+them might have its own counter).
+*/
+class TreeIdGeneratorFactory {
+public:
+  virtual ~TreeIdGeneratorFactory() {}
+  
+  virtual TreeIdGenerator* createTreeGenerator() = 0;
+  virtual TreeIdGenerator& getDefaultTreeIdGenerator() = 0;
+};
+
+/*
+Zorba's implementation of the tree ID generator.
+*/
+class ZorbaTreeIdGenerator : public TreeIdGenerator {
+private:
+  ulong theNextId;
+public:
+  ZorbaTreeIdGenerator() : theNextId(1) {}
+  virtual TreeId create();
+};
+
+/*
+Zorba's implementation of the tree ID generator factory.
+*/
+class ZorbaTreeIdGeneratorFactory : public TreeIdGeneratorFactory {
+public:
+  virtual TreeIdGenerator* createTreeGenerator();
+  virtual TreeIdGenerator& getDefaultTreeIdGenerator();
+};
+
+} // simplestore
+} // zorba
+
+#endif /* ZORBA_TREE_ID_GENERATOR_H */
