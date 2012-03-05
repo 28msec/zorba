4281a3bef04c25a633133828130f22f402c7c921
Markos Zaharioudakis <markos_za@yahoo.com>
2012-03-05 18:04:22 +0100

    Fixed bug #867024

--- src/compiler/translator/translator.cpp
+++ src/compiler/translator/translator.cpp
@@ -3157,8 +3157,7 @@ void* begin_visit(const VFO_DeclList& v)
     {
       if (theSctx->xquery_version() < StaticContextConsts::xquery_version_3_0)
       {
-        RAISE_ERROR(err::XPST0003, loc,
-        ERROR_PARAMS("functions and variables annotations only available in XQuery 1.1 or later"));
+        RAISE_ERROR(err::XPST0003, loc, ERROR_PARAMS(ZED(XPST0003_Annotations)));
       }
 
       lAnns->accept(*this);
@@ -3916,7 +3915,7 @@ void* begin_visit(const CtxItemDecl& v)
 
   if (theSctx->xquery_version() <= StaticContextConsts::xquery_version_1_0)
     RAISE_ERROR(err::XPST0003, loc,
-    ERROR_PARAMS(ZED(XQueryVersionAtLeast10_2), theSctx->xquery_version()));
+    ERROR_PARAMS(ZED(XPST0003_XQueryVersionAtLeast30_2), theSctx->xquery_version()));
 
   theHaveContextItemDecl = true;
 
@@ -5333,12 +5332,10 @@ void* begin_visit(const BlockBody& v)
     return NULL;
   }
 
-  if ( !theSctx->is_feature_set(feature::scripting) )
+  if (!theSctx->is_feature_set(feature::scripting))
   {
-    throw XQUERY_EXCEPTION(
-      err::XPST0003,
-      ERROR_LOC( v.get_location() )
-    );
+    RAISE_ERROR(zerr::ZXQP0050_FEATURE_NOT_AVAILABLE, v.get_location(),
+    ERROR_PARAMS(ZED(ZXQP0050_Scripting)));
   }
 
   bool topLevel = v.isTopLevel();
@@ -5354,14 +5351,14 @@ void* begin_visit(const BlockBody& v)
 
   push_scope();
 
-  ulong numScopes = theAssignedVars.size();
+  csize numScopes = theAssignedVars.size();
 
   theAssignedVars.resize(numScopes + 1);
 
-  ulong numExprs = v.size();
+  csize numExprs = v.size();
   bool declaresVars = false;
 
-  for (ulong i = 0; i < numExprs; ++i)
+  for (csize i = 0; i < numExprs; ++i)
   {
     v[i]->accept(*this);
 
@@ -5469,12 +5466,10 @@ void* begin_visit(const AssignExpr& v)
 {
   TRACE_VISIT();
 
-  if ( !theSctx->is_feature_set(feature::scripting) )
+  if (!theSctx->is_feature_set(feature::scripting))
   {
-    throw XQUERY_EXCEPTION(
-      err::XPST0003,
-      ERROR_LOC( v.get_location() )
-    );
+    RAISE_ERROR(zerr::ZXQP0050_FEATURE_NOT_AVAILABLE, v.get_location(),
+    ERROR_PARAMS(ZED(ZXQP0050_Scripting)));
   }
   return no_state;
 }
@@ -5772,7 +5767,7 @@ void end_visit(const FLWORExpr& v, void* /*visit_state*/)
       v.is_non_10())
   {
     RAISE_ERROR(err::XPST0003, loc,
-    ERROR_PARAMS(ZED(XQueryVersionAtLeast10_2), theSctx->xquery_version()));
+    ERROR_PARAMS(ZED(XPST0003_XQueryVersionAtLeast30_2), theSctx->xquery_version()));
   }
 
   rchandle<flwor_expr> flwor = new flwor_expr(theRootSctx, loc, v.is_general());
@@ -5877,7 +5872,7 @@ void* begin_visit(const ForClause& v)
   if (v.has_allowing_empty())
   {
     if (theSctx->xquery_version() < StaticContextConsts::xquery_version_3_0)
-      RAISE_ERROR(err::XPST0003, loc, ERROR_PARAMS(ZED(OuterForClause11)));
+      RAISE_ERROR(err::XPST0003, loc, ERROR_PARAMS(ZED(XPST0003_OuterForClause11)));
 
     theFlworClausesStack.push_back(NULL);
   }
@@ -6087,9 +6082,7 @@ void* begin_visit(const WindowClause& v)
   TRACE_VISIT();
 
   if (theSctx->xquery_version() < StaticContextConsts::xquery_version_3_0)
-    throw XQUERY_EXCEPTION(
-      err::XPST0003, ERROR_PARAMS( ZED( WindowClause11 ) ), ERROR_LOC( loc )
-    );
+    RAISE_ERROR(err::XPST0003, loc, ERROR_PARAMS(ZED(XPST0003_WindowClause11)));
 
   return no_state;
 }
@@ -6708,9 +6701,7 @@ void* begin_visit(const CountClause& v)
   TRACE_VISIT ();
 
   if (theSctx->xquery_version() < StaticContextConsts::xquery_version_3_0)
-    throw XQUERY_EXCEPTION(
-      err::XPST0003, ERROR_PARAMS( ZED( CountClause11 ) ), ERROR_LOC( loc )
-    );
+    RAISE_ERROR(err::XPST0003, loc, ERROR_PARAMS(ZED(XPST0003_CountClause11)));
 
   return no_state;
 }
@@ -6774,9 +6765,7 @@ void* begin_visit(const SwitchExpr& v)
 
   if (theSctx->xquery_version() < StaticContextConsts::xquery_version_3_0)
   {
-    throw XQUERY_EXCEPTION(err::XPST0003,
-                           ERROR_PARAMS(ZED(SwitchExpr11)),
-                           ERROR_LOC(loc));
+    RAISE_ERROR(err::XPST0003, loc, ERROR_PARAMS(ZED(XPST0003_SwitchExpr11)));
   }
 
   v.get_switch_expr()->accept(*this);
@@ -7175,9 +7164,7 @@ void* begin_visit(const TryExpr& v)
 
   if (theSctx->xquery_version() < StaticContextConsts::xquery_version_3_0)
   {
-    throw XQUERY_EXCEPTION(err::XPST0003,
-                           ERROR_PARAMS(ZED(TryCatchExpr11)),
-                           ERROR_LOC(loc));
+    RAISE_ERROR(err::XPST0003, loc, ERROR_PARAMS(ZED(XPST0003_TryCatchExpr11)));
   }
 
   theTryStack.push_back(&v);
@@ -10366,11 +10353,8 @@ void* begin_visit(const DynamicFunctionInvocation& v)
   TRACE_VISIT();
   if ( !theSctx->is_feature_set(feature::hof) )
   {
-    throw XQUERY_EXCEPTION(
-      zerr::ZXQP0050_FEATURE_NOT_AVAILABLE,
-      ERROR_PARAMS( "higher-order functions (hof)" ),
-      ERROR_LOC( v.get_location() )
-    );
+    RAISE_ERROR(zerr::ZXQP0050_FEATURE_NOT_AVAILABLE, v.get_location(),
+    ERROR_PARAMS("higher-order functions (hof)" ));
   }
   return no_state;
 }
@@ -11396,7 +11380,7 @@ void end_visit(const DirPIConstructor& v, void* /*visit_state*/)
   utf8::to_upper(target_str, &target_upper);
 
   if (target_upper == "XML")
-    throw XQUERY_EXCEPTION(err::XPST0003, ERROR_LOC(loc));
+    RAISE_ERROR(err::XPST0003, loc, ERROR_PARAMS(ZED(XPST0003_PiTarget)));
 
   expr_t target = new const_expr(theRootSctx, loc, target_str);
   expr_t content = new const_expr(theRootSctx, loc, v.get_pi_content().str());
@@ -12465,7 +12449,8 @@ void end_visit(const VarBinding& v, void*)
 
 
 #ifndef ZORBA_NO_FULL_TEXT
-template<typename FTNodeType> bool flatten( ftnode *n ) {
+template<typename FTNodeType> bool flatten( ftnode *n ) 
+{
   if ( FTNodeType *const n2 = dynamic_cast<FTNodeType*>( n ) ) {
     typename FTNodeType::ftnode_list_t &list = n2->get_node_list();
     typename FTNodeType::ftnode_list_t::iterator i = list.begin();
@@ -12480,7 +12465,8 @@ template<typename FTNodeType> bool flatten( ftnode *n ) {
 }
 #endif /* ZORBA_NO_FULL_TEXT */
 
-void *begin_visit (const FTAnd& v) {
+void *begin_visit (const FTAnd& v) 
+{
   TRACE_VISIT ();
 #ifndef ZORBA_NO_FULL_TEXT
   push_ftstack( NULL ); // sentinel
@@ -12488,7 +12474,8 @@ void *begin_visit (const FTAnd& v) {
   return no_state;
 }
 
-void end_visit (const FTAnd& v, void* /*visit_state*/) {
+void end_visit (const FTAnd& v, void* /*visit_state*/) 
+{
   TRACE_VISIT_OUT ();
 #ifndef ZORBA_NO_FULL_TEXT
   ftand::ftnode_list_t list;
@@ -12508,35 +12495,46 @@ void end_visit (const FTAnd& v, void* /*visit_state*/) {
 #endif /* ZORBA_NO_FULL_TEXT */
 }
 
-void *begin_visit (const FTAnyallOption& v) {
+
+void *begin_visit (const FTAnyallOption& v) 
+{
   TRACE_VISIT ();
   // nothing to do
   return no_state;
 }
 
-void end_visit (const FTAnyallOption& v, void* /*visit_state*/) {
+
+void end_visit (const FTAnyallOption& v, void* /*visit_state*/) 
+{
   TRACE_VISIT_OUT ();
   // nothing to do
 }
 
-void *begin_visit (const FTBigUnit& v) {
+
+void *begin_visit (const FTBigUnit& v) 
+{
   TRACE_VISIT ();
   // nothing to do
   return no_state;
 }
 
-void end_visit (const FTBigUnit& v, void* /*visit_state*/) {
+
+void end_visit (const FTBigUnit& v, void* /*visit_state*/) 
+{
   TRACE_VISIT_OUT ();
   // nothing to do
 }
 
-void *begin_visit (const FTCaseOption& v) {
+void *begin_visit (const FTCaseOption& v) 
+{
   TRACE_VISIT ();
   // nothing to do
   return no_state;
 }
 
-void end_visit (const FTCaseOption& v, void* /*visit_state*/) {
+
+void end_visit (const FTCaseOption& v, void* /*visit_state*/) 
+{
   TRACE_VISIT_OUT ();
 #ifndef ZORBA_NO_FULL_TEXT
   ftmatch_options *const mo = dynamic_cast<ftmatch_options*>( top_ftstack() );
@@ -12549,7 +12547,9 @@ void end_visit (const FTCaseOption& v, void* /*visit_state*/) {
 #endif /* ZORBA_NO_FULL_TEXT */
 }
 
-void *begin_visit (const FTContainsExpr& v) {
+
+void *begin_visit (const FTContainsExpr& v) 
+{
   TRACE_VISIT ();
 #ifdef ZORBA_NO_FULL_TEXT
   throw XQUERY_EXCEPTION(
@@ -12559,7 +12559,8 @@ void *begin_visit (const FTContainsExpr& v) {
   return no_state;
 }
 
-void end_visit (const FTContainsExpr& v, void* /*visit_state*/) {
+void end_visit (const FTContainsExpr& v, void* /*visit_state*/) 
+{
   TRACE_VISIT_OUT ();
 #ifndef ZORBA_NO_FULL_TEXT
   expr_t ftignore = NULL;
@@ -13304,10 +13305,8 @@ expr_t translate(const parsenode& root, CompilerCB* ccb)
   std::map<zstring, zstring> modulesStack;
 
   if (typeid(root) != typeid(MainModule))
-    throw XQUERY_EXCEPTION(
-      err::XPST0003, ERROR_PARAMS( ZED( ModuleDeclNotInMain ) ),
-      ERROR_LOC( root.get_location() )
-    );
+    RAISE_ERROR(err::XPST0003, root.get_location(),
+    ERROR_PARAMS(ZED(ModuleDeclNotInMain)));
 
   ModulesInfo minfo(ccb);
 
