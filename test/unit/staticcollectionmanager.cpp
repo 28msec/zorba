9c6343970db7308a57d4cb3b1e5ef1edadbced4e
Matthias Brantner <matthias.brantner@28msec.com>
2012-03-06 17:25:33 -0800

    fixed bug that occurred during the merge

--- test/unit/staticcollectionmanager.cpp
+++ test/unit/staticcollectionmanager.cpp
@@ -206,23 +206,25 @@ staticcollectionamanger3(zorba::Zorba* z)
   Item lCollName2 = lFac->createQName("http://www.mod2.com/", "coll");
   
   lColMgr->createCollection(lCollName2);
-  Collection_t lColl = lColMgr->getCollection(lCollName2);
-
-  std::vector<Annotation_t> lAnnotations;
-  lColl->getAnnotations(lAnnotations);
-  size_t num_annotations = 0;
-  for (std::vector<Annotation_t>::const_iterator lIter = lAnnotations.begin();
-       lIter != lAnnotations.end(); ++lIter)
   {
-    std::cout << "Annotation QName " << (*lIter)->getQName().getStringValue() << std::endl;
-    ++num_annotations;
-  }
+    Collection_t lColl = lColMgr->getCollection(lCollName2);
 
-  if (num_annotations != 3)
-  {
-    return false;
-  }
+    std::vector<Annotation_t> lAnnotations;
+    lColl->getAnnotations(lAnnotations);
+    size_t num_annotations = 0;
+    for (std::vector<Annotation_t>::const_iterator lIter = lAnnotations.begin();
+         lIter != lAnnotations.end(); ++lIter)
+    {
+      std::cout << "Annotation QName " << (*lIter)->getQName().getStringValue() << std::endl;
+      ++num_annotations;
+    }
 
+    if (num_annotations != 3)
+    {
+      return false;
+    }
+  }
+  lColMgr->deleteCollection(lCollName2);
 
   return true;
 }
