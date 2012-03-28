82bb35c1d99b8f0613c537e87e5113af5b9c8d79
Markos Zaharioudakis <markos_za@yahoo.com>
2012-03-28 06:41:54 +0200

    fixed performance problem with the findNodeSources function of the no-copy rule

--- src/functions/udf.h
+++ src/functions/udf.h
@@ -120,7 +120,9 @@ private:
 
   bool                        theIsExiting;
   bool                        theIsLeaf;
+
   std::vector<user_function*> theMutuallyRecursiveUDFs;
+  std::vector<expr*>          theRecursiveCalls;
 
   bool                        theIsOptimized;
 
@@ -132,7 +134,7 @@ private:
   bool                        theCacheResults;
   bool                        theCacheComputed;
 
-  rchandle<rclist<user_function*> >    theLocalUdfs;//for plan serializer
+  rchandle<rclist<user_function*> >  theLocalUdfs;//for plan serializer
 
 public:
   SERIALIZABLE_CLASS(user_function)
@@ -181,6 +183,10 @@ public:
   void addMutuallyRecursiveUDFs(
       const std::vector<user_function*>& udfs,
       const std::vector<user_function*>::const_iterator& cycle);
+
+  void addRecursiveCall(expr* call);
+
+  const std::vector<expr*>& getRecursiveCalls() const { return theRecursiveCalls; }
 
   bool isMutuallyRecursiveWith(const user_function* udf);
 
