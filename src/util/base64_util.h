3d2355ed11d4c3a2e0704ae7dd46ee20331eda49
Paul J. Lucas <paul@lucasmail.org>
2012-06-17 08:22:53 -0700

    Added template functions.

--- src/util/base64_util.h
+++ src/util/base64_util.h
@@ -78,6 +78,28 @@ size_type decode( char const *from, size_type from_len, char *to );
 void decode( std::istream &from, std::ostream &to );
 
 /**
+ * Decodes a Base64-encoded istream.  Embedded newlines and carriage-returns
+ * are skipped.
+ *
+ * @param from The istream to read from until EOF is reached.
+ * @param to The string to append the decoded bytes to.
+ * @throws invalid_argument if more than two trailing \c = characters are
+ * encountered or necessary, \c = is encountered anywhere in the stream except
+ * at the end, or an invalid byte in encountered.
+ */
+template<typename CharType,class TraitsType,class StringType> inline
+void decode( std::basic_istream<CharType,TraitsType> &from, StringType *to ) {
+  char from_buf[ 1024 * 4 ], to_buf[ 1024 * 3 ];
+  while ( !from.eof() ) {
+    from.read( from_buf, sizeof from_buf );
+    if ( std::streamsize const gcount = from.gcount() )
+      to->append( to_buf, decode( from_buf, gcount, to_buf ) );
+    else
+      break;
+  }
+}
+
+/**
  * Base64-encodes a buffer.
  *
  * @param from A pointer to the bugger to be encoded.
@@ -97,6 +119,24 @@ size_type encode( char const *from, size_type from_len, char *to );
  * @param to The ostream to write the encoded bytes to.
  */
 void encode( std::istream &from, std::ostream &to );
+
+/**
+ * Encodes a stream to Base64.
+ *
+ * @param from The istream to read from until EOF is reached.
+ * @param to The string to append the encoded bytes to.
+ */
+template<typename CharType,class TraitsType,class StringType> inline
+void encode( std::basic_istream<CharType,TraitsType> &from, StringType *to ) {
+  char from_buf[ 1024 * 3 ], to_buf[ 1024 * 4 ];
+  while ( !from.eof() ) {
+    from.read( from_buf, sizeof from_buf );
+    if ( std::streamsize const gcount = from.gcount() )
+      to->append( to_buf, encode( from_buf, gcount, to_buf ) );
+    else
+      break;
+  }
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 
