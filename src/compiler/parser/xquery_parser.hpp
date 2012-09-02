ee743b7a555cb1bcfd7bb80cc17798e1a62accc2
Nicolae Brinza <nbrinza@gmail.com>
2012-09-02 04:22:32 +0300

    Fixed bug #867227 -- improved error messages

--- src/compiler/parser/xquery_parser.hpp
+++ src/compiler/parser/xquery_parser.hpp
@@ -1,10 +1,8 @@
-
-/* A Bison parser, made by GNU Bison 2.4.1.  */
+/* A Bison parser, made by GNU Bison 2.6.2.  */
 
 /* Skeleton interface for Bison LALR(1) parsers in C++
    
-      Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008 Free Software
-   Foundation, Inc.
+      Copyright (C) 2002-2012 Free Software Foundation, Inc.
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -32,15 +30,19 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+/**
+ ** \file /home/colea/xquery_bzr/error-messages/build/src/compiler/parser/xquery_parser.hpp
+ ** Define the zorba::parser class.
+ */
+
 /* C++ LALR(1) parser skeleton written by Akim Demaille.  */
 
-#ifndef PARSER_HEADER_H
-# define PARSER_HEADER_H
+#ifndef ZORBA_HOME_COLEA_XQUERY_BZR_ERROR_MESSAGES_BUILD_SRC_COMPILER_PARSER_XQUERY_PARSER_HPP
+# define ZORBA_HOME_COLEA_XQUERY_BZR_ERROR_MESSAGES_BUILD_SRC_COMPILER_PARSER_XQUERY_PARSER_HPP
 
 /* "%code requires" blocks.  */
-
-/* Line 35 of lalr1.cc  */
-#line 35 "/home/markos/zorba/repo/expr-memman2/src/compiler/parser/xquery_parser.y"
+/* Line 36 of lalr1.cc  */
+#line 35 "/home/colea/xquery_bzr/error-messages/src/compiler/parser/xquery_parser.y"
 
 
 /*
@@ -84,34 +86,13 @@ typedef std::pair<zorba::zstring,zorba::zstring> string_pair_t;
 
 
 
-
-/* Line 35 of lalr1.cc  */
-#line 90 "/home/markos/zorba/repo/expr-memman2/build/src/compiler/parser/xquery_parser.hpp"
+/* Line 36 of lalr1.cc  */
+#line 91 "/home/colea/xquery_bzr/error-messages/build/src/compiler/parser/xquery_parser.hpp"
 
 
 #include <string>
 #include <iostream>
 #include "stack.hh"
-
-
-/* Line 35 of lalr1.cc  */
-#line 1 "[Bison:b4_percent_define_default]"
-
-namespace zorba {
-
-/* Line 35 of lalr1.cc  */
-#line 104 "/home/markos/zorba/repo/expr-memman2/build/src/compiler/parser/xquery_parser.hpp"
-  class position;
-  class location;
-
-/* Line 35 of lalr1.cc  */
-#line 1 "[Bison:b4_percent_define_default]"
-
-} // zorba
-
-/* Line 35 of lalr1.cc  */
-#line 114 "/home/markos/zorba/repo/expr-memman2/build/src/compiler/parser/xquery_parser.hpp"
-
 #include "location.hh"
 
 /* Enabling traces.  */
@@ -119,46 +100,10 @@ namespace zorba {
 # define YYDEBUG 1
 #endif
 
-/* Enabling verbose error messages.  */
-#ifdef YYERROR_VERBOSE
-# undef YYERROR_VERBOSE
-# define YYERROR_VERBOSE 1
-#else
-# define YYERROR_VERBOSE 1
-#endif
-
-/* Enabling the token table.  */
-#ifndef YYTOKEN_TABLE
-# define YYTOKEN_TABLE 0
-#endif
-
-/* YYLLOC_DEFAULT -- Set CURRENT to span from RHS[1] to RHS[N].
-   If N is 0, then set CURRENT to the empty location which ends
-   the previous symbol: RHS[0] (always defined).  */
-
-#ifndef YYLLOC_DEFAULT
-# define YYLLOC_DEFAULT(Current, Rhs, N)		\
-do {							\
-  if (N)						\
-    {							\
-      (Current).begin = (Rhs)[1].begin;			\
-      (Current).end   = (Rhs)[N].end;			\
-    }							\
-  else							\
-    {							\
-      (Current).begin = (Current).end = (Rhs)[0].end;	\
-    }							\
-} while (false)
-#endif
-
-
-/* Line 35 of lalr1.cc  */
-#line 1 "[Bison:b4_percent_define_default]"
 
 namespace zorba {
-
-/* Line 35 of lalr1.cc  */
-#line 162 "/home/markos/zorba/repo/expr-memman2/build/src/compiler/parser/xquery_parser.hpp"
+/* Line 36 of lalr1.cc  */
+#line 107 "/home/colea/xquery_bzr/error-messages/build/src/compiler/parser/xquery_parser.hpp"
 
   /// A Bison parser.
   class xquery_parser
@@ -168,9 +113,8 @@ namespace zorba {
 #ifndef YYSTYPE
     union semantic_type
     {
-
-/* Line 35 of lalr1.cc  */
-#line 155 "/home/markos/zorba/repo/expr-memman2/src/compiler/parser/xquery_parser.y"
+/* Line 36 of lalr1.cc  */
+#line 155 "/home/colea/xquery_bzr/error-messages/src/compiler/parser/xquery_parser.y"
 
     zorba::parsenode *node;
     zorba::exprnode *expr;
@@ -192,9 +136,8 @@ namespace zorba {
     ft_anyall_mode::type ft_anyall_value;
 
 
-
-/* Line 35 of lalr1.cc  */
-#line 198 "/home/markos/zorba/repo/expr-memman2/build/src/compiler/parser/xquery_parser.hpp"
+/* Line 36 of lalr1.cc  */
+#line 141 "/home/colea/xquery_bzr/error-messages/build/src/compiler/parser/xquery_parser.hpp"
     };
 #else
     typedef YYSTYPE semantic_type;
@@ -606,6 +549,14 @@ namespace zorba {
     /// The location stack.
     location_stack_type yylocation_stack_;
 
+    /// Whether the given \c yypact_ value indicates a defaulted state.
+    /// \param yyvalue   the value to check
+    static bool yy_pact_value_is_default_ (int yyvalue);
+
+    /// Whether the given \c yytable_ value indicates a syntax error.
+    /// \param yyvalue   the value to check
+    static bool yy_table_value_is_error_ (int yyvalue);
+
     /// Internal symbol numbers.
     typedef unsigned short int token_number_type;
     /* Tables.  */
@@ -613,7 +564,7 @@ namespace zorba {
     static const short int yypact_[];
     static const short int yypact_ninf_;
 
-    /// For a state, default rule to reduce.
+    /// For a state, default reduction number.
     /// Unless\a  yytable_ specifies something else to do.
     /// Zero means the default is an error.
     static const unsigned short int yydefact_[];
@@ -637,18 +588,14 @@ namespace zorba {
     /// For a rule, its LHS.
     static const unsigned short int yyr1_[];
     /// For a rule, its RHS length.
-    static const unsigned char yyr2_[];
+    static const unsigned char yyr2_[]; 
 
-#if YYDEBUG || YYERROR_VERBOSE || YYTOKEN_TABLE
+    /// Convert the symbol name \a n to a form suitable for a diagnostic.
+    static std::string yytnamerr_ (const char *n);
+
+
     /// For a symbol, its name in clear.
     static const char* const yytname_[];
-#endif
-
-#if YYERROR_VERBOSE
-    /// Convert the symbol name \a n to a form suitable for a diagnostic.
-    virtual std::string yytnamerr_ (const char *n);
-#endif
-
 #if YYDEBUG
     /// A type to store symbol numbers and -1.
     typedef short int rhs_number_type;
@@ -703,14 +650,10 @@ namespace zorba {
     xquery_driver& driver;
   };
 
-/* Line 35 of lalr1.cc  */
-#line 1 "[Bison:b4_percent_define_default]"
-
 } // zorba
-
-/* Line 35 of lalr1.cc  */
-#line 713 "/home/markos/zorba/repo/expr-memman2/build/src/compiler/parser/xquery_parser.hpp"
-
+/* Line 36 of lalr1.cc  */
+#line 656 "/home/colea/xquery_bzr/error-messages/build/src/compiler/parser/xquery_parser.hpp"
 
 
-#endif /* ! defined PARSER_HEADER_H */
+
+#endif /* !ZORBA_HOME_COLEA_XQUERY_BZR_ERROR_MESSAGES_BUILD_SRC_COMPILER_PARSER_XQUERY_PARSER_HPP  */
