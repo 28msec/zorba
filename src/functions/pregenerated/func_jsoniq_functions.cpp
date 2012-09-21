a45ecd100967560bb9609c35ce5f3bb0587c6102
Till Westmann <till.westmann@28msec.com>
2012-09-21 12:01:00 -0700

    fix type annotation for encode/decode functions in spec file add test

--- src/functions/pregenerated/func_jsoniq_functions.cpp
+++ src/functions/pregenerated/func_jsoniq_functions.cpp
@@ -262,7 +262,7 @@ void populate_context_jsoniq_functions(static_context* sctx)
       {
     DECL_WITH_KIND(sctx, fn_jsoniq_decode_from_roundtrip,
         (createQName("http://www.jsoniq.org/functions","","decode-from-roundtrip"), 
-        GENV_TYPESYSTEM.JSON_ITEM_TYPE_STAR, 
+        GENV_TYPESYSTEM.ITEM_TYPE_STAR, 
         GENV_TYPESYSTEM.ITEM_TYPE_STAR),
         FunctionConsts::FN_JSONIQ_DECODE_FROM_ROUNDTRIP_1);
 
@@ -278,7 +278,7 @@ void populate_context_jsoniq_functions(static_context* sctx)
       {
     DECL_WITH_KIND(sctx, fn_jsoniq_decode_from_roundtrip,
         (createQName("http://www.jsoniq.org/functions","","decode-from-roundtrip"), 
-        GENV_TYPESYSTEM.JSON_ITEM_TYPE_STAR, 
+        GENV_TYPESYSTEM.ITEM_TYPE_STAR, 
         GENV_TYPESYSTEM.JSON_OBJECT_TYPE_ONE, 
         GENV_TYPESYSTEM.ITEM_TYPE_STAR),
         FunctionConsts::FN_JSONIQ_DECODE_FROM_ROUNDTRIP_2);
@@ -296,7 +296,7 @@ void populate_context_jsoniq_functions(static_context* sctx)
     DECL_WITH_KIND(sctx, fn_jsoniq_encode_for_roundtrip,
         (createQName("http://www.jsoniq.org/functions","","encode-for-roundtrip"), 
         GENV_TYPESYSTEM.ITEM_TYPE_STAR, 
-        GENV_TYPESYSTEM.JSON_ITEM_TYPE_STAR),
+        GENV_TYPESYSTEM.ITEM_TYPE_STAR),
         FunctionConsts::FN_JSONIQ_ENCODE_FOR_ROUNDTRIP_1);
 
   }
@@ -313,7 +313,7 @@ void populate_context_jsoniq_functions(static_context* sctx)
         (createQName("http://www.jsoniq.org/functions","","encode-for-roundtrip"), 
         GENV_TYPESYSTEM.ITEM_TYPE_STAR, 
         GENV_TYPESYSTEM.JSON_OBJECT_TYPE_ONE, 
-        GENV_TYPESYSTEM.JSON_ITEM_TYPE_STAR),
+        GENV_TYPESYSTEM.ITEM_TYPE_STAR),
         FunctionConsts::FN_JSONIQ_ENCODE_FOR_ROUNDTRIP_2);
 
   }
