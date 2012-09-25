64103c8c3a4c5b18ceb4e393da16219b7bc82fc9
Paul J. Lucas <paul@lucasmail.org>
2012-09-24 20:00:26 -0700

    Added big, fat comment.

--- src/util/regex.cpp
+++ src/util/regex.cpp
@@ -242,6 +242,17 @@ void convert_xquery_re( zstring const &xq_re, zstring *icu_re,
           else if ( !m_flag ) {
             zstring::const_iterator const temp = xq_c + 1;
             if ( temp == xq_re.end() ) {
+              //
+              // XQuery 3.0 F&O 5.6.1: By default, ... $ matches the end of the
+              // entire string.  [Newlines are treated as any other character.]
+              //
+              // However, in ICU, $ always matches before any trailing
+              // newlines.
+              //
+              // To make ICU work as XQuery needs it to, substitute \z for $
+              // when it is the last character in the regular expression (and
+              // multi-line mode is not set).
+              //
               icu_re->append( "\\z" );
               continue;
             }
