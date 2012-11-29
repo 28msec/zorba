437971a86ddcd6cf6e6e2282c52edff28eacf153
Paul J. Lucas <paul@lucasmail.org>
2012-11-28 19:49:56 -0800

    1. Added clear(). 2. Added allow_multiple parameter.

--- src/util/json_parser.cpp
+++ src/util/json_parser.cpp
@@ -556,7 +556,20 @@ void parser::require_token_debug( int line, token::type tt, token *t ) {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-parser::parser( istream &in ) : lexer_( in ) {
+parser::parser( std::istream &in, bool allow_multiple ) :
+  allow_multiple_( allow_multiple ),
+  lexer_( in )
+{
+  init();
+}
+
+void parser::clear() {
+  peeked_token_.clear();
+  ztd::clear_stack( state_stack_ );
+  init();
+}
+
+void parser::init() {
 #if DEBUG_JSON_PARSER
   get_indent( cout ) = 0;
 #endif /* DEBUG_JSON_PARSER */
@@ -601,7 +614,7 @@ bool parser::next( token *t ) {
     switch ( state_ ) {
 
       // <JSON> ::= <Array> | <Object>
-      case J0:  PUSH_STATE( J1 );
+      case J0:  PUSH_STATE( allow_multiple_ ? J0 : J1 );
                 switch ( PEEK_TOKEN() ) {
                   case token::begin_array : GOTO_STATE( A0 );
                   case token::begin_object: GOTO_STATE( O0 );
