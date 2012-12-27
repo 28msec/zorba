3b9661603e95de6229230e74e15090889ffa6bca
Paul J. Lucas <paul@lucasmail.org>
2012-12-26 19:10:40 -0800

    Added helper functions.

--- src/util/utf8_streambuf.h
+++ src/util/utf8_streambuf.h
@@ -152,12 +152,11 @@ void attach( std::basic_ios<charT,Traits> &ios ) {
 }
 
 /**
- * Detaches a previously attached base64::streambuf from a stream.  The
- * streambuf is destroyed and the stream's original streambuf is restored.
+ * Detaches a previously attached utf8::streambuf from a stream.  The streambuf
+ * is destroyed and the stream's original streambuf is restored.
  *
- * @param ios The stream to detach the base64::streambuf from.  If the
- * stream doesn't have a base64::streambuf attached to it, this function
- * does nothing.
+ * @param ios The stream to detach the utf8::streambuf from.  If the stream
+ * doesn't have a utf8::streambuf attached to it, this function does nothing.
  */
 template<typename charT,typename Traits> inline
 void detach( std::basic_ios<charT,Traits> &ios ) {
@@ -170,15 +169,140 @@ void detach( std::basic_ios<charT,Traits> &ios ) {
 }
 
 /**
- * Checks whether the given stream has a base64::streambuf attached.
+ * Checks whether the given stream has a utf8::streambuf attached.
  *
  * @param ios The stream to check.
- * @return \c true only if a base64::streambuf is attached.
+ * @return \c true only if a utf8::streambuf is attached.
  */
 template<typename charT,typename Traits> inline
 bool is_attached( std::basic_ios<charT,Traits> &ios ) {
   return !!ios.pword( get_streambuf_index() );
 }
+
+/**
+ * A %utf8::auto_attach is a class that attaches a utf8::streambuf to a stream
+ * and automatically detaches it when the %auto_attach object is destroyed.
+ * \code
+ *  void f( ostream &os ) {
+ *    utf8::auto_attach<ostream> const raii( os, "ISO-8859-1" );
+ *    // ...
+ *  }
+ * \endcode
+ * A %utf8::auto_attach is useful for streams not created by you.
+ *
+ * @see http://en.wikipedia.org/wiki/Resource_Acquisition_Is_Initialization
+ */
+template<class StreamType>
+class auto_attach {
+public:
+  /**
+   * Constructs an %auto_attach object calling attach() on the given stream.
+   *
+   * @param stream The stream to attach the utf8::streambuf to.  If the stream
+   * already has a utf8::streambuf attached to it, this contructor does
+   * nothing.
+   */
+  auto_attach( StreamType &stream ) : stream_( stream ) {
+    attach( stream );
+  }
+
+  /**
+   * Destroys this %auto_attach object calling detach() on the previously
+   * attached stream.
+   */
+  ~auto_attach() {
+    detach( stream_ );
+  }
+
+private:
+  StreamType &stream_;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * A %utf8::stream is used to wrap a C++ standard I/O stream with a
+ * utf8::streambuf so that encoding/decoding and the management of the
+ * streambuf happens automatically.
+ *
+ * A %utf8::stream is useful for streams created by you.
+ *
+ * @tparam StreamType The I/O stream class type to wrap. It must be a concrete
+ * stream class.
+ */
+template<class StreamType>
+class stream : public StreamType {
+public:
+  /**
+   * Constructs a %utf8::stream.
+   */
+  stream() :
+#ifdef WIN32
+# pragma warning( push )
+# pragma warning( disable : 4355 )
+#endif /* WIN32 */
+    utf8_buf_( this->rdbuf() )
+#ifdef WIN32
+# pragma warning( pop )
+#endif /* WIN32 */
+  {
+    init();
+  }
+
+  /**
+   * Constructs a %stream.
+   *
+   * @tparam StreamArgType The type of the first argument of \a StreamType's
+   * constructor.
+   * @param stream_arg The argument to pass as the first argument to
+   * \a StreamType's constructor.
+   */
+  template<typename StreamArgType>
+  stream( StreamArgType stream_arg ) :
+    StreamType( stream_arg ),
+#ifdef WIN32
+# pragma warning( push )
+# pragma warning( disable : 4355 )
+#endif /* WIN32 */
+    utf8_buf_( this->rdbuf() )
+#ifdef WIN32
+# pragma warning( pop )
+#endif /* WIN32 */
+  {
+    init();
+  }
+
+  /**
+   * Constructs a %utf8::stream.
+   *
+   * @tparam StreamArgType The type of the first argument of \a StreamType's
+   * constructor.
+   * @param stream_arg The argument to pass as the first argument to
+   * \a StreamType's constructor.
+   * @param mode The open-mode to pass to \a StreamType's constructor.
+   */
+  template<typename StreamArgType>
+  stream( StreamArgType stream_arg, std::ios_base::openmode mode ) :
+    StreamType( stream_arg, mode ),
+#ifdef WIN32
+# pragma warning( push )
+# pragma warning( disable : 4355 )
+#endif /* WIN32 */
+    utf8_buf_( this->rdbuf() )
+#ifdef WIN32
+# pragma warning( pop )
+#endif /* WIN32 */
+  {
+    init();
+  }
+
+private:
+  streambuf utf8_buf_;
+
+  void init() {
+    this->std::ios::rdbuf( &utf8_buf_ );
+  }
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 
