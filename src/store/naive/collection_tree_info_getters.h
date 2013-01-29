0d0309748232a0d63b429030e6606b08171ebb8d
Ghislain Fourny <ghislain.fourny@28msec.com>
2013-01-29 17:15:20 +0100

    Committing missing file.

---
+++ src/store/naive/collection_tree_info_getters.h
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2006-2008 The FLWOR Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ZORBA_STORE_COLLECTION_TREE_INFO_GETTERS_H
+#define ZORBA_STORE_COLLECTION_TREE_INFO_GETTERS_H
+
+#include <zorba/config.h>
+
+#include "json_items.h"
+#include "node_items.h"
+
+namespace zorba
+{
+
+namespace simplestore
+{
+
+class CollectionTreeInfoGetters
+{
+public:
+  static simplestore::Collection* getCollection(const store::Item* lItem)
+  {
+    if (lItem->isNode())
+    {
+      const simplestore::XmlNode* lNode =
+          static_cast<const simplestore::XmlNode*>(lItem);
+      CollectionTreeInfo* lInfo = lNode->getTree()->theTreeInfo;
+      return lInfo ? lInfo->getCollection() : NULL;
+    }
+    if (lItem->isJSONItem())
+    {
+      const simplestore::json::JSONItem* lJSON =
+          static_cast<const simplestore::json::JSONItem*>(lItem);
+      CollectionTreeInfo* lInfo = lJSON->theTreeInfo;
+      return lInfo ? lInfo->getCollection() : NULL;
+    }
+    assert(false);
+    return NULL;
+  }
+
+  static const TreeId* getTreeId(const store::Item* lItem)
+  {
+    if (lItem->isNode())
+    {
+      const simplestore::XmlNode* lNode =
+          static_cast<const simplestore::XmlNode*>(lItem);
+      CollectionTreeInfo* lInfo = lNode->getTree()->theTreeInfo;
+      return lInfo ? &lInfo->getTreeId() : NULL;
+    }
+    if (lItem->isJSONItem())
+    {
+      const simplestore::json::JSONItem* lJSON =
+          static_cast<const simplestore::json::JSONItem*>(lItem);
+      CollectionTreeInfo* lInfo = lJSON->theTreeInfo;
+      return lInfo ? &lInfo->getTreeId() : NULL;
+    }
+    assert(false);
+    return NULL;
+  }
+
+  static StructuredItem* getRoot(const store::Item* lItem)
+  {
+    if (lItem->isNode())
+    {
+      const simplestore::XmlNode* lNode =
+          static_cast<const simplestore::XmlNode*>(lItem);
+      CollectionTreeInfo* lInfo = lNode->getTree()->theTreeInfo;
+      return lInfo ? lInfo->getRoot() : NULL;
+    }
+    if (lItem->isJSONItem())
+    {
+      const simplestore::json::JSONItem* lJSON =
+          static_cast<const simplestore::json::JSONItem*>(lItem);
+      CollectionTreeInfo* lInfo = lJSON->theTreeInfo;
+      return lInfo ? lInfo->getRoot() : NULL;
+    }
+    assert(false);
+    return NULL;
+  }
+
+  static const xs_integer& getPosition(const store::Item* lItem)
+  {
+    static xs_integer lDummy(-1);
+    if (lItem->isNode())
+    {
+      const simplestore::XmlNode* lNode =
+          static_cast<const simplestore::XmlNode*>(lItem);
+      CollectionTreeInfo* lInfo = lNode->getTree()->theTreeInfo;
+      return lInfo ? lInfo->getPosition() : lDummy;
+    }
+    if (lItem->isJSONItem())
+    {
+      const simplestore::json::JSONItem* lJSON =
+          static_cast<const simplestore::json::JSONItem*>(lItem);
+      CollectionTreeInfo* lInfo = lJSON->theTreeInfo;
+      return lInfo ? lInfo->getPosition() : lDummy;
+    }
+    assert(false);
+    return lDummy;
+  }
+
+  static simplestore::Collection* getCollection(const simplestore::XmlTree* aTree)
+  {
+    CollectionTreeInfo* lInfo = aTree->theTreeInfo;
+    return lInfo ? lInfo->getCollection() : NULL;
+  }
+
+  static const TreeId* getTreeId(const simplestore::XmlTree* aTree)
+  {
+    CollectionTreeInfo* lInfo = aTree->theTreeInfo;
+    return lInfo ? &lInfo->getTreeId() : NULL;
+  }
+
+  static StructuredItem* getRoot(const simplestore::XmlTree* aTree)
+  {
+    CollectionTreeInfo* lInfo = aTree->theTreeInfo;
+    return lInfo ? lInfo->getRoot() : NULL;
+  }
+
+  static const xs_integer& getPosition(const simplestore::XmlTree* aTree)
+  {
+    static xs_integer lDummy(-1);
+    CollectionTreeInfo* lInfo = aTree->theTreeInfo;
+    return lInfo ? lInfo->getPosition() : lDummy;
+  }
+};
+
+
+} // namespace simplestore
+} // namespace zorba
+#endif /* ZORBA_STORE_COLLECTION_TREE_INFO_GETTERS_H */
+
+/*
+ * Local variables:
+ * mode: c++
+ * End:
+ */
+/* vim:set et sw=2 ts=2: */
+
