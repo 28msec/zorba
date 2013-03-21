3538d3526b900df561f70f667bfe218654e63e3c
Matthias Brantner <matthias.brantner@28msec.com>
2013-03-21 11:13:09 -0700

    fixed crash when cloning array exprs not having a content

--- src/compiler/expression/expr_clone.cpp
+++ src/compiler/expression/expr_clone.cpp
@@ -204,7 +204,8 @@ expr* expr::clone(user_function* udf, substitution_t& subst) const
     create_json_array_expr(theSctx,
                            udf,
                            theLoc,
-                           e->theContentExpr->clone(udf, subst));
+                           (e->theContentExpr ?
+                            e->theContentExpr->clone(udf, subst) : NULL));
 
     break;
   }
