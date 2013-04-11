0a671ab430fa1e98603b23f8a53e63c79e53c9c7
Markos Zaharioudakis <markos_za@yahoo.com>
2013-04-11 09:57:41 +0200

    Fixed bug in merging flwor expressions (no merging of the domain expr of a FOR clause, if the FOR clause allows empty)

--- src/compiler/rewriter/rules/flwor_rules.cpp
+++ src/compiler/rewriter/rules/flwor_rules.cpp
@@ -1682,10 +1682,14 @@ expr* MergeFLWOR::apply(RewriterContext& rCtx, expr* node, bool& modified)
           }
         }
       }
-      else if (c->get_kind() == flwor_clause::for_clause &&
-               static_cast<for_clause*>(c)->get_pos_var() == NULL)
+      else if (c->get_kind() == flwor_clause::for_clause)
       {
-        expr* domainExpr = static_cast<for_clause*>(c)->get_expr()->skip_wrappers();
+        for_clause* fc = static_cast<for_clause*>(c);
+
+        bool hasPosVar = (fc->get_pos_var() != NULL);
+        bool isOuter = (fc->is_allowing_empty());
+
+        expr* domainExpr = fc->get_expr()->skip_wrappers();
 
         if (domainExpr->get_expr_kind() == flwor_expr_kind &&
             !domainExpr->is_sequential())
@@ -1699,13 +1703,25 @@ expr* MergeFLWOR::apply(RewriterContext& rCtx, expr* node, bool& modified)
             flwor_clause* nestedClause = nestedFlwor->get_clause(j);
             flwor_clause::ClauseKind nestedClauseKind = nestedClause->get_kind();
             
-            if (nestedClauseKind != flwor_clause::let_clause &&
-                nestedClauseKind != flwor_clause::for_clause &&
-                nestedClauseKind != flwor_clause::where_clause)
+            if (nestedClauseKind == flwor_clause::let_clause)
+              continue;
+
+            if (nestedClauseKind == flwor_clause::for_clause &&
+                nestedClauseKind == flwor_clause::where_clause)
             {
-              merge = false;
-              break;
+              if (isOuter || hasPosVar)
+              {
+                merge = false;
+                break;
+              }
+              else
+              {
+                continue;
+              }
             }
+
+            merge = false;
+            break;
           }
         }
       }
