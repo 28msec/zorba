86a948b7ad61b37b8766917eed04cfec011ef689
Markos Zaharioudakis <markos_za@yahoo.com>
2013-04-17 03:49:17 -0700

    fixed bug in raising XQTY0086 (copying of namespace-sensitive values during node construction

--- src/store/naive/node_items.cpp
+++ src/store/naive/node_items.cpp
@@ -3991,16 +3991,32 @@ XmlNode* AttributeNode::copyInternal(
 
   bool isListValue;
 
-  if (parent == rootParent &&
-      typeName != NULL &&
-      (typeName->equals(GET_STORE().theSchemaTypeNames[store::XS_QNAME]) ||
-       typeName->equals(GET_STORE().theSchemaTypeNames[store::XS_NOTATION])))
-  {
-    throw XQUERY_EXCEPTION(err::XQTY0086);
-  }
-
   if (copymode.theTypePreserve)
   {
+    if ((parent == rootParent || copymode.theNsPreserve == false) &&
+        typeName != NULL)
+    {
+      if (theTypedValue->isAtomic())
+      {
+        store::SchemaTypeCode typecode = theTypedValue->getTypeCode();
+
+        if (typecode == store::XS_QNAME || typecode == store::XS_NOTATION)
+          throw XQUERY_EXCEPTION(err::XQTY0086);
+      }
+      else
+      {
+        const std::vector<store::Item_t>& values = getValueVector().getItems();
+        csize numValues = values.size();
+        for (csize i = 0; i < numValues; ++i)
+        {
+          store::SchemaTypeCode typecode = values[i]->getTypeCode();
+
+          if (typecode == store::XS_QNAME || typecode == store::XS_NOTATION)
+            throw XQUERY_EXCEPTION(err::XQTY0086);
+        }
+      }
+    }
+
     typedValue = theTypedValue;
     isListValue = haveListValue();
   }
