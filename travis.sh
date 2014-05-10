df729bcde9fc19760d31f8c30f7625365e41f2e6
William Candillon <wcandillon@28.io>
2014-05-10 20:36:07 +0200

    Update build script to run w3c tests

--- travis.sh
+++ travis.sh
@@ -1,22 +1,7 @@
+mkdir build && cd build && cmake -DZORBA_XQUERYX:BOOL=1 -DZORBA_TEST_XQUERYX:BOOL=1 -DZORBA_WITH_BIG_INTEGER=1 -DCMAKE_BUILD_TYPE=Release -DZORBA_TEST_PLAN_SERIALIZATION:BOOL=1 -DZORBATEST_USE_MT_XQTS:BOOL=1 .. && make -j2 && make all modules_svg
 ../test/rbkt/Scripts/w3c/import_w3c_testsuite.sh .. &
 ../test/rbkt/Scripts/w3c/import_w3c_full_text_testsuite.sh .. &
 ../test/update/Scripts/import_w3c_update_testsuite.sh .. &
 cmake -D ZORBA=bin/zorba -D BUILDDIR=build -P ../test/fots/ImportFOTS.cmake &
- 
-FAIL=0
-for job in `jobs -p`
-do
-  wait $job || let "FAIL+=1"
-done
- 
-if [ $FAIL != "0" ]
-then
-  exit 1
-fi
- 
 make fots-activate-sets
-
-env QTTEST=42 QTTEST2=other QTTESTEMPTY='' CLOUDANT_CREDENTIALS=/home/jenkins/rq/dbs/cloudant/credentials.json ctest -j2 -T Test -E '^zorba_info-extraction_module|conversion/currency-convert' --output-on-failure --no-compress-output &
-wait $! || FAIL=1
-cmake -D zorba=/usr/bin/zorba -P /home/jenkins/rq/ctest2junit.cmake
-exit ${FAIL}
+env QTTEST=42 QTTEST2=other QTTESTEMPTY='' ctest -j2 -T Test -E '^zorba_info-extraction_module|conversion/currency-convert' --output-on-failure --no-compress-output
