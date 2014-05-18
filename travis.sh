7c28a443bb2b3c403374466ec92e61a82d7c922a
William Candillon <w@28.io>
2014-05-18 14:34:41 +0200

    Reduce number of cores

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ mkdir ../zorba_modules
 cmake -Doutdir=../zorba_modules -Dallmodules=1 -P modules/DownloadModules.cmake
 
 echo ====== Build Zorba ======
-mkdir -p build && cd build && cmake -DZORBA_XQUERYX:BOOL=1 -DZORBA_TEST_XQUERYX:BOOL=1 -DZORBA_WITH_BIG_INTEGER=1 -DCMAKE_BUILD_TYPE=Release -DZORBA_TEST_PLAN_SERIALIZATION:BOOL=1 -DZORBATEST_USE_MT_XQTS:BOOL=1 .. && make -j32
+mkdir -p build && cd build && cmake -DZORBA_XQUERYX:BOOL=1 -DZORBA_TEST_XQUERYX:BOOL=1 -DZORBA_WITH_BIG_INTEGER=1 -DCMAKE_BUILD_TYPE=Release -DZORBA_TEST_PLAN_SERIALIZATION:BOOL=1 -DZORBATEST_USE_MT_XQTS:BOOL=1 .. && make -j8
 
 echo ====== Import Tests ======
 ../test/rbkt/Scripts/w3c/import_w3c_testsuite.sh ..
@@ -15,7 +15,7 @@ cmake -D ZORBA=bin/zorba -D BUILDDIR=. -P ../test/fots/ImportFOTS.cmake
 make fots-activate-sets
 
 echo ====== Run Tests ======
-env QTTEST=42 QTTEST2=other QTTESTEMPTY='' CLOUDANT_CREDENTIALS=/home/jenkins/rq/dbs/cloudant/credentials.json ctest -j32 -T Test -E '^zorba_info-extraction_module|conversion/currency-convert' --output-on-failure --no-compress-output &
+env QTTEST=42 QTTEST2=other QTTESTEMPTY='' CLOUDANT_CREDENTIALS=/home/jenkins/rq/dbs/cloudant/credentials.json ctest -j8 -T Test -E '^zorba_info-extraction_module|conversion/currency-convert' --output-on-failure --no-compress-output &
 wait $! || FAIL=1
 cmake -D zorba=./bin/zorba -P ../ctest2junit.cmake
 exit ${FAIL}
