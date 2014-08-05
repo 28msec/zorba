cc6ce6f8fb7d5fa0482f255cc18610d7ca8401f7
Matthias Brantner <mbrantner.gmail.com>
2014-08-05 11:38:05 -0700

    improved scripts to work with absolute paths everywhere

--- travis.sh
+++ travis.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 set -e
+
+ZORBA_SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ZORBA_BUILD_DIR="${ZORBA_SRC_DIR}/build"
+ZORBA_MODULE_DIR="${ZORBA_SRC_DIR}/../zorba_modules"
+
 set -x
 
-echo ====== Download External Modules ====
-mkdir -p ../zorba_modules
-cmake -Doutdir=../zorba_modules -Dallmodules=1 -P modules/DownloadModules.cmake
+echo "================================================================================"
+echo "ZORBA_SRC_DIR ${ZORBA_SRC_DIR}"
+echo "ZORBA_BUILD_DIR ${ZORBA_BUILD_DIR}"
+echo "ZORBA_MODULE_DIR ${ZORBA_MODULE_DIR}"
 
-echo ====== Build Zorba ======
-mkdir -p build && cd build && cmake -DZORBA_XQUERYX:BOOL=1 -DZORBA_TEST_XQUERYX:BOOL=1 -DZORBA_WITH_BIG_INTEGER=1 -DCMAKE_BUILD_TYPE=Release -DZORBA_TEST_PLAN_SERIALIZATION:BOOL=1 -DZORBATEST_USE_MT_XQTS:BOOL=1 .. && make -j8
+echo "================================================================================"
+mkdir -p "${ZORBA_MODULE_DIR}"
+cmake -Doutdir="${ZORBA_MODULE_DIR}" -Dallmodules=1 -Doverwrite=true -P "${ZORBA_SRC_DIR}/modules/DownloadModules.cmake"
 
-echo ====== Import Tests ======
-../test/rbkt/Scripts/w3c/import_w3c_testsuite.sh .. &
-../test/rbkt/Scripts/w3c/import_w3c_full_text_testsuite.sh .. &
-../test/update/Scripts/import_w3c_update_testsuite.sh .. &
+echo "================================================================================"
+mkdir -p "${ZORBA_BUILD_DIR}"
+cd "${ZORBA_BUILD_DIR}"
+cmake -DZORBA_XQUERYX:BOOL=1 -DZORBA_TEST_XQUERYX:BOOL=1 -DZORBA_WITH_BIG_INTEGER=1 -DCMAKE_BUILD_TYPE=Release -DZORBA_TEST_PLAN_SERIALIZATION:BOOL=1 -DZORBATEST_USE_MT_XQTS:BOOL=1 "${ZORBA_SRC_DIR}"
+make -j8
+
+echo "================================================================================"
+"${ZORBA_SRC_DIR}/test/rbkt/Scripts/w3c/import_w3c_testsuite.sh" "${ZORBA_SRC_DIR}" &
+"${ZORBA_SRC_DIR}/test/rbkt/Scripts/w3c/import_w3c_full_text_testsuite.sh" "${ZORBA_SRC_DIR}" &
+"${ZORBA_SRC_DIR}/test/update/Scripts/import_w3c_update_testsuite.sh" "${ZORBA_SRC_DIR}" &
 FAIL=0
 for job in `jobs -p`
 do
@@ -24,12 +37,12 @@ then
   exit 1
 fi
 
-echo ====== Import Tests ======
-cmake -D ZORBA=./bin/zorba -D BUILDDIR=/home/travis/build/28msec/zorba/build -P ../test/fots/ImportFOTS.cmake
+echo "================================================================================"
+cmake -D ZORBA="${ZORB_BUILD_DIR}/bin/zorba" -D BUILDDIR="${ZORBA_BUILD_DIR}" -P "${ZORBA_SRC_DIR}/test/fots/ImportFOTS.cmake"
 make fots-activate-sets
 
-echo ====== Run Tests ======
+echo "================================================================================"
 env QTTEST=42 QTTEST2=other QTTESTEMPTY='' CLOUDANT_CREDENTIALS=/home/jenkins/rq/dbs/cloudant/credentials.json ctest -j8 -T Test -E '^zorba_info-extraction_module|conversion/currency-convert' --output-on-failure --no-compress-output &
 wait $! || FAIL=1
-#cmake -D zorba=./bin/zorba -P ../ctest2junit.cmake
+#cmake -D zorba="${ZORB_BUILD_DIR}/bin/zorba" -P "${ZORBA_SRC_DIR}/ctest2junit.cmake"
 exit ${FAIL}
