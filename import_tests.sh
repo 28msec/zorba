9c54845727cd501e767f942cf155ca8574dd8006
Federico Cavalieri <f@28.io>
2014-10-10 15:03:47 +0200

    Reverted parallel execution of FOTS import

--- import_tests.sh
+++ import_tests.sh
@@ -1,7 +1,7 @@
-"${ZORBA_SRC_DIR}/test/rbkt/Scripts/w3c/import_w3c_testsuite.sh" --builddir "${ZORBA_BUILD_DIR}" "${ZORBA_SRC_DIR}" 
-"${ZORBA_SRC_DIR}/test/rbkt/Scripts/w3c/import_w3c_full_text_testsuite.sh" --builddir "${ZORBA_BUILD_DIR}" "${ZORBA_SRC_DIR}" 
-"${ZORBA_SRC_DIR}/test/update/Scripts/import_w3c_update_testsuite.sh" --builddir "${ZORBA_BUILD_DIR}" "${ZORBA_SRC_DIR}" 
-cmake -D ZORBA="${ZORBA_BUILD_DIR}/bin/zorba" -D BUILDDIR="${ZORBA_BUILD_DIR}" -P "${ZORBA_SRC_DIR}/test/fots/ImportFOTS.cmake"
+"${ZORBA_SRC_DIR}/test/rbkt/Scripts/w3c/import_w3c_testsuite.sh" --builddir "${ZORBA_BUILD_DIR}" "${ZORBA_SRC_DIR}" &
+"${ZORBA_SRC_DIR}/test/rbkt/Scripts/w3c/import_w3c_full_text_testsuite.sh" --builddir "${ZORBA_BUILD_DIR}" "${ZORBA_SRC_DIR}" &
+"${ZORBA_SRC_DIR}/test/update/Scripts/import_w3c_update_testsuite.sh" --builddir "${ZORBA_BUILD_DIR}" "${ZORBA_SRC_DIR}" &
+cmake -D ZORBA="${ZORBA_BUILD_DIR}/bin/zorba" -D BUILDDIR="${ZORBA_BUILD_DIR}" -P "${ZORBA_SRC_DIR}/test/fots/ImportFOTS.cmake" &
 
 FAIL=0
 for job in `jobs -p`
