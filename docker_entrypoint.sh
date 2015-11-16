8be0b919362b086364bdab30f620b686d1b3add2
William Candillon <wcandillon@gmail.com>
2015-11-16 10:52:37 +0100

    :lipstick:

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,6 +3,14 @@ set -e
 
 mkdir -p /zorba/build
 cd /zorba/build
-cmake ..
-make -j4
-./bin/zorba $@
+
+if [ $# -eq 0 ]
+  then
+    cmake ..
+    make -j4
+fi
+
+if [ $# -gt 0 ]
+  then
+    ./bin/zorba $@
+fi
