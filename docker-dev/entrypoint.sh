77b334de7362d9cc2d6ce28b9669cd16fe779815
William Candillon <wcandillon@gmail.com>
2015-11-19 18:51:03 +0100

    :wrench:

---
+++ docker-dev/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p /zorba/build
+cd /zorba/build
+cmake ..
+make -j4
+nginx -c "/zorba/src/server/tests/nginx/nginx.conf"
+spawn-fcgi -p 8000 -n "/zorba/build/src/server/zorba-server":
